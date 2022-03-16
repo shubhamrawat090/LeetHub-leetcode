62b72bfb49f6f19587584be4df4be65ddbdfd0c1
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-03-16 13:02:21 +0530

    Time: 1 ms (98.71%), Space: 44.4 MB (63.36%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.java
+++ 946-validate-stack-sequences/946-validate-stack-sequences.java
@@ -1,16 +1,15 @@
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-        Stack<Integer> st = new Stack<>(); // Create a stack
-        
+        int i = 0; // Intialise one pointer pointing on pushed array
         int j = 0; // Intialise one pointer pointing on popped array
         
         for(int val : pushed){
-            st.push(val); // insert the values in stack
-            while(!st.isEmpty() && st.peek() == popped[j]){ // if st.peek() values equal to popped[j];
-                st.pop(); // then pop out
+            pushed[i++] = val; // using pushed as the stack.
+            while(i > 0 && pushed[i - 1] == popped[j]){ // pushed[i - 1] values equal to popped[j];
+                i--; // decrement i
                 j++; // increment j
             }
         }
-        return st.isEmpty(); // check if stack is empty return true else false
+        return i == 0; // Since pushed is a permutation of popped so at the end we are supposed to be left with an empty stack
     }
 }
