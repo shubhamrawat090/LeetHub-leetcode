221d7389b493ad32238a7ed9e73867040eeca41c
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-03-17 14:26:44 +0530

    Time: 14 ms (24.30%), Space: 47.3 MB (38.15%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,52 +9,29 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ArrayList<TreeNode> pToRoot = nodeToRootPath(root, p);
-        ArrayList<TreeNode> qToRoot = nodeToRootPath(root, q);
-        
-        int i = pToRoot.size()-1;
-        int j = qToRoot.size()-1;
-        TreeNode LCA = null;
-        while(i >= 0 && j>= 0){
-        
-            if(pToRoot.get(i) != qToRoot.get(j)){
-                break;
-            }
-
-            LCA = pToRoot.get(i);
-            i--; j--;
+        if(root == null){
+            //no node exists
+            return null;
         }
         
-        return LCA;
+        if(root == p || root == q){
+            //if root is one of the nodes to be found then root is LCA
+            return root;
+        }
+        
+        //Get left and right's answers 
+        TreeNode leftAns = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightAns = lowestCommonAncestor(root.right, p, q);
+        
+        if(leftAns!=null && rightAns!=null){
+            //if left and right have p & q in them then root is LCA
+            return root;
+        }else if(leftAns!=null && rightAns==null){
+            //p & q both are present in left tree, then left child is the LCA
+            return leftAns;
+        }
+        
+        //p & q both are present in right tree, then right child is the LCA
+        return rightAns;
     }
-    
-    public ArrayList<TreeNode> nodeToRootPath(TreeNode node, TreeNode target){
-    // write your code here
-    if(node == null){
-      return new ArrayList<>();
-    }
-
-    if(node.val == target.val){
-      ArrayList<TreeNode> temp = new ArrayList<>();
-      temp.add(node);
-      return temp;
-    }
-
-    ArrayList<TreeNode> left = nodeToRootPath(node.left, target);
-    //if there is an ans for left subtree then add root to it and return
-    if(left.size()>0){
-      left.add(node);
-      return left;
-    }
-
-    ArrayList<TreeNode> right = nodeToRootPath(node.right, target);
-    //if there is an ans for right subtree then add root to it and return
-    if(right.size()>0){
-      right.add(node);
-      return right;
-    }
-
-    //if no ans is found in either left or right or root then return empty arraylist
-    return new ArrayList<>();
-  }
 }
