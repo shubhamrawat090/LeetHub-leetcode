cc8be1b59a29be02da3ed074ee991cba9eea4b88
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-03-17 14:47:51 +0530

    Time: 1 ms (93.30%), Space: 43 MB (19.24%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -14,40 +14,23 @@
  * }
  */
 class Solution {
+    List<Integer> res = new ArrayList<>();
+    int maxDepth = -1;
     public List<Integer> rightSideView(TreeNode root) {
-        if(root == null) return new ArrayList<>();
-        
-        List<Integer> ans = new ArrayList<>();
-        
-        //queue for processing
-        Queue<TreeNode> q = new ArrayDeque<>();
-        
-        //add root to queue
-        q.add(root);
-        
-        //work till queue is empty
-        while(!q.isEmpty()){
-            //currently queue contains nodes of a particular so do this size no. of times
-            int count = q.size();
-            
-            //list for nodes of current level
-            List<Integer> levels = new ArrayList<>();
-            
-            //bfs is from right to left so first value in queue is the rightmost node of that level
-            ans.add(q.peek().val);
-            while(count-- > 0){
-                //remove front node of queue as it contains first node of that level from left -> right
-                TreeNode curr = q.remove();
-                //add that value to levels list
-                levels.add(curr.val);
-                
-                //get the right child of current tree if it exists
-                if(curr.right!=null) q.add(curr.right);
-                //get the left child of current tree if it exists
-                if(curr.left!=null) q.add(curr.left);
-            }
+        if (root == null) return res;
+        helper(root, 0);
+        return res;
+    }
+    public void helper(TreeNode node, int depth) {
+        if (node == null) {
+            return;
         }
-        
-        return ans;
+        if (depth > maxDepth) {
+            res.add(node.val);
+        }
+        maxDepth = Math.max(maxDepth, depth);
+        helper(node.right, depth+1);
+        helper(node.left, depth+1);
+
     }
 }
