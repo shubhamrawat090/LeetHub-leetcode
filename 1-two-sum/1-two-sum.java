520b1d5d4bdf0b9b28391ea134ac05a37c7b657f
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-03-19 20:08:29 +0530

    Time: 2 ms (91.61%), Space: 46.5 MB (9.81%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,20 +1,17 @@
-public class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        // stores value->idx pair
-        HashMap<Integer,Integer> indexMap = new HashMap<Integer,Integer>();
-        for(int i = 0; i < numbers.length; i++){
-            //getting target - nums[i] and checking if it is present in hashmap which means that a pair is present with sum = target
-            Integer requiredNum = (Integer)(target - numbers[i]);
-            if(indexMap.containsKey(requiredNum)){
-                //getting idx of (target - curr no., our curr no. )
-                int toReturn[] = {indexMap.get(requiredNum), i};
-                //store these idx in arr and return it
-                return toReturn;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> valIdxMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            // if target-curr val exists in map
+            // then return the index for map element, curr element
+            if (valIdxMap.containsKey(target - nums[i])) {
+                return new int[] { valIdxMap.get(target - nums[i]), i };
             }
-            //store val->idx pair in map
-            indexMap.put(numbers[i], i);
+
+            valIdxMap.put(nums[i], i);
         }
-        //if no pair is found then return null
-        return null;
+
+        return new int[] { -1, -1 };
     }
 }
