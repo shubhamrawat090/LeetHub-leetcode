aed357c3e904cc624d2859a5cdfebe6bc602ec52
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-03-20 21:48:29 +0530

    Time: 1 ms (99.50%), Space: 43.5 MB (59.42%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
@@ -1,47 +1,26 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        List<Integer> ans = new ArrayList<>();
-        HashMap<Integer, Integer> map1 = new HashMap<>();
-        HashMap<Integer, Integer> map2 = new HashMap<>();
+        //frequency array for nums1
+        int[] arr = new int[1001];
+        for (int i = 0; i < nums1.length; i++) {
+            arr[nums1[i]]++;
+        }
+        //stores answer
+        ArrayList<Integer> al = new ArrayList<Integer>();
         
-        //frequency map for nums1
-        for(int i=0; i<nums1.length; i++){
-            int val = nums1[i];
-            if(map1.containsKey(val)){
-                map1.put(val, map1.get(val)+1);
-            }else{
-                map1.put(val, 1);
+        /*traverse through nums2 and check in frequency array of nums1 that if the values of nums2 are present in it, if that is the case then add that value in our ans list and subtract 1 from frequency as this value is processed 1 time*/
+        for (int i = 0; i < nums2.length; i++) {
+            if (arr[nums2[i]] > 0) {
+                arr[nums2[i]]--;
+                al.add(nums2[i]);
             }
         }
-        //frequency map for nums2
-        for(int i=0; i<nums2.length; i++){
-            int val = nums2[i];
-            if(map2.containsKey(val)){
-                map2.put(val, map2.get(val)+1);
-            }else{
-                map2.put(val, 1);
-            }
+        //converting array list to array(we can use a new array as well I have used the frequency array reference variable only as the need for it was finished so this variable can be reused)
+        arr = new int[al.size()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = al.get(i);
         }
-        
-        //traverse keys of map1 and check if they are present in map2
-        for(int key: map1.keySet()){
-            //if a key of map1 is present in map2 then the intersection is minimum of both their frequencies
-            if(map2.containsKey(key)){
-                int minFreq = Math.min(map1.get(key), map2.get(key));
-                //in ans list add key minFreq no. of times
-                while(minFreq-->0){
-                    ans.add(key);
-                }
-            }
-        }
-        
-        //convert list to array
-        int[] finalAns = new int[ans.size()];
-        for(int i=0; i<ans.size(); i++){
-            finalAns[i] = ans.get(i);
-        }
-        
-        //return final array
-        return finalAns;
+        //return final ans
+        return arr;
     }
 }
