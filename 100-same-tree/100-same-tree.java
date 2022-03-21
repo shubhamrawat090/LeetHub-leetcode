4313868285f39238f28dc0cf8a7712cd68bc5e5c
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-03-21 22:50:09 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (44.18%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -15,22 +15,21 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        //if both trees and empty that means they are equal
-        if(p == null && q == null) return true;
+        if(p==null && q==null){
+            return true;
+        }
         
-        //if one is empty and other isn't, they aren't equal
-        if((p == null && q != null) || (q == null && p != null)) return false;
+        if((p==null && q!=null) || (p!=null && q==null)){
+            return false;
+        }
         
-        //getting ans for left subtrees of both trees
         boolean leftAns = isSameTree(p.left, q.left);
-        
-        //getting ans for right subtrees of both trees
         boolean rightAns = isSameTree(p.right, q.right);
         
-        //if leftAns=true && rightAns=true && root values of both trees match then it means, they both are equal
-        if(leftAns && rightAns && (p.val == q.val)) return true;
+        if(leftAns && rightAns && p.val == q.val){
+            return true;
+        }
         
-        //Otherwise, the two trees aren't equal
         return false;
     }
 }
