01e17b177d73ff293adbf97ae89347bea197ebd1
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-03-23 16:01:48 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (17.53%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.java
+++ 991-broken-calculator/991-broken-calculator.java
@@ -1,6 +1,11 @@
 class Solution {
     public int brokenCalc(int startValue, int target) {
+        //if startValue>=target then only thing we can do is subtract 1, 
+        //the no. of times we have to subtract 1 is startValue - target i.e. the difference between then to reach the target
         if(startValue >= target) return startValue - target;
+        
+        //Otherwise, if target is even we divide it by 2
+        //if it is odd we add 1 to change it to even
         if(target % 2 == 0){
             return 1 + brokenCalc(startValue, target / 2);
         }
