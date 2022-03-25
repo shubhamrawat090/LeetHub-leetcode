d42a467440a5f72157f232b507e84709567dd095
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-03-25 13:13:48 +0530

    Time: 1 ms (99.54%), Space: 43 MB (9.55%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -1,14 +1,24 @@
 class Solution {
     public int twoCitySchedCost(int[][] costs) {
+        //half of costs array is taken
         int N = costs.length/2;
+        //calculates our cost
         int minCost = 0;
+        //iterator for calculating refund
         int idx = 0;
+        //arr of size = costs
         int[] refund = new int[2*N];
+        
         for(int[] cost : costs) {
+            //for each val of costs get cityB - cityA
             refund[idx++] = cost[1] - cost[0];
+            //initially think all of them are going to city A
             minCost += cost[0];
         }
+        //refund array has the loss encountered when all go to city A
         Arrays.sort(refund);
+        
+        //add first half of this loss to minimize the cost
         for (int i = 0; i < N; i++) {
             minCost += refund[i];
         }
