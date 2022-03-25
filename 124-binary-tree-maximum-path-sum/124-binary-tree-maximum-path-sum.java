ac0db4294c3e8f4cf696fbb65e65733424335f49
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-03-25 09:55:40 +0530

    Time: 1 ms (83.00%), Space: 47.6 MB (63.05%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
@@ -14,19 +14,29 @@
  * }
  */
 class Solution {
+    //global variable that holds out ans
     static int maxPathSum;
+    //helper function which checks at every root for maxPathSum and stores max of all values in global var
     public int pathSum(TreeNode root) {
+        //if no nodes then 0 is our ans
         if(root == null){
             return 0;
         }
         
+        //get ans for left subtree
         int leftAns = pathSum(root.left);
+        //get ans for right subtree
         int rightAns = pathSum(root.right);
+        //get ans for left subtree + root
         int leftWithRoot = leftAns + root.val;
+        //get ans for right subtree + root
         int rightWithRoot = rightAns + root.val;
+        //get ans for both subtrees and root i.e. entire tree
         int leftRightRoot = leftAns + rightAns + root.val;
+        //maximum of previous value, just root, entire tree, left subtree with root and right subtree with root
         maxPathSum = Math.max(maxPathSum, Math.max(root.val, Math.max(leftRightRoot, Math.max(leftWithRoot, rightWithRoot))));
         
+        //our function gives the best from just root, left subtree including root and right subtree including root
         return Math.max(root.val, Math.max(rightWithRoot, leftWithRoot));
     }
     
