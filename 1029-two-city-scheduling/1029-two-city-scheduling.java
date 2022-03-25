3f6cfd2fe52e2bc0756893a770e895e8d6fc239b
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-03-25 12:55:47 +0530

    Time: 2 ms (78.58%), Space: 41.6 MB (76.19%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -1,19 +1,17 @@
 class Solution {
-    
     public int twoCitySchedCost(int[][] costs) {
-        int cost = 0;
-        
-        //sort array costs according to maximum profit when sending to city a which means a[0]-b[0]>a[1]-b[1]
-        Arrays.sort(costs, (a, b) -> {
-            return (a[0] - a[1]) - (b[0] - b[1]);
-        });
-        
-        //after sorting first half is with maximized profit for city a and second half is maximized profit for city b
-        for(int i=0; i<costs.length; i++){
-            int[] arr = costs[i];
-            cost += i<costs.length/2? arr[0]: arr[1];
+        int N = costs.length * 1/2;
+        int minCost = 0;
+        int idx = 0;
+        int[] refund = new int[2*N];
+        for(int[] cost : costs) {
+            refund[idx++] = cost[1] - cost[0];
+            minCost += cost[0];
         }
-        
-        return cost;
+        Arrays.sort(refund);
+        for (int i = 0; i < N; i++) {
+            minCost += refund[i];
+        }
+        return minCost;
     }
 }
