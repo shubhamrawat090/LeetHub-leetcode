4e5c1d9a6d0c71a6e3d6a56ef253c59c5c7d97cc
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-03-25 12:39:14 +0530

    Time: 3 ms (45.38%), Space: 41.6 MB (76.19%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -8,6 +8,7 @@ class Solution {
             return (a[0] - a[1]) - (b[0] - b[1]);
         });
         
+        //after sorting first half is with maximized profit for city a and second half is maximized profit for city b
         for(int i=0; i<costs.length; i++){
             int[] arr = costs[i];
             cost += i<costs.length/2? arr[0]: arr[1];
