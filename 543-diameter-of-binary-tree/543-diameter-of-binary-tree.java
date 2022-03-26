99f4d23b5b6aed94695613de5c3ec8c0de17e0f8
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-03-26 19:36:23 +0530

    Time: 0 ms (100.00%), Space: 43.6 MB (27.49%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -14,23 +14,31 @@
  * }
  */
 class Solution {
+    //global variable for storing the best diameter of all nodes
     int diameter = 0;
+    
     public int diameterOfBinaryTree(TreeNode root) {
         height(root);
         return diameter;
     }
     
+    //calculates height but also stores the diameter of all nodes
     public int height(TreeNode root){
         if(root == null){
             return -1;
         }
         
+        //left child height
         int lh = height(root.left);
+        //right child height
         int rh = height(root.right);
-        
+
+        //current node's diameter
         int myDia = lh + rh + 2;
+        //comparing our current diameter with the best diameter so far
         diameter = Math.max(diameter, myDia);
         
+        //returning height of current node
         return Math.max(lh, rh) + 1;
     }
 }
