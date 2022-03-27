664b929e2cfcd6de9301101e8ed3ebb2feb60256
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-03-27 12:16:04 +0530

    Time: 1 ms (99.77%), Space: 48.4 MB (70.88%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -3,13 +3,10 @@ class Solution {
         int[] soldierNum = new int[mat.length];
         
         for(int i=0; i<mat.length; i++){
-            int soldiers=0;
-            for(int j=0; j<mat[0].length; j++){
-                if(mat[i][j]==1) soldiers++;
-                else break;
-            }
+            int soldiers = binarySearch(mat[i]);
             //we have added i because in case of same no. of soldiers the lower value of i will be considered weaker
             soldierNum[i] = soldiers*100 + i;
+            //to separate index i from this value we make the no. of soldiers into groups of 100 or any number and then just modulo by that to remove the soldiers value as it is in groups of 100
         }
         
         Arrays.sort(soldierNum);//sort to get weakest -> strongest soldiers
@@ -17,9 +14,25 @@ class Solution {
         int[] ans = new int[k];//stores our ans
         
         for(int i=0; i<k; i++){
-            ans[i] = soldierNum[i]%100;
+            ans[i] = soldierNum[i]%100;//removing soldiers num and getting i from it
         }
         
         return ans;
+    }
+    
+    private int binarySearch(int[] row){
+        int lo = 0;
+        int hi = row.length;
+        
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+            if (row[mid] == 1)
+                lo = mid + 1;
+            else
+                hi = mid;
+        }
+        
+        return lo;
     }
 }
