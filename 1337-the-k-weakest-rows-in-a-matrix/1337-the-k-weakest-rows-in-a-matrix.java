52ec28f2948dd9862e81b5dcc0d46454b7bad9f0
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-03-27 12:19:28 +0530

    Time: 3 ms (60.90%), Space: 48.6 MB (57.31%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -8,62 +8,62 @@
 
 
 //using Priority Queue
-class Solution {
-    public int[] kWeakestRows(int[][] mat, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0]!=b[0] ? b[0]-a[0] : b[1]-a[1]);
-        int[] res = new int[k];
+// class Solution {
+//     public int[] kWeakestRows(int[][] mat, int k) {
+//         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0]!=b[0] ? b[0]-a[0] : b[1]-a[1]);
+//         int[] res = new int[k];
         
-        for(int i = 0; i < mat.length; i++) {
-            int soldiers = 0;
-            for(int j = 0; j < mat[0].length; j++) {
-                if(mat[i][j] == 1) soldiers++;
-                else break;
-            }
-            pq.offer(new int[]{soldiers, i});
-        }
+//         for(int i = 0; i < mat.length; i++) {
+//             int soldiers = 0;
+//             for(int j = 0; j < mat[0].length; j++) {
+//                 if(mat[i][j] == 1) soldiers++;
+//                 else break;
+//             }
+//             pq.offer(new int[]{soldiers, i});
+//         }
         
-        while(pq.size() > k){
-            pq.poll();
-        }
+//         while(pq.size() > k){
+//             pq.poll();
+//         }
         
-        while(k > 0) res[--k] = pq.poll()[1];
+//         while(k > 0) res[--k] = pq.poll()[1];
         
-        return res;
-    }
-}
+//         return res;
+//     }
+// }
 
 
 //Using Binary Seearch 
-// class Solution {
-//     public int[] kWeakestRows(int[][] mat, int k) {
-//         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] != b[0] ? b[0] - a[0] : b[1] - a[1]);
-//         int[] ans = new int[k];
+class Solution {
+    public int[] kWeakestRows(int[][] mat, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] != b[0] ? b[0] - a[0] : b[1] - a[1]);
+        int[] ans = new int[k];
         
-//         for (int i = 0; i < mat.length; i++) {
-//             pq.offer(new int[] {numOnes(mat[i]), i});
-//             if (pq.size() > k)
-//                 pq.poll();
-//         }
+        for (int i = 0; i < mat.length; i++) {
+            pq.offer(new int[] {numOnes(mat[i]), i});
+            if (pq.size() > k)
+                pq.poll();
+        }
         
-//         while (k > 0)
-//             ans[--k] = pq.poll()[1];
+        while (k > 0)
+            ans[--k] = pq.poll()[1];
         
-//         return ans;
-//     }
+        return ans;
+    }
     
-//     private int numOnes(int[] row) {
-//         int lo = 0;
-//         int hi = row.length;
+    private int numOnes(int[] row) {
+        int lo = 0;
+        int hi = row.length;
         
-//         while (lo < hi) {
-//             int mid = lo + (hi - lo) / 2;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
             
-//             if (row[mid] == 1)
-//                 lo = mid + 1;
-//             else
-//                 hi = mid;
-//         }
+            if (row[mid] == 1)
+                lo = mid + 1;
+            else
+                hi = mid;
+        }
         
-//         return lo;
-//     }
-// }
+        return lo;
+    }
+}
