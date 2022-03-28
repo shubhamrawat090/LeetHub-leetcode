1aa9303094b01b4e15e024103a4003be087c8264
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-03-29 00:17:42 +0530

    Added solution - LeetHub

--- Array to BST - GFG/array-to-bst.java
+++ Array to BST - GFG/array-to-bst.java
@@ -33,64 +33,29 @@ class GFG
 
 class Solution
 {
-    class Node{
-        int data;
-        Node left;
-        Node right;
-        
-        Node(){}
-        Node(int data){
-            this.data = data;
-            left = null;
-            right = null;
-        }
-    }
-    
     public int[] sortedArrayToBST(int[] nums)
     {
         // Code here 
-        Node root = helper(nums, 0, nums.length-1);
+        int[] ans = new int[nums.length];
         
-        ArrayList<Integer> ans = preorder(root);
-        
-        int[] finalAns = new int[ans.size()];
-        int i = 0;
-        for(int val: ans){
-            finalAns[i++] = val;
-        }
-        
-        return finalAns;
-    }
-    
-    private ArrayList<Integer> preorder(Node root){
-        if(root == null){
-            return new ArrayList<>();
-        }
-        
-        ArrayList<Integer> ans = new ArrayList<>();
-        
-        ans.add(root.data);
-        ans.addAll(preorder(root.left));
-        ans.addAll(preorder(root.right));
+        helper(nums, 0, nums.length-1, ans);
         
         return ans;
-    } 
+    }
     
-    private Node helper(int[] arr, int start, int end){
+    int idx = 0;
+    private void helper(int[] arr, int start, int end, int[] ans){
         if(start > end){
             //just like binary search STOP end start idx exceeds end idx
-            return null;
+            return;
         }
+        
         int mid = start + (end - start)/2;
+        ans[idx++] = arr[mid];
         
-        Node root = new Node(arr[mid]);//root is the middle value of sorted arr
+        helper(arr, start, mid-1, ans);
+        helper(arr, mid+1, end, ans);
         
-        //faith-> get BST converted for left subtree
-        root.left = helper(arr, start, mid-1);
-        
-        //faith-> get BST converted for right subtree
-        root.right = helper(arr, mid+1, end);
-        
-        return root;
+        return;
     }
 }
