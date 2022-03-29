21fc7647ca0b1a38978aa25368e53fc5e76bab5d
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-03-29 19:42:08 +0530

    Time: 3 ms (27.95%), Space: 45.6 MB (39.15%) - LeetHub

--- 530-minimum-absolute-difference-in-bst/530-minimum-absolute-difference-in-bst.java
+++ 530-minimum-absolute-difference-in-bst/530-minimum-absolute-difference-in-bst.java
@@ -19,27 +19,37 @@ class Solution {
             return 0;
         }
         
+        //stores the inorder of tree which is in increasing order
         List<Integer> increasingList = new ArrayList<>();
+        //do inorder traversal and store elements in the list
         inorder(root, increasingList);
         
+        //get the minDiff of all diffs
         int minDiff = Integer.MAX_VALUE;
+        //traverse the list and compare adjacent value differences with our minDiff
         for(int i=0; i<increasingList.size()-1; i++){
             int diff = Math.abs(increasingList.get(i) - increasingList.get(i+1));
             minDiff = Math.min(minDiff, diff);
         }
         
+        //return calculated minDiff
         return minDiff;
     }
     
+    //inorder function
     private void inorder(TreeNode root, List<Integer> list){
+        //stop when no node exist
         if(root == null){
             return;
         }
         
+        //left call
         inorder(root.left, list);
         
+        //node -> add value of node in the list
         list.add(root.val);
         
+        //right call
         inorder(root.right, list);
     }
 }
