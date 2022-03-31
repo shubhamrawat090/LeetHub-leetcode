bd1f03dd41f5737e26203c7d4e9a6e163e65aaa1
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-03-31 06:25:54 +0530

    Time: 9 ms (7.63%), Space: 50 MB (51.93%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -1,31 +1,38 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        //2 Pointer Approch USED
-        //start and end pointer
-        int left = 0, right = numbers.length-1;
-        
-        //traverse before the meet
-        while(left<right){
-            //sum of no. at start and end ptr
-            int sum = numbers[left]+numbers[right];
-            
-            //return the indices(1-based indexing) of the pair, if found
-            if(sum == target){
-                return new int[]{left+1, right+1};
-            }
-            //sum < target means we have to increase our sum to meet target
-            if(sum<target){
-                //left pointer is increased to go to a higher value as arr is sorted
-                left++;
-            }
-            //sum > target means we have to deccrease our sum to meet target
-            else{
-                //right pointer is deccreased to go to a lower value as arr is sorted
-                right--;
+        if(numbers.length == 0){
+            return new int[]{-1, -1};
+        }
+
+        for(int i=0; i<numbers.length; i++){
+            int val = numbers[i];
+            int complement = target - val;
+
+            int compIdx = search(numbers, complement, i);
+            if(compIdx!=-1 && compIdx!=i){
+                return new int[]{i+1, compIdx+1};
             }
         }
         
-        //no pair found
-        return null;
+        return new int[]{-1, -1};
+    }
+    
+    public int search(int[] nums, int target, int idx) {
+        int start=0, end=nums.length-1;
+        
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            
+            if(nums[mid] == target){
+                if(mid == idx) start = mid + 1;
+                else return mid;
+            }else if(nums[mid] < target){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        
+        return -1;
     }
 }
