441da986344ee03e82b099c0b7a1c5212d037d3c
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-03-31 05:38:17 +0530

    Time: 2 ms (98.79%), Space: 50.4 MB (72.47%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -15,40 +15,50 @@
  */
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
+        //root is sent 2 times because 1 of them is the root of original tree as we have to start searching in the original tree and not the root of the subtree in different recursive function calls
         return targetSumPair(root, root, k);
     }
     
+    //for every node search its complement(targetSum - node.data) in the entire tree
+    //time O(NlogN) => traversing each node * searching in entire tree
     public boolean targetSumPair(TreeNode node, TreeNode originalRoot, int targetSum){
         if(node == null){
           return false;
         }
 
+        //get ans from left subtree if there is a pair available
         boolean left = targetSumPair(node.left, originalRoot, targetSum);
 
+        //complement of our root's value
         int complement = targetSum - node.val;
+        //only pairs allowed are (a,b) where a<b, a = root.val, b = complement of root.val
         if(complement>node.val){
           if(search(originalRoot, complement)==true)
             return true;
         }
 
+        //get ans from left subtree if there is a pair available
         boolean right = targetSumPair(node.right, originalRoot, targetSum);
         
+        //return true if either of the ans is true which means we have found atleast 1 pair
         return right || left;
-      } 
+    } 
+    
+      
+      // function to search in the entire tree
+      public boolean search(TreeNode node, int target){
+        if(node == null){
+          return false;
+        }
 
-  public boolean search(TreeNode node, int target){
-    if(node == null){
-      return false;
-    }
+        if(node.val == target){
+          return true;
+        }
 
-    if(node.val == target){
-      return true;
-    }
-
-    if(target<node.val){
-      return search(node.left, target);
-    }else{
-      return search(node.right, target);
-    }
-  }
+        if(target<node.val){
+          return search(node.left, target);
+        }else{
+          return search(node.right, target);
+        }
+      }
 }
