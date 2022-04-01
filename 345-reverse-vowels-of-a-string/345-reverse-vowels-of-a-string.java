56db6dc52a26e08398f60ae6ac37bebbbd7c52c0
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-01 09:27:11 +0530

    Time: 4 ms (87.32%), Space: 46 MB (27.98%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.java
@@ -1,30 +1,43 @@
 class Solution {
+    //function to check for vowels(UPPER AND LOWER CASE)
     private boolean checkVowel(char ch){
         return (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch == 'u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch == 'U');
     }
+    
+    
     public String reverseVowels(String s) {
+        //convert char arr because of string immutability
         char[] chArr = s.toCharArray();
+        
+        //starting and ending pointer
         int i=0, j=chArr.length-1;
         
+        //work till the pointers meet
         while(i<j){
+            //move i ptr to right for consonants
             while(i<j && !checkVowel(chArr[i])){
                 i++;
             }
             
+            //move j ptr to right for vowels
             while(j>i && !checkVowel(chArr[j])){
                 j--;
             }
             
+            //if i and j meet that means there is only 1 vowel which does not need to be reversed
             if(i>=j) break;
             
+            //reverse ith & jth characters which are vowels
             char temp = chArr[i];
             chArr[i] = chArr[j];
             chArr[j] = temp;
             
+            //move i to 1 step right and j to 1 step left
             i++;
             j--;
         }
         
+        //convert char arr to string and return the ans
         StringBuilder ans = new StringBuilder();
         for(char ch: chArr){
             ans.append(ch);
