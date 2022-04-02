2a0a6dae0d91ac9ba64d1f454d7ccb983f050883
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-02 07:16:17 +0530

    Time: 22 ms (8.37%), Space: 54.8 MB (29.46%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -0,0 +1,60 @@
+/* ALGORITHM
+
+    * Maintain 2 Pointers : Start and End pointing to the first and last index of the string.
+    * Now traverse a while loop until start < end.
+    * If char pointed by both pointer is same than,
+    * Increment start and Decrement end.
+    * If character didn't match increment start and end ramins same and increase the cnt1, as we are skipping the character from starting.
+    * cnt will keep the track of no of instances that didn't match.
+    * Reinitialise the start and end pointers.
+    * Now again use a while loop and this time if character not matched decrement end end & start reamains same and incerease cnt2, as we are skipping character from end
+    * If cnt or cnt1 is greater than 1 i will break the loop for optimization so that no that no need to compare all string content.
+    * also we can skipped only 1 character therfore break only when cnt > 1.
+    * if cnt1 and cnt2 remains 0 that means string is already a palindrome.
+    * if cnt1 and cnt2 is 1 this means one character is obstructing the string to be palindrome, therefore we will delete that char by skipping it as done in code.
+    * skipping start once and skipping end once.
+    * If cnt1 == cnt2 remains 0 or 1 then return true.
+    * As deleting of one character will result in a palindrome.
+*/
+
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i=0, j=s.length()-1;
+        int countSkipStart = 0;
+        while(i<j){
+            if(s.charAt(i) != s.charAt(j)){
+                countSkipStart++;
+                i++;
+            }else{
+                //s.charAt(i) == s.charAt(j)
+                i++;
+                j--;
+            }
+        }
+        
+        i=0;
+        j=s.length()-1;  
+        int countSkipEnd = 0;
+        
+        while(i<j){
+            if(s.charAt(i) != s.charAt(j)){
+                countSkipEnd++;
+                j--;
+            }else{
+                //s.charAt(i) == s.charAt(j)
+                i++;
+                j--;
+            }
+        }
+        
+        if(countSkipStart == 1 || countSkipEnd == 1){
+            return true;
+        }
+        
+        if(countSkipStart == 0 || countSkipEnd == 0){
+            return true;
+        }
+        
+        return false;
+    }
+}
