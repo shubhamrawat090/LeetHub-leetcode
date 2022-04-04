52901be71d1691219e3150baa0bfef965b316dde
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-04 06:40:53 +0530

    Time: 3 ms (81.58%), Space: 184.4 MB (20.18%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -10,33 +10,20 @@
  */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-        if(head==null || head.next==null) return head;
-        ListNode left = new ListNode();
-        ListNode right = new ListNode();
-        ListNode temp = head;
-        int size=0;
-        while(temp!=null){
-            temp = temp.next;
-            size++;
+        ListNode prev = head, nxt = head;
+        for(int i = 1; i <= k - 1; i++)
+            nxt = nxt.next;
+        ListNode marker1 = nxt;
+        while(nxt.next!=null)
+        {
+            prev = prev.next;
+            nxt = nxt.next;
         }
         
-        // if(k==size-k+1){
-        //     return head;
-        // }
-        temp=head;
-        for(int i=0; i<size; i++){
-            if(i==k-1){
-                left = temp;
-            }
-            if(i==size-k){
-                right = temp;
-            }
-            temp=temp.next;
-        }
-      
-        int tempVal =left.val;
-        left.val = right.val;
-        right.val = tempVal;
+        // swap(prev.val, marker1.val);
+        int temp = prev.val;
+        prev.val = marker1.val;
+        marker1.val = temp;
         
         return head;
     }
