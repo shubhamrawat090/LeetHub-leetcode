74cc854285a1434f7fa26e93994c45e130ceb5c5
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-05 15:55:55 +0530

    Time: 5 ms (44.04%), Space: 81.2 MB (38.32%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -1,14 +1,25 @@
 class Solution {
     public int maxArea(int[] height) {
+        //keeps max area
         int maxArea = 0;
-        int n = height.length;
+        
+        //pointer at start and end of arr
         int left = 0, right = height.length - 1;
+        
+        //work till both pointers meet
         while(left<right){
+            //distance between two lines
             int dist = right-left;
+            //height to be taken is the minimum of both heights as water will fill till this height only
             int ht = Math.min(height[left], height[right]);
+            //calculate the area
             int area = dist*ht;
+            //store the max area out of all areas calculated
             maxArea = Math.max(maxArea, area);
             
+            //if left line is of smaller then we have found the best ans using this height and have to go the next on left and same for right
+            //this is because moving right ptr when left is small will result in decreasing distance with being able to use the left height only as the new height encountered can be greater, smaller, equal to the left height which will only decrease our area as the distance is decreased when moving the right pointer
+            //SO MOVE THE POINTER WHICH IS THE SMALLEST AS THE CURR ANS IS THE BEST ONE USING THAT HEIGHT
             if(height[left]<height[right]){
                 left++;
             }else{
@@ -16,6 +27,7 @@ class Solution {
             }
         }
         
+        //return calculated area
         return maxArea;
     }
 }
