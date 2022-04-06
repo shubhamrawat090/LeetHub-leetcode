af1f3a847a205a10011b9c1db6481c112735ac8d
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-06 17:21:07 +0530

    Time: 693 ms (15.73%), Space: 42.6 MB (75.40%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
@@ -1,42 +1,17 @@
 class Solution {
     public int threeSumMulti(int[] arr, int target) {
-        int mod = 1000000007; //10^9 + 7
-        long result = 0; //count of triplets
-        long[] count = new long[101];
+        Map<Integer, Integer> map = new HashMap<>();
         
-        for(int val: arr) count[val]++; // store occurrences of each value in array
-        
-        //take 2 pointers i and j from 0-100
-        for(int i=0; i<=100; i++){
-            for(int j=i; j<=100; j++){
-                //get a k value such that i + j + k = target
-                int k = target - i - j;
-                
-                if(k<0 || k>100) continue; // skip values outside range 0-100
-                
-                // 3 cases for i and j and k
-                
-                // FIRST, i==j==k, C(n,3) = n!/3!(n-3)! = n*(n-1)*(n-2)/6
-                if(i==j && j==k){//all same
-                    result += (count[i] * (count[i]-1) * (count[i]-2))/6;
-                    result %= mod;//keep result in range
-                }
-                
-                // SECOND, i==j!=k, C(n,2)*count[k] = n!/2!(n-2)!*count[k] = n*(n-1)/2*count[k]
-                else if(i==j && j!=k){//1 different
-                    result += ((count[i] * (count[i]-1))/2)*count[k];
-                    result %= mod;//keep result in range
-                }
-                
-                // THIRD, i<j<k, count[i]*count[j]*count[k]
-                else if(i<j && j<k){//all different
-                    result += count[i]*count[j]*count[k];
-                    result %= mod;//keep result in range
-                }
+        int res = 0;
+        int mod = 1000000007;
+        for (int i = 0; i < arr.length; i++) {
+            res = (res + map.getOrDefault(target - arr[i], 0)) % mod;
+            
+            for (int j = 0; j < i; j++) {
+                int temp = arr[i] + arr[j];
+                map.put(temp, map.getOrDefault(temp, 0) + 1);
             }
         }
-        
-        //convert result to int, keep result in range with mod and return
-        return (int)(result%mod);
+        return res;
     }
 }
