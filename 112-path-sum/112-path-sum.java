460cbcea1b58d203d19c4de7beb2d78ff1459e5f
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-10 13:04:00 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (77.61%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -15,12 +15,16 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
+        //if no node exists return false
         if(root == null) return false;
         
+        //if leaf node is encountered, check that current node's value is equal to targetSum as targetSum has been reduced by node.val at each node before it and if our curr node's value matches targetSum it means targetSum reduced to 0
+        //Our motive is to traverse to each node till we encounter leaf node and reduce targetSum by node.val till it becomes zero
         if(root.left == null && root.right == null){
             return root.val == targetSum;
         }
         
+        //reduce targetSum by curr node's val and check left and right subtree if either of them can reduce our targetSum exactly to zero
         return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
     }
 }
