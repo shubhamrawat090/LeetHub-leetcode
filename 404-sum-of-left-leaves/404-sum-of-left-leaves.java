170cdbf97e6f2d46091737f9594123bb4034b329
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-10 13:20:10 +0530

    Time: 1 ms (27.03%), Space: 41.7 MB (66.20%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.java
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.java
@@ -16,15 +16,21 @@
 class Solution {
     
     public int sumOfLeftLeaves(TreeNode root) {
+        //return 0 if no node exists
         if(root == null) return 0;
+        
+        //calculates sum of left leaf nodes
         int sum = 0;
         
+        //if left node exists, then check if it is a leaf or not => add its value to sum if left node is a leaf
         if(root.left!=null && (root.left.left == null && root.left.right == null)){
             sum += root.left.val;
         }
         
+        //add the sum of remaining left leaf nodes from left and right subtree
         sum += sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
         
+        //return final calculated sum
         return sum;
     }
 }
