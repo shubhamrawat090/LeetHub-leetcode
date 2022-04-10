a395d6dcde71bad207876bf559a53a4d6c304640
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-10 05:45:37 +0530

    Time: 4 ms (54.24%), Space: 42.9 MB (13.58%) - LeetHub

--- 682-baseball-game/682-baseball-game.java
+++ 682-baseball-game/682-baseball-game.java
@@ -4,36 +4,22 @@ class Solution {
         
         for(String op: ops){
             if(op.equals("+")){
-                //add prev 2
-                int topVal = stk.pop();
-                int bottomVal = stk.pop();
-                int newVal = topVal + bottomVal;
-                
-                //push bottomVal
-                stk.push(bottomVal);
-                
-                //push topVal
-                stk.push(topVal);
-                
-                //push newVal
-                stk.push(newVal);
-                
-            }else if(op.equals("C")){
-                //remove prev
-                stk.pop();
-                
+                int first = stk.pop();
+                int second = stk.pop();
+                int sum = first + second;
+                stk.push(second);
+                stk.push(first);
+                stk.push(sum);
             }else if(op.equals("D")){
-                //double
                 stk.push(2*stk.peek());
-                
+            }else if(op.equals("C")){
+                stk.pop();
             }else{
-                //number
                 stk.push(Integer.parseInt(op));
             }
         }
         
         int score = 0;
-        //calculate score stored in stack
         while(!stk.isEmpty()){
             score += stk.pop();
         }
