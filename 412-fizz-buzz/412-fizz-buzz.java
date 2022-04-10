ae4aac8b2ca9080559195e879fb7284800280eda
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-10 13:31:21 +0530

    Time: 2 ms (64.56%), Space: 48.5 MB (36.41%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.java
+++ 412-fizz-buzz/412-fizz-buzz.java
@@ -1,14 +1,25 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
+        //stores ans
         List<String> ans = new ArrayList<>();
+        
+        //traverse from 1 to n
         for(int i=1; i<=n; i++){
+            //if i is divisible by 3 & 5 both
             if(i%3 == 0 && i%5 == 0){
                 ans.add("FizzBuzz");
-            }else if(i%3 == 0){
+            }
+            //if i is divisible by 3
+            else if(i%3 == 0){
                 ans.add("Fizz");
-            }else if(i%5 == 0){
+            }
+            //if i is divisible by 5
+            else if(i%5 == 0){
                 ans.add("Buzz");
-            }else{
+            }
+            //if i is divisible by neither 3 nor 5
+            else{
+                // i+"" converts integer to string
                 ans.add(i+"");
             }
         }
