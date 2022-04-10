50614be4c2e2533633299c1554c5185965ba3c30
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-10 05:56:15 +0530

    Time: 1 ms (96.26%), Space: 43.1 MB (23.13%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.java
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -2,17 +2,25 @@ class Solution {
     public int minOperations(String[] logs) {
         int levels = 0;
         for(String log: logs){
+            //if at root and "../" is performed, do nothing 
             if(levels == 0 && log.equals("../")){
                 continue;
-            }else if(log.equals("../")){
+            }
+            //go 1 level down
+            else if(log.equals("../")){
                 levels--;
-            }else if(log.equals("./")){
+            }
+            //do nothing as we have to remain on the same folder
+            else if(log.equals("./")){
                 continue;
-            }else{
+            }
+            //do 1 level deep
+            else{
                 levels++;
             }
         }
         
+        //levels is the no. of "../" operations required to go back to parent folder
         return levels;
     }
 }
