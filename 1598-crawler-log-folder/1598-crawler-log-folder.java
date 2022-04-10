09a9d7754daf8c7b4cfca65ad2179ee44b49028c
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-10 05:53:03 +0530

    Time: 1 ms (96.26%), Space: 41.6 MB (93.10%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        int levels = 0;
+        for(String log: logs){
+            if(levels == 0 && log.equals("../")){
+                continue;
+            }else if(levels!=0 && log.equals("../")){
+                levels--;
+            }else if(log.equals("./")){
+                continue;
+            }else{
+                levels++;
+            }
+        }
+        
+        return levels;
+    }
+}
