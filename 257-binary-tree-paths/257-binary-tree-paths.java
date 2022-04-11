da68c32d2d0a2085c3e63f3cdff663bd4cf1e5f3
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-11 11:59:23 +0530

    Time: 1 ms (99.98%), Space: 42.7 MB (80.02%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.java
+++ 257-binary-tree-paths/257-binary-tree-paths.java
@@ -14,30 +14,43 @@
  * }
  */
 class Solution {
+    //on each dfs iteration it stores all paths encountered
+    private List<String> res = new ArrayList<>();
+    
     public List<String> binaryTreePaths(TreeNode root) {
-        if(root == null){
-            return new ArrayList<>();
+        dfs(root, new StringBuilder());// pass to helper function with a stringbuilder for getting paths as we traverse
+        return res;// return all the paths stored in golbal variable
+    }
+    
+    //helper function
+    private void dfs(TreeNode root, StringBuilder sb) {
+        //if tree doesn't exist do nothing
+        if (root == null) {
+            return;
         }
         
-        if(root.left == null && root.right == null){
-            List<String> ans = new ArrayList<>();
-            ans.add(Integer.toString(root.val));
-            return ans;
+        //if adding first time then just add the value of current node
+        if (sb.length() == 0) {
+            sb.append(root.val);
+        }
+        //if there is already a path in sb then add "->our value" to it
+        else {
+            sb.append("->");
+            sb.append(root.val);
         }
         
-        List<String> left = binaryTreePaths(root.left);
-        List<String> right = binaryTreePaths(root.right);
-        
-        List<String> ans = new ArrayList<>();
-        
-        for(String val: left){
-            ans.add(root.val+"->"+val);
+        //on encountering leaf node STORE THE PATH INSIDE GLOBAL VARIABLE
+        if (root.left == null && root.right == null) {
+            res.add(sb.toString());
         }
         
-        for(String val: right){
-            ans.add(root.val+"->"+val);
-        }
-        
-        return ans;
+        //length of sb till our current path is stored
+        int len = sb.length();
+        //go to left subtree and add all paths to it
+        dfs(root.left, sb);
+        //reset path back to our root's val
+        sb.setLength(len);
+        //now go to right subtree and all paths to it
+        dfs(root.right, sb);
     }
 }
