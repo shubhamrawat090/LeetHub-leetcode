bb871475b5b4ba0bd3c588143f24807a3d1d4fff
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-15 11:52:08 +0530

    Time: 0 ms (100.00%), Space: 42 MB (84.81%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.java
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.java
@@ -15,19 +15,27 @@
  */
 class Solution {
     public TreeNode trimBST(TreeNode root, int low, int high) {
+        //if tree doesnt exist return null
         if(root == null){
             return null;
         }
         
+        //if root.val<low, then all values on the left of root are less than low due to BST property
+        //so the answer lies in right side only
         if(root.val<low){
             return trimBST(root.right, low, high);
-        }else if(root.val>high){
+        }
+        //if root.val>high, then all values on the right of root are higher than high due to BST property
+        //so the answer lies in left side only
+        else if(root.val>high){
             return trimBST(root.left, low, high);
         }
         
+        //if root.val is in range, then take root.val and now find answers for its left & right subtree and join them to its left and right
         root.left = trimBST(root.left, low, high);
         root.right = trimBST(root.right, low, high);
         
+        //return the tree
         return root;
     }
 }
