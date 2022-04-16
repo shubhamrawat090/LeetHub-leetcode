638551275de4673b619fe1be1aaa9d177d87073c
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-16 09:00:24 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (81.34%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.java
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.java
@@ -16,31 +16,25 @@
 class Solution {
     public TreeNode convertBST(TreeNode root) {
         if(root == null){
-            return root;
+            return null;
         }
         
-        //traverse reverse inorder(Right Node Left) -> Decreasing order, and store sum in golbal varaible
         reverseInorder(root);
-        //return newly changed tree
         return root;
     }
     
-    //global var that store sum of all greater value nodes + our value
     int sum = 0;
-    void reverseInorder(TreeNode root){
-        //no need to check it as we have check it in above function
-        if(root == null) return;
+    
+    public void reverseInorder(TreeNode root){
+        if(root == null){
+            return;
+        }
         
-        //////////////// RIGHT call ////////////////
         reverseInorder(root.right);
         
-        //////////////// NODE work ////////////////
-        
-        //store current root's value in previously calculated sum as sum = previous sum(sum of all greater val nodes) + our sum
         sum += root.val;
-        root.val = sum;//set our value as this sum
+        root.val = sum;
         
-        //////////////// LEFT call ////////////////
         reverseInorder(root.left);
     }
 }
