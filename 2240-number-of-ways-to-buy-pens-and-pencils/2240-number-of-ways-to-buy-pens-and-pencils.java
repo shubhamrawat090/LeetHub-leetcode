6cf46eb74b314c06398a30326d31a158fa33b80a
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-17 14:10:54 +0530

    Time: 16 ms (100.00%), Space: 39.3 MB (100.00%) - LeetHub

--- 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.java
+++ 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.java
@@ -2,30 +2,37 @@ class Solution {
     public long waysToBuyPensPencils(int total, int cost1, int cost2) {
         int i=0;
         long count=0;
+        //1 way 0 pencil 0 pen
         if(cost1>total && cost2> total){
             count++;
             return count;
         }
         
+        //cannot buy pen so buying pencils
         if(cost1>total){
             return (total/cost2)+1;
         }
         
+        //cannot buy pencils so buying pen
         if(cost2>total){
             return (total/cost1)+1;
         }
         
+        //till cost of buying multiple pens is in total budget
         while(i*cost1<total){
+            //getting no. of pencils we can buy
             int tempTotal = total;
             tempTotal -= i*cost1;
             count += (tempTotal/cost2)+1;  
             i++;
         }
         
+        //if you can still buy a pen the add it
         if(i*cost1<=total){
             count++;
         }
         
+        // return count
         return count;
     }
 }
