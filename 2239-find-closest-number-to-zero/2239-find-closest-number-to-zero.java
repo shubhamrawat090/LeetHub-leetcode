4856ec96e2dfcfad7cf3a48c25f49314d6f3c5b3
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-17 22:23:57 +0530

    Time: 2 ms (50.00%), Space: 41.9 MB (100.00%) - LeetHub

--- 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.java
+++ 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.java
@@ -1,33 +1,33 @@
-class Solution {
-    public int findClosestNumber(int[] nums) {
-        int ans = Integer.MAX_VALUE;
-        int diff = Integer.MAX_VALUE;
+// class Solution {
+//     public int findClosestNumber(int[] nums) {
+//         int ans = Integer.MAX_VALUE;
+//         int diff = Integer.MAX_VALUE;
         
-        for(int num: nums){
-            if(Math.abs(num-0)<diff){
-                diff = Math.abs(num-0);
-                ans = num;
-            }else if(Math.abs(num-0)==diff){
-                ans = Math.max(ans, num);
-            }
-        }
+//         for(int num: nums){
+//             if(Math.abs(num-0)<diff){
+//                 diff = Math.abs(num-0);
+//                 ans = num;
+//             }else if(Math.abs(num-0)==diff){
+//                 ans = Math.max(ans, num);
+//             }
+//         }
         
-        return ans;
+//         return ans;
         
-    }
-}
+//     }
+// }
 
 // 2ND APPROACH
 //O(n)
-// class Solution {
-//     public int findClosestNumber(int[] nums) {
-//         int ans=nums[0],i;
-//         for(i=1;i<nums.length;i++)
-//         {
-//             int d1=Math.abs(nums[i]),d2=Math.abs(ans);
-//             if(d1<d2) ans=nums[i];
-//             else if(d1==d2 && ans<nums[i]) ans=nums[i];
-//         }    
-//         return ans;
-//     }
-// }
+class Solution {
+    public int findClosestNumber(int[] nums) {
+        int ans=nums[0],i;
+        for(i=1;i<nums.length;i++)
+        {
+            int d1=Math.abs(nums[i]),d2=Math.abs(ans);
+            if(d1<d2) ans=nums[i];
+            else if(d1==d2 && ans<nums[i]) ans=nums[i];
+        }    
+        return ans;
+    }
+}
