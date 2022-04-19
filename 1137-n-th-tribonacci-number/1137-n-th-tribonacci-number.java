12b60686ada04ba84d6dee1ca63cbaf9bc3d18f1
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-19 22:42:39 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (28.95%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,39 @@
+class Solution { 
+    public int tribonacci(int n) {
+        //recursive
+        // return rec(n);
+        
+        //memoized
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return memo(n, dp);
+    }
+    
+    public int rec(int n){
+        if(n == 0){
+            return 0;
+        }
+        
+        if(n == 1 || n == 2){
+            return 1;
+        }
+        
+        return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
+    }
+    
+    public int memo(int n, int[] dp){
+        if(n == 0){
+            return dp[n] = 0;
+        }
+        
+        if(n == 1 || n == 2){
+            return dp[n] = 1;
+        }
+        
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        
+        return dp[n] = memo(n-1, dp) + memo(n-2, dp) + memo(n-3, dp);
+    }
+}
