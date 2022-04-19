bbe3de91f5c68cfff688000e57b79876122ebfdb
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-19 22:33:44 +0530

    Time: 1 ms (83.83%), Space: 43.1 MB (72.32%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -34,30 +34,40 @@ class Solution {
 //         return Math.min(start0, start1);
         
         //tabular
-        int[] dp = new int[cost.length+1];
-        for(int idx = cost.length; idx>=0; idx--){
-            if(idx == cost.length){
-                dp[idx] = 0;
-                continue;
-            }
+//         int[] dp = new int[cost.length+1];
+//         for(int idx = cost.length; idx>=0; idx--){
+//             if(idx == cost.length){
+//                 dp[idx] = 0;
+//                 continue;
+//             }
             
-            int ans = cost[idx];
+//             int ans = cost[idx];
 
-            int step1 = 0;
+//             int step1 = 0;
 
-            if(idx+1<=cost.length){
-                step1+=dp[idx+1];
-            }
+//             if(idx+1<=cost.length){
+//                 step1+=dp[idx+1];
+//             }
 
-            int step2 = 0;
+//             int step2 = 0;
 
-            if(idx+2<=cost.length){
-                step2+=dp[idx+2];
-            }
+//             if(idx+2<=cost.length){
+//                 step2+=dp[idx+2];
+//             }
 
-            dp[idx] = ans + Math.min(step1, step2);
+//             dp[idx] = ans + Math.min(step1, step2);
+//         }
+        
+//         return Math.min(dp[0], dp[1]);
+        
+        //space optimize
+        int next1 = cost[cost.length - 2], next2 = cost[cost.length-1];
+        for(int idx = cost.length-3; idx>=0; idx--){
+            int minCost = cost[idx] + Math.min(next1, next2);
+            next2 = next1;
+            next1 = minCost;
         }
         
-        return Math.min(dp[0], dp[1]);
+        return Math.min(next1, next2);
     }
 }
