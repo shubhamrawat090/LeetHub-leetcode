b85c5e2ec8eb53e11d6abf272acf38266b1334c1
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-19 14:11:14 +0530

    Time: 3 ms (69.35%), Space: 47.4 MB (42.12%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -14,54 +14,66 @@
  * }
  */
 class Solution {
-    //2 pointers containing the nodes that need to be swapped in order to recover BST
-    TreeNode first = null;
-    TreeNode second = null;
-    //keep prev as low as possible from the range mentioned in constraints
-    TreeNode prev = new TreeNode(Integer.MIN_VALUE);
-    
     public void recoverTree(TreeNode root) {
-        inorder(root);
         
-        //swap values of first and second pointer
+        /// USING MORRIS INORDER TRAVERSAL
+        
+        if(root == null) return;
+        
+        TreeNode prev = null;
+        TreeNode first = null;
+        TreeNode second = null;
+        
+        while(root!=null){
+            if(root.left == null){
+                // PROCESSING NODE
+                if(prev!=null && prev.val>root.val){
+                    //first not found
+                    if(first==null){
+                        first = prev;
+                    }
+                    
+                    second = root;
+                }
+                
+                prev = root;
+                
+                root = root.right;
+            }else{
+                //get a ptr to right most of left node
+                TreeNode temp = root.left;
+                while(temp.right != null && temp.right != root){
+                    temp = temp.right;
+                }
+                
+                //join temp to root to create threaded binary tree
+                if(temp.right == null){
+                    temp.right = root;
+                    root = root.left;
+                }else{
+                    temp.right = null;
+                    
+                    // PROCESSING NODE
+                    if(prev!=null && prev.val>root.val){
+                        //first not found
+                        if(first==null){
+                            first = prev;
+                        }
+
+                        second = root;
+                    }
+
+                    prev = root;
+                    
+                    root = root.right;
+                }
+            }
+        }
+        
+        //Swap first and second ptr values
         int temp = first.val;
         first.val = second.val;
-        second.val = temp; 
+        second.val = temp;
+        
     }
-    
-    void inorder(TreeNode root){
-        if(root == null){
-            return;
-        }
-        
-        inorder(root.left);
-        //processing
-        
-        //            root   3
-        //                 /   \
-        //                /     \
-        // prev(first)   1       4  prev
-        //                      /
-        //                     /
-        //                   2  root(second)
-        
-        //first node not found & the pair with anomaly found(prev.val>root.val)
-        if(first==null && prev.val>root.val){
-            //set first pointer as prev
-            first = prev;
-        }
-        
-        //first node found now finding second & the pair with anomaly found(prev.val>root.val)
-        if(first!=null && prev.val>root.val){
-            //set second pointer as root
-            second = root;
-        }
-        
-        //after every processing prev is updated as root
-        prev = root;
-        
-        inorder(root.right);
-    }
-    
-    
 }
