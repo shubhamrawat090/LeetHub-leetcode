6d7b72e8980e6a13845e81f8de592fbb822990ac
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-20 07:19:43 +0530

    Time: 6 ms (17.07%), Space: 47.2 MB (14.61%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.java
@@ -1,50 +1,16 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public boolean sameTree(TreeNode nodeA, TreeNode nodeB){
-        if(nodeA == null && nodeB == null){
-            return true;
-        }
-        
-        if(nodeA == null || nodeB == null){
-            return false;
-        }
-        
-        if(nodeA.val != nodeB.val){
-            return false;
-        }
-        
-        return sameTree(nodeA.right, nodeB.right) && sameTree(nodeA.left, nodeB.left);
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null) return false;
+        return matchSubtree(root, subRoot) // if this matches below two recursions won't execute
+            || isSubtree(root.left, subRoot)
+            || isSubtree(root.right, subRoot);
     }
     
-    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root == null && subRoot == null){
-            return true;
-        }
-        
-        if(root == null || subRoot == null){
-            return false;
-        }
-        
-        boolean ans = false;
-        
-        if(root.val == subRoot.val){
-            ans = sameTree(root, subRoot);
-        }
-        
-        return ans || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    boolean matchSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null && subRoot == null) return true;
+        if(root == null || subRoot == null) return false;
+        if(root.val != subRoot.val) return false;
+        return matchSubtree(root.left, subRoot.left)
+                && matchSubtree(root.right, subRoot.right);
     }
 }
