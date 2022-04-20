855bb88f4066ee7ba952d22223c46e0d2405dcda
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-20 07:20:34 +0530

    Time: 3 ms (96.16%), Space: 46.5 MB (52.78%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.java
@@ -1,16 +1,53 @@
+// Strategy: keep doing a dfs until your parent node matches the child node. If your parent node matches the child node, simultaneously DFS the parent and the child and check if they're equal
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root == null) return false;
-        return matchSubtree(root, subRoot) // if this matches below two recursions won't execute
-            || isSubtree(root.left, subRoot)
-            || isSubtree(root.right, subRoot);
+        return dfs(root, subRoot);
     }
     
-    boolean matchSubtree(TreeNode root, TreeNode subRoot) {
-        if(root == null && subRoot == null) return true;
-        if(root == null || subRoot == null) return false;
-        if(root.val != subRoot.val) return false;
-        return matchSubtree(root.left, subRoot.left)
-                && matchSubtree(root.right, subRoot.right);
+    public boolean dfs(TreeNode parent, TreeNode child){
+        if(parent == null || child == null){
+            return false;
+        }
+        if(parent.val == child.val){
+            if(isValidTree(parent, child)){
+                return true;
+            }
+        }
+        return dfs(parent.left, child) || dfs(parent.right, child);
+        
+    }
+    
+    public boolean isValidTree(TreeNode parent, TreeNode child){
+        if(parent == null && child == null){
+            return true;
+        }
+        if(parent != null && child == null){
+            return false;
+        }
+        if(parent == null && child != null){
+            return false;
+        }
+        if(parent.val == child.val){
+            if(isValidTree(parent.left, child.left) && isValidTree(parent.right, child.right)){
+                return true;
+            }
+        }
+        return false;
     }
 }
