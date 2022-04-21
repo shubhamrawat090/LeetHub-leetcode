6222eccedf89473b3118a0487d7404bd76c1e30a
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-21 20:25:27 +0530

    Time: 1 ms (57.92%), Space: 42 MB (40.21%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -1,22 +1,30 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
+        //contains our answer
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        //current row and previous row
         List<Integer> currRow, prevRow = null;
         
+        //iterate till no. of rows
         for(int i=0; i<numRows; i++){
+            //initialize empty row
             currRow = new ArrayList<Integer>();
             for(int j=0; j<=i; j++){
+                //first and last element of row is 1
                 if(j==0 || j==i){
                     currRow.add(1);
-                }else{
+                }
+                //take j-1 and j values from prevRow and add to get current row's element at j
+                else{
                     currRow.add(prevRow.get(j-1) + prevRow.get(j));
                 }
             }
-            
+            //store curr row in prev to work for next row
             prevRow = currRow;
+            //store our current computed row in answer list
             ans.add(currRow);
         }
-        
+        //return answers list
         return ans;
     }
 }
