96a9540d51e5ae91ef753abbf1fa8e129c4e5996
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-21 17:31:39 +0530

    Time: 13 ms (92.01%), Space: 49.5 MB (92.92%) - LeetHub

--- 705-design-hashset/705-design-hashset.java
+++ 705-design-hashset/705-design-hashset.java
@@ -1,33 +1,42 @@
 class MyHashSet {
-    private int numBuckets = 14000;
-    List<Integer>[] buckets;
+    private int numBuckets = 15000; // max loading factor = 10000(in constraints)/15000 = 0.667 < 0.75(required in JAVA)
+    List<Integer>[] buckets; //define the bucket of bucket size
     
     private int hash_function(int key){
+        //our hashfunction which gives use a hashvalue from 1 to numBuckets
         return key % numBuckets;
     }
 
     /** Initialize your data structure here. */
     public MyHashSet() {
+        //initializing the bucket
         buckets = new LinkedList[numBuckets];
     }
     
     public void add(int key) {
-        int i = hash_function(key);
+        int i = hash_function(key);//get hashvalue in the form of an idx
+        //if buckets[i] is empty i.e. we are inserting value for the first time
         if(buckets[i] == null) buckets[i] = new LinkedList<>();
+        //if the key is not found in buckets[i] list then add it there
         if(buckets[i].indexOf(key) == -1)
             buckets[i].add(key);
     }
     
     public void remove(int key) {
-        int i = hash_function(key);
-        if(buckets[i] == null) return;
-        if(buckets[i].indexOf(key) != -1) buckets[i].remove(buckets[i].indexOf(key));
+        int i = hash_function(key);//get the corresponsing hashvalue via hashfunction
+        if(buckets[i] == null) return;//no value present so do nothing
+        //if a value is found corresponding to the key at ith index list then remove it
+        int idx = buckets[i].indexOf(key);
+        if(idx != -1) buckets[i].remove(idx);
     }
     
     /** Returns true if this set contains the specified element */
     public boolean contains(int key) {
-        int i = hash_function(key);
+        int i = hash_function(key);//get the corresponsing hashvalue via hashfunction
+        //if no list exists at ith position OR the key doesn't exist in ith position list
+        //this means our key is not present
         if(buckets[i] == null || buckets[i].indexOf(key) == -1) return false;
+        //otherwise our key is present
         return true;
     }
 }
