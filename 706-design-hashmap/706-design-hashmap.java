675c29a4b2f1c74ef6c55b7b8c20b50ce3d1da98
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-22 10:04:04 +0530

    Time: 44 ms (36.62%), Space: 50.1 MB (79.56%) - LeetHub

---
+++ 706-design-hashmap/706-design-hashmap.java
@@ -0,0 +1,28 @@
+class MyHashMap {
+    int[] map;
+
+    public MyHashMap() {
+        map = new int[1000001];//1000000 is limit of map values in constraints
+        Arrays.fill(map, -1);//initial values
+    }
+    
+    public void put(int key, int value) {
+        map[key] = value;
+    }
+    
+    public int get(int key) {
+        return map[key];
+    }
+    
+    public void remove(int key) {
+        map[key] = -1;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
