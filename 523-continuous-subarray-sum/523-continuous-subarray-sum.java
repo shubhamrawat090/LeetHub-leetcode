9173f2076ec820ffe6c5ec03dc418fcb70016eb1
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-22 18:51:29 +0530

    Time: 25 ms (72.54%), Space: 89.4 MB (70.03%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
-
         Set<Integer> set = new HashSet<>();
-        int last = 0; 
-        for (int num : nums) {
-            int cur = (last + num) % k;
-            if (set.contains(cur)) return true;
-            set.add(last); 
-            last = cur;
+        int last = 0; //previous subarr sum remainder
+        for (int num : nums) {//iterate through entire array
+            int cur = (last + num) % k;//current subarr sum remainder
+            if (set.contains(cur)) return true;//if the rem already exists return true
+            set.add(last); //add prev sub arr sum remainder
+            last = cur; //change prev to curr for next iteration
         }
+        //return false if nothing happens which means no such subarr found
         return false;
     }
 }
