9b86adb3edaa09707e973fc64df2309bbdeda348
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-22 18:42:33 +0530

    Time: 27 ms (63.08%), Space: 89.5 MB (68.92%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.java
@@ -1,23 +1,14 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>(); // (remainder -> index) map
-        // for k=0 and also if the first element is multiple of k then diff 0-(-1) is 1 and we don't take 1 element
-        map.put(0, -1); 
-        int remainder = 0;
-        for(int i=0; i<nums.length; i++){
-            remainder += nums[i];
-            //divide by k only if k is not 0
-            if(k!=0) remainder %= k;//get the remainder
-            if(map.containsKey(remainder)){
-                //if that remainder is already present meaning sum(0,i) = sum(0,j) 
-                //therefore, sum(i, j) divisible by k
-                if(i - map.get(remainder) > 1) return true; // more than 1 element
-            }else{
-                //remainder occurred for the first time, put it in map
-                map.put(remainder, i);
-            }
+
+        Set<Integer> set = new HashSet<>();
+        int last = 0; 
+        for (int num : nums) {
+            int cur = (last + num) % k;
+            if (set.contains(cur)) return true;
+            set.add(last); 
+            last = cur;
         }
-        //no subarray with sum%k == 0 found
         return false;
     }
 }
