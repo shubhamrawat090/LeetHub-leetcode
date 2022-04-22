88a9a69465afec69f11a8df99decd0d972640ac1
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-22 19:59:14 +0530

    Time: 0 ms (100.00%), Space: 67.2 MB (37.45%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.java
+++ 938-range-sum-of-bst/938-range-sum-of-bst.java
@@ -19,18 +19,23 @@ class Solution {
             return 0;
         }
         
+        //initialize sum = 0
         int sum = 0;
         
+        //add curr value to sum only if it is inside range
         if(root.val >=low && root.val <= high) {
             sum += root.val;
         }
         
+        //we can go to left only if low < root.val as there will be values to explore there
         if(low < root.val)
-            sum += rangeSumBST(root.left, low, high);
+            sum += rangeSumBST(root.left, low, high);//add the sum derived from left subtree
         
+        //we can go to right only if high > root.val as there will be values to explore there
         if(high > root.val)
-            sum += rangeSumBST(root.right, low, high);
+            sum += rangeSumBST(root.right, low, high);//add the sum derived from right subtree
         
+        //return final sum
         return sum;
     }
 }
