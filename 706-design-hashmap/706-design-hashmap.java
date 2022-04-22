9cc94f225206c557f769ff2179bd598d44f98684
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-22 10:16:44 +0530

    Time: 25 ms (68.57%), Space: 62.8 MB (46.06%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.java
+++ 706-design-hashmap/706-design-hashmap.java
@@ -1,31 +1,64 @@
 class MyHashMap {
-    int[] map;
 
+    /** Initialize your data structure here. */
+    LinkedList<Entry>[] map;
+    public static int SIZE = 769;
     public MyHashMap() {
-        map = new int[1000001];//1000000 is limit of map values in constraints
-        // filling with -1 takes O(N) time, we don't do it to make it O(1)
-        // Arrays.fill(map, -1);//initial values
+        map = new LinkedList[SIZE];
     }
-    
+
+    /** value will always be non-negative. */
     public void put(int key, int value) {
-        // map[key] = value; //this is done when Arrays.fill(map, -1) is used
-        map[key] = value+1; //add 1 to our value and store it 
+        int bucket = key % SIZE;
+        if(map[bucket] == null) {
+            map[bucket] = new LinkedList<Entry>();
+            map[bucket].add(new Entry(key, value));
+        }
+        else {
+            for(Entry entry : map[bucket]){
+                if(entry.key == key){
+                    entry.val = value;
+                    return;
+                }
+            }
+            map[bucket].add(new Entry(key, value));
+        }
     }
-    
+
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     public int get(int key) {
-        return map[key]-1; //subtract 1 as we added it in put() operation
+        int bucket = key % SIZE;
+        LinkedList<Entry> entries = map[bucket];
+        if(entries == null) return -1;
+        for(Entry entry : entries) {
+            if(entry.key == key) return entry.val;
+        }
+        return -1;
     }
-    
+
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     public void remove(int key) {
-        // map[key] = -1;//this is done when Arrays.fill(map, -1) is used
-        map[key] = 0;//just reset to 0
+        int bucket = key % SIZE;
+        Entry toRemove = null;
+        if(map[bucket] == null) return;
+        else {
+            for(Entry entry : map[bucket]){
+                if(entry.key == key) {
+                    toRemove = entry;
+                }
+            }
+            if(toRemove == null) return;
+            map[bucket].remove(toRemove);
+        }
     }
 }
 
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap obj = new MyHashMap();
- * obj.put(key,value);
- * int param_2 = obj.get(key);
- * obj.remove(key);
- */
+class Entry {
+    public int key;
+    public int val;
+
+    public Entry(int key, int val){
+        this.key = key;
+        this.val = val;
+    }
+}
