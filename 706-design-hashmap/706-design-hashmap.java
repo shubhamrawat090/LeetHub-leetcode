493fbc6218d684451499217fd37e2ce4d01d2b4c
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-22 10:06:58 +0530

    Time: 32 ms (53.08%), Space: 50 MB (80.90%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.java
+++ 706-design-hashmap/706-design-hashmap.java
@@ -3,19 +3,22 @@ class MyHashMap {
 
     public MyHashMap() {
         map = new int[1000001];//1000000 is limit of map values in constraints
-        Arrays.fill(map, -1);//initial values
+        // filling with -1 takes O(N) time, we don't do it to make it O(1)
+        // Arrays.fill(map, -1);//initial values
     }
     
     public void put(int key, int value) {
-        map[key] = value;
+        // map[key] = value; //this is done when Arrays.fill(map, -1) is used
+        map[key] = value+1; //add 1 to our value and store it 
     }
     
     public int get(int key) {
-        return map[key];
+        return map[key]-1; //subtract 1 as we added it in put() operation
     }
     
     public void remove(int key) {
-        map[key] = -1;
+        // map[key] = -1;//this is done when Arrays.fill(map, -1) is used
+        map[key] = 0;//just reset to 0
     }
 }
 
