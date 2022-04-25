04e098550bbb82fbc92e68a28d5233970e0e7825
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-25 17:52:29 +0530

    Time: 7 ms (39.41%), Space: 43.2 MB (24.35%) - LeetHub

--- 284-peeking-iterator/284-peeking-iterator.java
+++ 284-peeking-iterator/284-peeking-iterator.java
@@ -1,44 +1,36 @@
 // Java Iterator interface reference:
 // https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
 
-// Key Point's to remember :-
-
-// next() will return the value at current itearator and then increments the iterator (cache the next value)
-
-// hasNext() tells is there any element after the current iterator
-
-// peek() will only return the curr element will not increment the iterator
-
 class PeekingIterator implements Iterator<Integer> {
     
-    Iterator<Integer> iter = null;
-    Integer next = null;
-    
+    List<Integer> list;
 	public PeekingIterator(Iterator<Integer> iterator) {
 	    // initialize any member here.
-	    iter = iterator;
-        if(hasNext()) next = iter.next();
+	    list = new LinkedList<>();
+        while(iterator.hasNext()){
+            list.add(iterator.next());
+        }
 	}
 	
     // Returns the next element in the iteration without advancing the iterator.
 	public Integer peek() {
-        return next;
+        if(list.size()>0)
+            return list.get(0);
+        return -1;
 	}
 	
 	// hasNext() and next() should behave the same as in the Iterator interface.
 	// Override them if needed.
 	@Override
 	public Integer next() {
-	    Integer currVal = next;
-        if(iter.hasNext()) 
-            next = iter.next();
-        else 
-            next = null;
-        return currVal;
+        int num = -1;
+	    if(list.size()>0)
+            num = list.remove(0);
+        return num;
 	}
 	
 	@Override
 	public boolean hasNext() {
-	    return next != null || iter.hasNext();
+	    return list.size()>0;
 	}
 }
