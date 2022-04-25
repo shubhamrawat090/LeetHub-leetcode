39e24075a420cddd919fc175e93b7f2adcfde2b3
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-26 00:44:37 +0530

    Time: 3 ms (82.15%), Space: 44.8 MB (68.83%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.java
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.java
@@ -25,18 +25,24 @@ class Solution {
             return;
         }
         
+        //add root value
         res.append(root.val);
         
+        //process left
         if(root.left!=null){
+            //add left subtree inside ()
             res.append("(");
             preorder(root.left, res);
             res.append(")");
         }
         
+        //process right
         if(root.right!=null){
+            //if right exists and left doesn't then add empty() to maintain the order of child subtree
             if(root.left == null){
                 res.append("()");
             }
+            //add right subtree inside ()
             res.append("(");
             preorder(root.right, res);
             res.append(")");
