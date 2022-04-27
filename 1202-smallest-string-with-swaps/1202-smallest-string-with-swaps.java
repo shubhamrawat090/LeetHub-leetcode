f7fc34d7976263c93decc7617986f7d524d17bd6
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-27 12:32:13 +0530

    Time: 43 ms (87.81%), Space: 91 MB (91.63%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.java
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.java
@@ -1,37 +1,59 @@
-class UF {
-    public UF(int n) {
-        id = new int[n];
-        for (int i = 0; i < n; ++i){
-            id[i] = i;
+//////////////////// FIND UNION ////////////////////////////
+class Solution {
+    int[] parent;
+    int[] rank;
+    
+    public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
+        int n =s.length();
+        parent = new int[n];
+        rank = new int[n];
+        
+        for(int i = 0; i<n; i++) parent[i] = i;
+        
+        for(List<Integer> v : pairs){
+            union(v.get(0), v.get(1));  // union of all the pairs that can be swappped
+        }
+        for(int i = 0; i<n; i++){
+            parent[i] = find(parent[i]); // making sure that the connected components have a same parent in parent[i]
+        }
+        
+        HashMap<Integer, ArrayList<Character>> map = new HashMap<>();
+        
+        for(int i = 0; i<n; i++){
+            ArrayList<Character> list = map.getOrDefault(parent[i], new ArrayList<>());
+            list.add(s.charAt(i));
+            map.put(parent[i], list); // adding all the character corresponding to thier parent Index
+        }
+        
+        for(ArrayList<Character> a : map.values()){
+            Collections.sort(a, (x,y) -> y-x);  // sorting all the connected components
+        }
+        
+        char[] ch = new char[n];
+        for(int i = 0; i<n; i++){
+            ch[i] = map.get(parent[i]).remove(map.get(parent[i]).size() - 1); // making our ans
+        }
+        
+        return new String(ch);
+    }
+    
+    void union(int a, int b){
+        int x = find(a), y = find(b);
+        if(x == y) return;
+        if(rank[x] > rank[y]){
+            parent[y] = x;
+        }else if(rank[y] > rank[x]){
+            parent[x] = y;
+        }else{
+            parent[x] = y;
+            rank[y]++;
         }
     }
-
-    public void union(int u, int v) {
-        id[find(u)] = find(v);
-    }
-
-    public int find(int u) {
-        return id[u] == u ? u : (id[u] = find(id[u]));
-    }
-
-    private int[] id;
-}
-
-class Solution {
-    public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
-        StringBuilder ans = new StringBuilder();
-        UF uf = new UF(s.length());
-        Map<Integer, Queue<Character>> map = new HashMap<>();
-
-        for (List<Integer> pair : pairs)
-          uf.union(pair.get(0), pair.get(1));
-
-        for (int i = 0; i < s.length(); ++i)
-          map.computeIfAbsent(uf.find(i), k -> new PriorityQueue<>()).offer(s.charAt(i));
-
-        for (int i = 0; i < s.length(); ++i)
-          ans.append(map.get(uf.find(i)).poll());
-
-        return ans.toString();
+    
+    int find(int x){
+        if(parent[x] != x){
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
     }
 }
