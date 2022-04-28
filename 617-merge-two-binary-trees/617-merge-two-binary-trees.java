0fc602107a9e0710463d7b1ec3e8f8490f85f9af
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-28 18:38:04 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (92.49%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.java
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.java
@@ -15,18 +15,23 @@
  */
 class Solution {
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        //if tree 1 has no node then return tree 2's node
         if(root1 == null){
             return root2;
         }
-        
+        //if tree 2 has no node then return tree 1's node
         if(root2 == null){
             return root1;
         }
         
+        //make a dummy node and add both tree values sum in it's root
         TreeNode dummy = new TreeNode(root1.val + root2.val);
+        //left child will have the merged tree from left subtrees of root1 and root2
         dummy.left = mergeTrees(root1.left, root2.left);
+        //right child will have the merged tree from right subtrees of root1 and root2
         dummy.right = mergeTrees(root1.right, root2.right);
         
+        //return our dummy
         return dummy;
     }
 }
