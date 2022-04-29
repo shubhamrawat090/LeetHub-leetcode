557ab1445d22c660304cbd36b495239a5e7d9586
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-29 11:49:15 +0530

    Time: 1 ms (83.33%), Space: 42.8 MB (95.15%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,34 +1,40 @@
-//Graph Coloring - DFS
+//Graph Coloring - BFS
 // 0 = uncolored
 // 1 = blue
 // -1 = red
 
 // TIME: O(V+E), SPACE: O(V)
+
 class Solution {
     public boolean isBipartite(int[][] g) {
-        int[] colors = new int[g.length];//color for each node in graph
+        int[] colors = new int[g.length];
         for (int i = 0; i < g.length; i++)
-            //if a node is uncoloured(not connected to any other node => no way to get to that)
-            //and, we are not able to colour it
-            if (colors[i] == 0 && !validColor(g, colors, 1, i)) // initially we are coloring the node blue
-                return false;
-        //return true as no conflict in colouring found
-        return true;
-    }
-
-    //check if we can color the graph with the color we want
-    boolean validColor(int[][] g, int[] colors, int color, int node) {
-        //if it is already coloured then check if our color is same as the color of the node which means there is no conflict, otherwise there is a conflict and the graph is not bipartite
-        if (colors[node] != 0) 
-            return colors[node] == color;
+            //do the work if the node is not colored
+            if (colors[i] == 0) {
+                //Queue for each node's children
+                Queue<Integer> q = new LinkedList<>();
+                q.add(i);//add our node to it
+                colors[i] = 1;//color our node blue
+                //work while queue is empty
+                while (!q.isEmpty()) {
+                    //get our node from queue
+                    Integer node = q.poll();
+                    //get all it's children/adjacent nodes
+                    for (int adjacent : g[node])
+                        //if adjacent nodes have the same color as our node => our graph is not bipartite
+                        if (colors[adjacent] == colors[node])
+                            return false;
+                        //if adjacent nodes are not colored
+                        else if (colors[adjacent] == 0) {
+                            // add it in queue
+                            q.add(adjacent);
+                            // color it opposite to that of our node
+                            colors[adjacent] = -colors[node];
+                        }
+                }
+            }
         
-        //if it is not colored, the color it with our color
-        colors[node] = color;
-        //check all adjacent nodes and color it with the other color than our color
-        for (int adjacent : g[node])
-            //return false if coloring is not possible
-            if (!validColor(g, colors, -color, adjacent))
-                return false;
+        //return true if no violation is found and entire graph is traversed
         return true;
     }
 }
