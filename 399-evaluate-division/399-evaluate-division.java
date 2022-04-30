81f6b90fb67bd163475bf0bd2b47d04d8d999fc8
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-30 11:11:28 +0530

    Time: 2 ms (55.02%), Space: 42.3 MB (68.21%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.java
+++ 399-evaluate-division/399-evaluate-division.java
@@ -1,67 +1,53 @@
 class Solution {
-     // Using DFS
-    private  Map<String, Map<String, Double>> makeGraph(List<List<String>> e, double[] values){
-        // build a graph
-        // like a -> b = values[i]
-        // and b -> a  = 1.0 / values[i];
-        Map<String, Map<String, Double>> graph = new HashMap<>();
-        String u, v;
-        
-        for(int i = 0; i < e.size(); i++){
-            u = e.get(i).get(0);
-            v = e.get(i).get(1);
-            
-            graph.putIfAbsent(u, new HashMap<>());
-            graph.get(u).put(v, values[i]);
-            
-            graph.putIfAbsent(v, new HashMap<>());
-            graph.get(v).put(u, 1/values[i]);
-            
-        }
-        return graph;
-    }
-    
-    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        Map<String, Map<String, Double>> graph = makeGraph(equations, values);
-        
-        double []ans = new double[queries.size()];
-        
-        // check for every Querie
-        // store it in ans array;
-        for(int i = 0; i < queries.size(); i++){
-            ans[i] = dfs(queries.get(i).get(0) , queries.get(i).get(1) , new HashSet<>(), graph);
-        }
-        return ans;
-    }
-    
-    public double dfs(String src, String dest, Set<String> visited, Map<String, Map<String, Double>> graph){
-        // check the terminated Case
-        // if string is not present in graph return -1.0;
-        // like [a, e] or [x, x] :)
-        if(graph.containsKey(src ) == false)
-            return -1.0;
-        
-        // simply say check src and dest are equal :) then return dest 
-        // store it in weight varaible;
-        //case like [a,a] also handle
-        if(graph.get(src).containsKey(dest)){
-            return graph.get(src).get(dest);
-        }
-        
-        visited.add(src);
-        
-        for(Map.Entry<String, Double> nbr : graph.get(src).entrySet()){
-            if(visited.contains(nbr.getKey()) == false){
-                double weight = dfs(nbr.getKey(), dest, visited, graph);
-                
-                // if weight is not -1.0(terminate case)
-                // then mutliply it 
-                // like in querie   a -> c => 2 * 3 = 6
-                if(weight != -1.0){
-                    return nbr.getValue() * weight;
-                }
+    class Node {
+            String key;
+            double val;
+
+            public Node(String k, double v) {
+                key = k;
+                val = v;
             }
         }
-        return -1.0;
-    }
+
+        public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+            Map<String, List<Node>> g = buildGraph(equations, values);
+
+            double[] result = new double[queries.size()];
+            for (int i = 0; i < queries.size(); i++)
+                result[i] = dfs(queries.get(i).get(0), queries.get(i).get(1), new HashSet(), g);
+
+            return result;
+        }
+
+        private double dfs(String s, String d, Set<String> visited, Map<String, List<Node>> g) {
+            if (!(g.containsKey(s) && g.containsKey(d)))
+                return -1.0;
+            if (s.equals(d))
+                return 1.0;
+
+            visited.add(s);
+            for (Node ng : g.get(s)) {
+                if (!visited.contains(ng.key)) {
+                    double ans = dfs(ng.key, d, visited, g);
+                    if (ans != -1.0)
+                        return ans * ng.val;
+                }
+            }
+
+            return -1.0;
+        }
+
+        // build graph
+        private Map<String, List<Node>> buildGraph(List<List<String>> eq, double[] values) {
+            Map<String, List<Node>> g = new HashMap();
+            for (int i = 0; i < values.length; i++) {
+                String src = eq.get(i).get(0);
+                String des = eq.get(i).get(1);
+                g.putIfAbsent(src, new ArrayList());
+                g.putIfAbsent(des, new ArrayList());
+                g.get(src).add(new Node(des, values[i]));
+                g.get(des).add(new Node(src, 1 / values[i]));
+            }
+            return g;
+        }
 }
