ea5f1f449b5c621800caa35a4ad518f18bf1230e
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-30 11:20:10 +0530

    Time: 1 ms (97.61%), Space: 40.9 MB (87.09%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.java
+++ 399-evaluate-division/399-evaluate-division.java
@@ -1,53 +1,74 @@
 class Solution {
+    //helper class with dest->value(weights) pair
     class Node {
-            String key;
-            double val;
+        String key;
+        double val;
 
-            public Node(String k, double v) {
-                key = k;
-                val = v;
-            }
+        public Node(String k, double v) {
+            key = k;
+            val = v;
         }
+    }
 
-        public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-            Map<String, List<Node>> g = buildGraph(equations, values);
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        //build a graph with (src, {dest, value})
+        //we use map because graph is dynamic as there is diff wt associated to diff directions
+        Map<String, List<Node>> g = buildGraph(equations, values);
+        
+        //contains the result for each query
+        double[] result = new double[queries.size()];
+        //call dfs for each query
+        for (int i = 0; i < queries.size(); i++)
+            //dfs returns the result of each query via traversing graph from query[0] to query[1] and multiplying the values(wt.)
+            result[i] = dfs(queries.get(i).get(0), queries.get(i).get(1), new HashSet(), g);
+        
+        //return the calcualted result for each query
+        return result;
+    }
 
-            double[] result = new double[queries.size()];
-            for (int i = 0; i < queries.size(); i++)
-                result[i] = dfs(queries.get(i).get(0), queries.get(i).get(1), new HashSet(), g);
-
-            return result;
-        }
-
-        private double dfs(String s, String d, Set<String> visited, Map<String, List<Node>> g) {
-            if (!(g.containsKey(s) && g.containsKey(d)))
-                return -1.0;
-            if (s.equals(d))
-                return 1.0;
-
-            visited.add(s);
-            for (Node ng : g.get(s)) {
-                if (!visited.contains(ng.key)) {
-                    double ans = dfs(ng.key, d, visited, g);
-                    if (ans != -1.0)
-                        return ans * ng.val;
-                }
-            }
-
+    //dfs implementation
+    private double dfs(String s, String d, Set<String> visited, Map<String, List<Node>> g) {
+        //if src or dest variables are not present in the graphs then there is no way to calculate the result
+        if (!(g.containsKey(s) && g.containsKey(d)))
             return -1.0;
-        }
-
-        // build graph
-        private Map<String, List<Node>> buildGraph(List<List<String>> eq, double[] values) {
-            Map<String, List<Node>> g = new HashMap();
-            for (int i = 0; i < values.length; i++) {
-                String src = eq.get(i).get(0);
-                String des = eq.get(i).get(1);
-                g.putIfAbsent(src, new ArrayList());
-                g.putIfAbsent(des, new ArrayList());
-                g.get(src).add(new Node(des, values[i]));
-                g.get(des).add(new Node(src, 1 / values[i]));
+        //src/dest = 1 if src == dest
+        if (s.equals(d))
+            return 1.0;
+        
+        //add current node in visited
+        visited.add(s);
+        //traverse all nodes from the src
+        for (Node ng : g.get(s)) {
+            //go to the node only if not visited already
+            if (!visited.contains(ng.key)) {
+                //find the ans from the neighbour node
+                double ans = dfs(ng.key, d, visited, g);
+                //multiply the ans with our node's answer only if it isn't -1 and return it
+                if (ans != -1.0)
+                    return ans * ng.val;
             }
-            return g;
         }
+        //return -1 if you can't find the result
+        return -1.0;
+    }
+
+    // build graph
+    private Map<String, List<Node>> buildGraph(List<List<String>> eq, double[] values) {
+        Map<String, List<Node>> g = new HashMap();
+        for (int i = 0; i < values.length; i++) {
+            //in equations list, get src = variable1 and dest = variable2
+            String src = eq.get(i).get(0);
+            String des = eq.get(i).get(1);
+            //if src or dest is not available then initialize it
+            g.putIfAbsent(src, new ArrayList());
+            g.putIfAbsent(des, new ArrayList());
+            //find the variable src/dest and ad the corresponding values to it like
+            // src->dest = val , dest->src = 1/val
+            // add those as nodes in graph
+            g.get(src).add(new Node(des, values[i]));
+            g.get(des).add(new Node(src, 1 / values[i]));
+        }
+        //return the graph built
+        return g;
+    }
 }
