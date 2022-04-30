f795afc95513e6ee549c1cf396bc954c904d86ca
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-04-30 14:01:33 +0530

    Time: 1 ms (81.16%), Space: 46.8 MB (63.74%) - LeetHub

--- 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.java
+++ 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.java
@@ -29,18 +29,25 @@ class Solution {
     }
     
     public TreeNode constructBST(ListNode head){
+        //return null if no nodes exist
         if(head == null)
             return null;
         
+        //find middle of list
         ListNode mid = mid(head);
+        //store middle of list's value in BST nodes
         TreeNode node = new TreeNode(mid.val);
         
+        //mid == head, only 1 node exists
         if(mid == head)
             return node;
         
+        //on the left of that node, join BST of left half of linked list
         node.left = constructBST(head);
+        //on the right of that node, join BST of right half of linked list
         node.right = constructBST(mid.next);
 
+        //return the BST
         return node;
     }
     
