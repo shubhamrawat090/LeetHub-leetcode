8bf5eaeac74436fe33644f14ad0d5672922700c2
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-02 01:08:45 +0530

    Time: 21 ms (28.57%), Space: 71.4 MB (71.43%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.java
+++ 2256-minimum-average-difference/2256-minimum-average-difference.java
@@ -1,36 +1,20 @@
 class Solution {
+    // Calculate the sum first and then minus the left sum in te for loop.
+
     public int minimumAverageDifference(int[] nums) {
-        long sum = 0;
-        long totalSum = 0;
-        long min = Integer.MAX_VALUE;
-        int n = nums.length;
-        for(int i=0; i<n; i++){
-            totalSum += nums[i];
+        int len = nums.length, res = 0;
+        long min = Integer.MAX_VALUE, sum = 0, leftSum = 0, rightSum = 0;
+        for (int num : nums)
+            sum += num;
+        for (int i = 0; i < len; i++) {
+            leftSum += nums[i];
+            rightSum = sum - leftSum;
+            long diff = Math.abs(leftSum / (i + 1) - (len - i == 1 ? 0 : rightSum / (len -i - 1)));
+            if (diff < min) {
+                min = diff;
+                res = i;
+            }
         }
-        int avgIdx = -1;
-        
-        for(int i=0; i<n; i++){
-            sum += nums[i];
-            long avg = sum/(i+1);
-            
-            totalSum -= nums[i];
-            
-            long avg2 = 0;
-            if(i == n-1){
-                avg2 = 0;
-            }
-            else {
-                avg2 = totalSum/(n-i-1);
-            }
-            
-            long absDiff = Math.abs(avg - avg2);
-            if(absDiff < min){
-                min = absDiff;
-                avgIdx = i;
-            }
-            
-        }
-        
-        return avgIdx;
+        return res;
     }
 }
