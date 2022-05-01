7811eb66eb9aa745556d24704dbf2bf9187229a9
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-02 01:49:29 +0530

    Time: 3 ms (10.96%), Space: 45.2 MB (5.97%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,101 @@
+class Solution {
+    //flag = 0 -> buy
+    //flag = 1 -> sell
+    
+    //recursive function => will give TLE
+    public int rec(int idx, int flag, int[] arr){
+        //if we buy at last and don't sell then our profit id -ve, 
+        //since we have to maximize our profit, we send 0 as it is the least positive profit
+        if(idx == arr.length) {
+            return 0;
+        }
+        
+        if(flag == 0) {
+                                            //BUY
+            //buying at this point => arr[idx] is spent by us, therefore, -arr[idx]
+            //flag is changed to sell => 1
+            int buyHere = -arr[idx] + rec(idx+1, 1, arr);
+            //not buying at this point => 0 is spent by us, therefore, subtract nothing
+            //flag remains buy => 0
+            int notBuyHere = rec(idx+1, 0, arr);
+            //returning the maximum of both the scenarios
+            return Math.max(buyHere, notBuyHere);
+        } else {
+                                            //SELL
+            //selling at this point => arr[idx] is gained by us, therefore, +arr[idx]
+            //flag is changed to buy => 0
+            int sellHere = arr[idx] + rec(idx+1, 0, arr);
+            //not buying at this point => 0 is gained by us, therefore, add nothing
+            //flag remains sell => 1
+            int notSellHere = rec(idx+1, 1, arr);
+            //returning the maximum of both the scenarios
+            return Math.max(sellHere, notSellHere);
+        }
+    }
+    
+    //memoized function => using 2D DP => still gives TLE
+    int[][] dp;
+    public int memo(int idx, int flag, int[] arr){
+        //if we buy at last and don't sell then our profit id -ve, 
+        //since we have to maximize our profit, we send 0 as it is the least positive profit
+        if(idx == arr.length) {
+            return dp[idx][flag] = 0;
+        }
+        
+        if(dp[idx][flag] != 0) {
+            return dp[idx][flag];
+        }
+        
+        if(flag == 0) {
+                                            //BUY
+            //buying at this point => arr[idx] is spent by us, therefore, -arr[idx]
+            //flag is changed to sell => 1
+            int buyHere = -arr[idx] + memo(idx+1, 1, arr);
+            //not buying at this point => 0 is spent by us, therefore, subtract nothing
+            //flag remains buy => 0
+            int notBuyHere = memo(idx+1, 0, arr);
+            //returning the maximum of both the scenarios
+            return dp[idx][flag] = Math.max(buyHere, notBuyHere);
+        } else {
+                                            //SELL
+            //selling at this point => arr[idx] is gained by us, therefore, +arr[idx]
+            //flag is changed to buy => 0
+            int sellHere = arr[idx] + memo(idx+1, 0, arr);
+            //not buying at this point => 0 is gained by us, therefore, add nothing
+            //flag remains sell => 1
+            int notSellHere = memo(idx+1, 1, arr);
+            //returning the maximum of both the scenarios
+            return dp[idx][flag] = Math.max(sellHere, notSellHere);
+        }
+    }
+    
+    //driver function
+    public int maxProfit(int[] arr) {
+        int n = arr.length;
+        // return rec(0, 0, arr);//gives TLE
+        dp = new int[arr.length+1][2];
+        // return memo(0, 0, arr); //still gives TLE
+        
+        //tabulation
+        for(int idx = n; idx >= 0; idx--) {
+            for(int flag = 0; flag < 2; flag ++) {
+                if(idx == n) {
+                    dp[idx][flag] = 0;
+                    continue;
+                }
+
+                if(flag == 0) {//buy
+                    int buyHere = -arr[idx] + dp[idx+1][1];
+                    int notBuyHere = dp[idx+1][0];
+                    dp[idx][flag] = Math.max(buyHere, notBuyHere);
+                } else {//sell
+                    int sellHere = arr[idx] + dp[idx+1][0];
+                    int notSellHere = dp[idx+1][1];
+                    dp[idx][flag] = Math.max(sellHere, notSellHere);
+                }
+            }
+        }
+        
+        return dp[0][0];
+    }
+}
