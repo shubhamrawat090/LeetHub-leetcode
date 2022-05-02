27d8c4a75be4a2733b81e6c55cc266d601c998df
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-02 11:17:25 +0530

    Time: 3 ms (79.48%), Space: 43.9 MB (94.79%) - LeetHub

--- 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.java
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.java
@@ -1,7 +1,9 @@
 class Solution {
     public int[] sortArrayByParityII(int[] nums) {
+        //even odd store separately in arrays
         int[] even = new int[nums.length/2];
         int[] odd = new int[nums.length/2];
+        //even ptr and odd ptr 
         int e=0, o=0;
         for(int num: nums) {
             if(num%2 == 0) {
@@ -12,13 +14,18 @@ class Solution {
                 o++;
             }
         }
+        //reset even and odd ptr
         e=0;
         o=0;
+        //traverse the arr
         for(int i=0; i<nums.length; i++) {
+            //if element is even then add value from even arr
             if(i%2 == 0) {
                 nums[i] = even[e];
                 e++;
-            }else {
+            }
+            //if element is odd then add value from odd arr
+            else {
                 nums[i] = odd[o];
                 o++;
             }
