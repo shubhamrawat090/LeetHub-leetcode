44f3d1fdc28f533d6e800951412aa9f19e1b2ae3
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-02 10:32:19 +0530

    Time: 2 ms (38.15%), Space: 48.4 MB (44.44%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.java
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -1,19 +1,21 @@
-// Using partition of an array method
+// Using 2 pointer method
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int left = 0, right = 0;
+        int left = 0, right = nums.length-1;
         
-        while(right < nums.length) {
-            if(nums[right] % 2 != 0) {
-                right++;
+        while(left < right) {
+            if(nums[left] % 2 != 0) {
+                if(nums[right] % 2 == 0) {
+                    //swap left and right
+                    int temp = nums[left];
+                    nums[left] = nums[right];
+                    nums[right] = temp;
+                    
+                    left++;
+                }
+                right--;
             }else {
-                //swap left and right
-                int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
-                
                 left++;
-                right++;
             }
         }
         
