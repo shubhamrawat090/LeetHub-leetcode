e4c82c36478d5503c9ce30000144256116c1ac65
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-03 07:32:12 +0530

    Time: 3 ms (49.15%), Space: 55 MB (7.27%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
@@ -1,25 +1,23 @@
+// Iterate the nums array, find the first occurance that the nums[i] < max, then mark the starting index = i - 1 and end index = i. If nums[i] < max happens again, extend the end index. If nums[start -1] > nums[end], we need to decrease the starting index to find the real starting point. The starting point decreases at most n times. O(2n) is O(n).
+
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
-        Integer start = null, end = null;
+        Integer start = null;
+        Integer end = null;
         int max = Integer.MIN_VALUE;
-        
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i] >= max) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= max) {
                 max = nums[i];
-            } else{
-                if(start == null) {
+            } else {
+                if (start == null) {
                     start = i - 1;
                     end = i;
-                }else {
+                } else {
                     end = i;
                 }
-                
-                while(start > 0 && nums[start - 1] > nums[end]){
-                    start--;
-                }
+                while (start > 0 && nums[end] < nums[start - 1]) start--;
             }
         }
-        
-        return start == null? 0: end - start + 1;
+        return start == null ? 0 : end - start + 1; 
     }
 }
