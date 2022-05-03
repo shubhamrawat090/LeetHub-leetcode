a5f8af5e0354a3914c7b5afa9d22f0245ad9ce33
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-03 07:33:21 +0530

    Time: 1 ms (98.93%), Space: 43.5 MB (78.18%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
@@ -1,25 +1,36 @@
+// Using binary search to find starting point. In worst case, the start - 1 is the target, every time we almost find O(lgn) time. So it's slower than previous solution in worst case scenario. In best case scenario, the starting point is always in the middle of [0, start], and it is faster than previous solution's O(2n).
+
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
-        Integer start = null, end = null;
+        Integer start = null;
+        Integer end = null;
         int max = Integer.MIN_VALUE;
-        
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i] >= max) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= max) {
                 max = nums[i];
-            } else{
-                if(start == null) {
+            } else {
+                if (start == null) {
                     start = i - 1;
                     end = i;
-                }else {
+                } else {
                     end = i;
                 }
-                
-                while(start > 0 && nums[start - 1] > nums[end]){
-                    start--;
-                }
+                if (start > 0 && nums[end] < nums[start - 1]) {
+                    start = binarySearch(nums, -1, start, nums[end]);
+                }                
             }
         }
-        
-        return start == null? 0: end - start + 1;
+        return start == null ? 0 : end - start + 1; 
+    }
+    private int binarySearch(int[] nums, int l, int r, int val) {
+        while (l + 1 < r) {
+            int m = (l + r) >> 1;
+            if (nums[m] <= val) {
+                l = m;
+            } else {
+                r = m;
+            }
+        }
+        return r;
     }
 }
