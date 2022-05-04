9c1ec669039116bfd177c4458aefdc5db7f4200c
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-04 07:26:59 +0530

    Time: 21 ms (87.79%), Space: 57.9 MB (74.05%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,21 +1,34 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        HashMap<Integer,Integer>map=new HashMap<>();
-        
-        int count=0;
-        
-        for(int i=0; i<nums.length ;i++){
-            //to check if that k-nums[i] present and had some value left or already paired
-            if(map.containsKey(k-nums[i]) && map.get(k-nums[i]) > 0){
-                count++;
-                map.put(k-nums[i], map.get(k-nums[i]) - 1);
-            }else{
-                //getOrDefault is easy way it directly checks if value is 0 returns 0 where I added 1
-                //and if some value is present then it return that value "similar to map.get(i)" and I added 1 on it 
-                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        int n = nums.length;
+        if (n == 1) return 0;
+        Arrays.parallelSort(nums);
+        int l = -1;
+        int r = n;
+        // binary search to find right boundary
+        while (l + 1 < r) {
+            int m = l + ((r - l) >> 1);
+            if (nums[m] < k) {
+                l = m;
+            } else {
+                r = m;
+            } 
+        } 
+        r = l;
+        l = 0;
+        int total = 0;
+        while (l < r) {
+            int sum = nums[l] + nums[r];
+            if (sum > k) {
+                r--;
+            } else if (sum < k) {
+                l++;
+            } else {
+                total++;
+                l++;
+                r--;
             }
         }
-        
-        return count;
+        return total;
     }
 }
