3fee036aa9381dedac60dc4066d467118c6cc92e
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-04 07:16:50 +0530

    Time: 51 ms (39.12%), Space: 91.2 MB (13.74%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,26 +1,19 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        //sort array
-        Arrays.sort(nums);
-        //start and end pointers
-        int i=0, j=nums.length-1;
-        int count = 0; // count no. of steps
-        //till start and end ptrs meet or cross each other
-        while(i<j) {
-            //if we get a sum of k then count a step and move start and end ptrs
-            if(nums[i] + nums[j] == k) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        
+        int count=0;
+        
+        for(int i=0; i<nums.length ;i++){
+            //to check if that k-nums[i] present and had some value left or already paired
+            if(map.containsKey(k-nums[i]) && map.get(k-nums[i]) > 0){
                 count++;
-                i++;
-                j--;
+                map.put(k-nums[i], map.get(k-nums[i]) - 1);
+            }else{
+                //getOrDefault is easy way it directly checks if value is 0 returns 0 where I added 1
+                //and if some value is present then it return that value "similar to map.get(i)" and I added 1 on it 
+                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
             }
-            //if sum > k, move the end ptr as we need a smaller value
-            else if(nums[i] + nums[j] > k) {
-                j--;
-            }
-            //if sum < k, move the start ptr as we need a bigger value
-            else {
-                i++;
-            } 
         }
         
         return count;
