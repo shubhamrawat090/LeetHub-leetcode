77e4eb51c56a06a713d8d33452b722a2fee597e8
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-04 07:08:26 +0530

    Time: 22 ms (83.97%), Space: 69.2 MB (62.98%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,16 +1,24 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
+        //sort array
         Arrays.sort(nums);
+        //start and end pointers
         int i=0, j=nums.length-1;
-        int count = 0;
+        int count = 0; // count no. of steps
+        //till start and end ptrs meet or cross each other
         while(i<j) {
+            //if we get a sum of k then count a step and move start and end ptrs
             if(nums[i] + nums[j] == k) {
                 count++;
                 i++;
                 j--;
-            }else if(nums[i] + nums[j] > k) {
+            }
+            //if sum > k, move the end ptr as we need a smaller value
+            else if(nums[i] + nums[j] > k) {
                 j--;
-            }else {
+            }
+            //if sum < k, move the start ptr as we need a bigger value
+            else {
                 i++;
             } 
         }
