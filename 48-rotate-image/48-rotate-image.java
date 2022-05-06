adb3db27798ec1a55ef5986205e16a36683dffc8
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-06 11:23:53 +0530

    Time: 1 ms (6.55%), Space: 43.3 MB (9.45%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -5,6 +5,9 @@ class Solution {
         
         int row = 0, col = n-1;
         
+        //take first row of original matrix and put it in last col of temp matrix;
+        //row++; col--;
+        //keep doing above 2 steps till row<n || col>=0
         while(row<n || col>=0) {
             for(int i=0; i<n; i++) {
                 tempMatrix[i][col] = matrix[row][i];
@@ -14,6 +17,7 @@ class Solution {
             col--;
         }
         
+        //copy contents of temp matrix into original matrix
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
                 matrix[i][j] = tempMatrix[i][j];
