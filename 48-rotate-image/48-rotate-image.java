37fcf5d6be1186fa92218abbbec36a9cc503c641
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-06 11:32:32 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (20.80%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,26 +1,26 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-        int[][] tempMatrix = new int[n][n];
         
-        int row = 0, col = n-1;
-        
-        //take first row of original matrix and put it in last col of temp matrix;
-        //row++; col--;
-        //keep doing above 2 steps till row<n || col>=0
-        while(row<n || col>=0) {
-            for(int i=0; i<n; i++) {
-                tempMatrix[i][col] = matrix[row][i];
+        //transpose of matrix
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<=i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
-            
-            row++;
-            col--;
         }
         
-        //copy contents of temp matrix into original matrix
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                matrix[i][j] = tempMatrix[i][j];
+        //reverse each row
+        for(int[] row: matrix) {
+            int start = 0, end = n-1;
+            while(start<end) {
+                int temp = row[start];
+                row[start] = row[end];
+                row[end] = temp;
+                
+                start++;
+                end--;
             }
         }
     }
