4009efe394a71bdf0137df25e7b76d94a1c61002
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-06 12:49:36 +0530

    Time: 1 ms (33.18%), Space: 42.3 MB (78.75%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,27 +1,35 @@
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        //iterators for nums1, nums2 and mergedArr(nums1 starting from end)
-        int itr1 = m-1, itr2 = n-1, i=m+n-1;
-        
-        //iterate from back till either nums1(m->0) or nums2(n->0) is fully traversed
-        while(itr1>=0 && itr2>=0) {
-            //if nums1 has greater value then add its value to nums1[i] and decrement itr1 and i
-            if(nums1[itr1] > nums2[itr2]) {
-                nums1[i--] = nums1[itr1--];
-            }
-            //otherwise add its value to nums1[i] and increment itr2 and i
-            else {
-                nums1[i--] = nums2[itr2--];
-            }
+    public void merge(int[] arr1, int n, int[] arr2, int m) {
+        int k;
+        if(m == 0) {
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+          // take first element from arr1 
+          // compare it with first element of second array
+          // if condition match, then swap
+          if (arr1[i] > arr2[0]) {
+            int temp = arr1[i];
+            arr1[i] = arr2[0];
+            arr2[0] = temp;
+          }
+
+          // then sort the second array
+          // put the element in its correct position
+          // so that next cycle can swap elements correctly
+          int first = arr2[0];
+          // insertion sort is used here
+          for (k = 1; k < m && arr2[k] < first; k++) {
+            arr2[k - 1] = arr2[k];
+          }
+          arr2[k - 1] = first;
         }
         
-        //if there is any value left in nums1, add it to nums1[i]
-        while(itr1>=0) {
-            nums1[i--] = nums1[itr1--];
+        int itr1 = n;
+        for(int j=0; j<m; j++) {
+            arr1[itr1] = arr2[j];
+            itr1++;
         }
-        //if there is any value left in nums2, add it to nums1[i]
-        while(itr2>=0) {
-            nums1[i--] = nums2[itr2--];
-        }
+        
     }
 }
