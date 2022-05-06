251b60599daf18aa8e86534c75bf76a2d7af4aba
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-06 11:58:35 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (88.16%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,35 +1,27 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        //merged Arr of size m+n
-        // int[] mergedArr = new int[m+n];
-        
-        //iterators for nums1, nums2 and mergedArr
+        //iterators for nums1, nums2 and mergedArr(nums1 starting from end)
         int itr1 = m-1, itr2 = n-1, i=m+n-1;
         
-        //iterate till either nums1 or nums2 is fully traversed
+        //iterate from back till either nums1(m->0) or nums2(n->0) is fully traversed
         while(itr1>=0 && itr2>=0) {
-            //if nums1 has smaller value then add its value to merged arr and increment itr1 and i
+            //if nums1 has greater value then add its value to nums1[i] and decrement itr1 and i
             if(nums1[itr1] > nums2[itr2]) {
                 nums1[i--] = nums1[itr1--];
             }
-            //otherwise add its value to merged arr and increment itr2 and i
+            //otherwise add its value to nums1[i] and increment itr2 and i
             else {
                 nums1[i--] = nums2[itr2--];
             }
         }
         
-        //if there is any value left in nums1, add it to mergedArr
+        //if there is any value left in nums1, add it to nums1[i]
         while(itr1>=0) {
             nums1[i--] = nums1[itr1--];
         }
-        //if there is any value left in nums2, add it to mergedArr
+        //if there is any value left in nums2, add it to nums1[i]
         while(itr2>=0) {
             nums1[i--] = nums2[itr2--];
         }
-        
-        //add the merged arr values in nums1, as per required by the question
-        // for(int j=0; j<m+n; j++) {
-        //     nums1[j] = mergedArr[j];
-        // }
     }
 }
