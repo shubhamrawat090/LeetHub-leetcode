25125bd92e884f035de54ea7342ba2eabef44953
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-06 11:21:50 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (68.21%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -2,27 +2,18 @@ class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
         int[][] tempMatrix = new int[n][n];
-        //transpose matrix and store in temp matrix
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                tempMatrix[j][i] = matrix[i][j];
+        
+        int row = 0, col = n-1;
+        
+        while(row<n) {
+            for(int i=0; i<n; i++) {
+                tempMatrix[i][col] = matrix[row][i];
             }
+            
+            row++;
+            col--;
         }
         
-        //reverse each row of transposed matrix
-        for(int[] arr: tempMatrix) {
-            int start=0, end=n-1;
-            while(start < end) {
-                int temp = arr[start];
-                arr[start] = arr[end];
-                arr[end] = temp;
-                
-                start++;
-                end--;
-            }
-        }
-        
-        //copy temp matrix -> which is rotated by 90 degrees to our main matrix
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
                 matrix[i][j] = tempMatrix[i][j];
