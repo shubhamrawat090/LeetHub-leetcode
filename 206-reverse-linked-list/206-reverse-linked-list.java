3fc247e9970febb89f216007668d6b4d3b9085dc
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-08 16:02:50 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (59.01%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -14,14 +14,9 @@ class Solution {
         if(head == null || head.next == null) {
             return head;
         }
-        ListNode tail = head;
-        //getting tail ptr at last of linked list
-        while(tail.next!=null) {
-            tail = tail.next;
-        }
-        //take curr ptr and prev ptr for each node
-        //point curr to prev, reversing the links of the node from next ptr to prev ptr
+        
         ListNode curr = head, prev = null;
+        
         while(curr!=null) {
             //storing the ahead LL so that we don't lose the LL ahead while reversing the link of curr ptr
             ListNode ahead = curr.next;
@@ -33,11 +28,6 @@ class Solution {
             curr = ahead;
         }
         
-        //finally reversing the head and tail ptr
-        ListNode temp = head;
-        head = tail;
-        tail = temp;
-        
-        return head;
+        return prev;
     }
 }
