4d387422a8c6db67852479066c2a6486a15b656d
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-09 07:06:54 +0530

    Time: 6 ms (35.51%), Space: 41.9 MB (75.31%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,63 +1,39 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        //return empty list if digits string is empty
         if(digits.isEmpty()) return new ArrayList<>();
         
-        //return the answer from helper function
         return helper("", digits);
     }
     
-    //recursive function contains two arguments: processed & unprocessed
-    //initially processed is empty and unprocessed is the entire string(digits)
-    private List<String> helper(String p, String up){
-        
-        //when unprocessed is empty, there is an answer in processed
-        //just add it in a list and return it
-        if(up.isEmpty()){
-            List<String> list = new ArrayList<>();
-            list.add(p);
-            return list;
+    public List<String> helper(String p, String up) {
+        if(up.isEmpty()) {
+            List<String> base = new ArrayList<>();
+            base.add(p);
+            return base;
         }
         
-        //gets the first character of unprocessed and converts it to equivalent digit
-        //like '2' to 2
-        int digit = up.charAt(0) - '0';
+        int firstNum = up.charAt(0) - '0';
         
-        //list for storing all the answers
-        List<String> list = new ArrayList<>();
+        int startIdx = (firstNum - 2) * 3, lastIdx = (firstNum - 1) * 3;
         
-        //starting & ending indices for function calls, used to get the corresponding character to the digit
-        int startIdx = (digit-2)*3, endIdx = (digit-1)*3;
-        
-        //7, 8, 9 have slight alterations as 7 and 9 contains 4 characters each
-        //this changes the range for 7,8,9 as mentioned below
-        if(digit == 7){
-            endIdx++;
-        }else if(digit == 8){
+        if(firstNum == 7) {
+            lastIdx++;
+        }else if(firstNum == 8) {
             startIdx++;
-            endIdx++;
-        }else if(digit == 9){
+            lastIdx++;
+        }else if(firstNum == 9) {
             startIdx++;
-            endIdx+=2;
+            lastIdx+=2;
         }
         
-        //e.g. for digit=6 loop runs from 
-        //i = 12((6-2)*3) to i < 15((6-1)*3), with changes for digit = 7,8,9
-        for(int i=startIdx; i<endIdx && i<=25; i++){
-            //last alphabet 'z'='a'+25
-            //i=26,27... out of range of alphabets (a-z) so skip i>25
+        List<String> ans = new ArrayList<>();
+        
+        for(int i=startIdx; i<lastIdx && i<26; i++) {
+            char ch = (char)('a' + i);
             
-            //'a'+0 = 'a'
-            //'a'+1 = 'b'
-            //and so on till 'a'+25='z'
-             char ch = (char)('a'+i);
-            
-            //add all the answers coming from recursive calls to our list
-            //add ch to processed, and remove 1 character from unprocessed as it is processed now
-            list.addAll(helper(p+ch, up.substring(1)));
+            ans.addAll(helper(p+ch, up.substring(1)));
         }
         
-        //return final list
-        return list;
+        return ans;
     }
 }
