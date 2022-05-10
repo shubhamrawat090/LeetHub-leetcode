76f2b8c63a266e658d7b1938763b890bf0635905
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-10 23:14:50 +0530

    Time: 53 ms (7.43%), Space: 76 MB (37.87%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,29 +1,12 @@
 class Solution {
-    public int findDuplicate(int[] nums) {
-        //slow ptr moves 1 step while fast ptr moves 2 steps at a time,
-        //if they meet then there is a cycle
-        //the starting point of that cycle is the no. which is repeating
-        int slow = nums[0], fast = nums[0];
-        
-        //DETECTING CYCLE -> use floyd's cycle detection
-        while(true){
-            slow = nums[slow]; //moves by 1 step
-            fast = nums[nums[fast]]; //moves by 2 steps
-            
-            if(slow == fast){//cycle detected
-                break;
+    public int findDuplicate(int[] arr) {
+        int n = arr.length;
+        Arrays.sort(arr);
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                return arr[i];
             }
         }
-        
-        //DETECTING STARTING POINT OF THE CYCLE
-        //reset slow or fast anyone and then move both by 1 step, wherever they meet is the starting point of the cycle
-        slow = nums[0];
-        
-        while(slow!=fast){
-            slow = nums[slow];
-            fast = nums[fast];
-        }
-        
-        return slow; // return fast; //both are pointing at starting pt of cycle
+        return 0;
     }
 }
