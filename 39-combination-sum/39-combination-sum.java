9b60e1efa30d9bb58954b07fb922151cd9b83b8a
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-10 12:37:56 +0530

    Time: 3 ms (91.12%), Space: 44.1 MB (70.05%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,24 +1,22 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        
-        helper(target, candidates, res, new LinkedList(), 0);
-        return res;
-    }
-    
-    public void helper(int target, int[] candidates, List<List<Integer>> res, LinkedList list, int idx) {
-        if(idx == candidates.length) {
-            if(target == 0) {
-                res.add(new ArrayList(list));
+    private void findCombinations(int ind, int[] arr, int target, List < List < Integer >> ans, List < Integer > ds) {
+        if (ind == arr.length) {
+            if (target == 0) {
+                ans.add(new ArrayList < > (ds));
             }
             return;
         }
 
-        if(candidates[idx] <= target) {
-            list.add(candidates[idx]);
-            helper(target-candidates[idx], candidates, res, list, idx);
-            list.removeLast();
+        if (arr[ind] <= target) {
+            ds.add(arr[ind]);
+            findCombinations(ind, arr, target - arr[ind], ans, ds);
+            ds.remove(ds.size() - 1);
         }
-        helper(target, candidates, res, list, idx+1);
+        findCombinations(ind + 1, arr, target, ans, ds);
+    }
+    public List < List < Integer >> combinationSum(int[] candidates, int target) {
+        List < List < Integer >> ans = new ArrayList < > ();
+        findCombinations(0, candidates, target, ans, new ArrayList < > ());
+        return ans;
     }
 }
