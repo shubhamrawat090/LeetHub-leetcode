f0d2ef79925b0b2f2b6222defb65dd67b646d8c5
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-11 11:41:18 +0530

    Time: 233 ms (5.95%), Space: 41 MB (35.01%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -1,6 +1,39 @@
-// mathematics solution
+// Recursive: Time = O(5^n)
 class Solution {
+    //vowel array
+    char[] vowel = new char[]{'a','e','i','o','u'};
+    
     public int countVowelStrings(int n) {
-        return (n+4)*(n+3)*(n+2)*(n+1) / 24;
+        int ans = 0;
+        
+        //we start iterating from back(considering our c to be last character of a string of length n) 
+        //and we count all possible combinations corresponding to each character in the vowel arr
+        for(char c: vowel) {
+            ans += count(n-1, c);
+        }
+        
+        return ans;
+    }
+    
+    //recursive function
+    private int count(int length, char lastChar) {
+        if(length == 0) {
+            //when length becomes 0, it means it is the 1 and only string that we need
+            return 1;
+        }
+        
+        //store combinations from the curr characters with lastChar >= curr char
+        int temp = 0;
+        
+        for(char c: vowel) {
+            //ensure lexicographical order
+            if(lastChar>=c) {
+                //current char is parsed, now consider for length-1 string remaining
+                //our curr char becomes lastChar now
+                temp += count(length-1, c);
+            }
+        }
+        
+        return temp;
     }
 }
