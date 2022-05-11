d67eed3d8a6ff937993145af85fee2a86cd33b1e
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-11 12:04:05 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (15.68%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -1,7 +1,50 @@
+// Recursive: Time = O(5^n)
 class Solution {
-    //no. of combinations of length n with k values with repetitions = (n + k - 1)!/((k-1)!*(n)!)
-    //put n, k=5(5 vowels) => (n+4)!/(4!*n!) --> further simplified to (n+4)*(n+3)*(n+2)*(n+1) / 24
+    //considering a=0, e=1, i=2, o=4, u=5
+    //vowel arr
+    char[] vowel = new char[]{'a','e','i','o','u'};
+    
     public int countVowelStrings(int n) {
-        return (n+4)*(n+3)*(n+2)*(n+1) / 24;
+        int ans = 0;
+        
+        //we start iterating from back(considering our c to be last character of a string of length n) 
+        //and we count all possible combinations corresponding to each character in the vowel arr
+        int[][] dp = new int[n+1][5];
+        
+        for(int[] row: dp) {
+            Arrays.fill(row,-1);
+        }
+        
+        for(int i=0; i<5; i++) {
+            ans += memo(n-1, i, dp);
+        }
+        
+        return ans;
+    }
+    
+    //memoized function
+    private int memo(int length, int lastChar, int[][] dp) {
+        if(length == 0) {
+            //when length becomes 0, it means it is the 1 and only string that we need
+            return dp[length][lastChar]  = 1;
+        }
+        
+        if(dp[length][lastChar] != -1) {
+            return dp[length][lastChar];
+        }
+        
+        //store combinations from the curr characters with lastChar >= curr char
+        int temp = 0;
+        
+        for(int i=0; i<5; i++) {
+            //ensure lexicographical order
+            if(vowel[lastChar]>=vowel[i]) {
+                //current char is parsed, now consider for length-1 string remaining
+                //our curr char becomes lastChar now
+                temp += memo(length-1, i, dp);
+            }
+        }
+        
+        return dp[length][lastChar] = temp;
     }
 }
