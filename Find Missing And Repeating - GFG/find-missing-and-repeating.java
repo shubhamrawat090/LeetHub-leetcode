9df0b3cc854928fdc425911f4e5f6b769fd58cc4
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-11 13:12:48 +0530

    Added solution - LeetHub

--- Find Missing And Repeating - GFG/find-missing-and-repeating.java
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.java
@@ -30,23 +30,64 @@ class GFG {
 
 class Solve {
     int[] findTwoElement(int arr[], int n) {
-        // code here
-        int[] freq = new int[n+1];
-        for(int i=0; i<n; i++) {
-            freq[arr[i]]++;
+        /* Will hold xor of all elements and numbers from 1 to n */
+        int xor1;
+    
+        /* Will have only single set bit of xor1 */
+        int set_bit_no;
+    
+        int i;
+        int x = 0; // missing
+        int y = 0; // repeated
+    
+        xor1 = arr[0];
+    
+        /* Get the xor of all array elements */
+        for (i = 1; i < n; i++)
+            xor1 = xor1 ^ arr[i];
+    
+        /* XOR the previous result with numbers from 1 to n */
+        for (i = 1; i <= n; i++)
+            xor1 = xor1 ^ i;
+    
+        /* Get the rightmost set bit in set_bit_no */
+        set_bit_no = xor1 & ~(xor1 - 1);
+    
+        /* Now divide elements into two sets by comparing a rightmost set bit
+           of xor1 with the bit at the same position in each element.
+           Also, get XORs of two sets. The two XORs are the output elements.
+           The following two for loops serve the purpose */
+    
+        for (i = 0; i < n; i++) {
+            if ((arr[i] & set_bit_no) == 0)
+                /* arr[i] belongs to first set */
+                x = x ^ arr[i];
+    
+            else
+                /* arr[i] belongs to second set */
+                y = y ^ arr[i];
+        }
+    
+        for (i = 1; i <= n; i++) {
+            if ((i & set_bit_no) == 0)
+                /* i belongs to first set */
+                x = x ^ i;
+    
+            else
+                /* i belongs to second set */
+                y = y ^ i;
+        }
+    
+        // NB! numbers can be swapped, maybe do a check ?
+        int x_count = 0;
+        for (int j=0; j<n; j++) {
+            if (arr[j]==x)
+                x_count++;
         }
         
-        int repeat = 0, missing = 0;
-        for(int i=1; i<=n; i++) {
-            if(freq[i] > 1) {
-                repeat = i;
-            }
-            
-            if(freq[i] == 0) {
-                missing = i;
-            }
-        }
+        if (x_count==0)
+            return new int[]{y, x};
         
-        return new int[]{repeat, missing};
+        return new int[]{x, y};
     }
 }
