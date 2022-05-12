6a3c14ede729e3b296ebc6e46e2d230bd7f34031
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-12 12:13:58 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (83.44%) - LeetHub

---
+++ 46-permutations/46-permutations.java
@@ -0,0 +1,39 @@
+class Solution {
+    // We will use BACKTRACKING 
+    //               1 2 3   swap 2 & 3 => 1 3 2
+    // swap 1 & 2 => 2 1 3   swap 1 & 3 => 2 3 1
+    // swap 1 & 3 => 3 1 2   swap 1 & 2 => 3 2 1
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        
+        helper(res, nums, 0);
+        
+        return res;
+    }
+    
+    private void helper(List<List<Integer>> res, int[] nums, int idx) {
+        if(idx == nums.length) {
+            List<Integer> list = new ArrayList<>();
+            for(int num: nums) {
+                list.add(num);
+            }
+            res.add(list);
+            return;
+        }
+        
+        for(int i=idx; i<nums.length; i++) {
+            //swap curr idx and ith value via traversing the arr
+            swap(nums, i, idx);
+            //call for next idx
+            helper(res, nums, idx+1);
+            //backtrack
+            swap(nums, i, idx);
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
