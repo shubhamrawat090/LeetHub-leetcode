169dd3459cf8a55d16eff6bab4239d1168632888
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-12 21:16:53 +0530

    Time: 1 ms (100.00%), Space: 42.9 MB (82.45%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.java
+++ 113-path-sum-ii/113-path-sum-ii.java
@@ -18,6 +18,7 @@ class Solution {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> path = new ArrayList<>();
         
+        //dfs helper function
         helper(root, targetSum, res, path);
         
         return res;
@@ -25,18 +26,20 @@ class Solution {
     
     private void helper(TreeNode root, int target, List<List<Integer>> res, List<Integer> path) {
         if(root == null) return;
-        
+        //add root to path
         path.add(root.val);
             
-        if(root.left == null && root.right == null) {
-            if(root.val == target) {
-                res.add(new ArrayList<>(path));
-            }
-        } else {
+        //if root is leaf node and its value is target left then add that path to resultant
+        if(root.left == null && root.right == null && root.val == target) {
+            res.add(new ArrayList<>(path));
+        } 
+        //work for left and right subtrees
+        else {
             helper(root.left, target-root.val, res, path);
             helper(root.right, target-root.val, res, path);
         }
         
+        //backtrack
         path.remove(path.size()-1);
     }
 }
