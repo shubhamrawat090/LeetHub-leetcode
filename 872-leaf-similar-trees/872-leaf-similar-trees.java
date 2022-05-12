edc1bcc54a3d11b2313e3e4d0e0de6d558f8731a
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-12 20:38:55 +0530

    Time: 1 ms (52.80%), Space: 42.2 MB (33.74%) - LeetHub

--- 872-leaf-similar-trees/872-leaf-similar-trees.java
+++ 872-leaf-similar-trees/872-leaf-similar-trees.java
@@ -14,35 +14,20 @@
  * }
  */
 class Solution {
-    // Time O(N) Space O(N)
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        List<Integer> list1 = new ArrayList<>();
-        getList(root1, list1);
-        
-        List<Integer> list2 = new ArrayList<>();
-        getList(root2, list2);
-        
-        if(list1.size() == list2.size()) {
-            for(int i=0; i<list1.size(); i++) {
-                if(list1.get(i) != list2.get(i)) {
-                    return false;
-                }
-            }
-            
-            return true;
-        }
-        
-        return false;
+        List<Integer> leaves1 = new ArrayList();
+        List<Integer> leaves2 = new ArrayList();
+        dfs(root1, leaves1);
+        dfs(root2, leaves2);
+        return leaves1.equals(leaves2);
     }
-    
-    private void getList(TreeNode root, List<Integer> list) {
-        if(root == null) return;
-        
-        if(root.left == null && root.right == null) {
-            list.add(root.val);
+
+    public void dfs(TreeNode node, List<Integer> leafValues) {
+        if (node != null) {
+            if (node.left == null && node.right == null)
+                leafValues.add(node.val);
+            dfs(node.left, leafValues);
+            dfs(node.right, leafValues);
         }
-        
-        getList(root.left, list);
-        getList(root.right, list);
     }
 }
