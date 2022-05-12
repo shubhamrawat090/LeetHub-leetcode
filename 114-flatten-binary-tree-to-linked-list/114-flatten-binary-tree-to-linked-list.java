fc0c7e0e6dbfac0c0079ed1c523e8dca1f752498
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-12 21:36:52 +0530

    Time: 3 ms (13.55%), Space: 42.1 MB (70.97%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -1,46 +1,21 @@
 class Solution {
     public void flatten(TreeNode root) {
-        if (root == null) {
-            return;
-        }
-        traverse0(root);
-    }
-    
-    // return end of "linked-list"
-    private TreeNode traverse0(TreeNode root) {
-        if (root == null) {
-            return null;
-        }
+        Stack<TreeNode> stack = new Stack();
+        TreeNode curr = root;
         
-        //storing left and right subtree in temporary nodes
-        TreeNode left = root.left;
-        TreeNode right = root.right;
-        
-        //make left side null
-        root.left = null;
-        
-        //go to end of left subtree and flatten it
-        TreeNode leftEnd = traverse0(left);
-        
-        //if there is end of left node
-        //add left tree to right-side of root
-        //to the end of left subtree add right subtree
-        if (leftEnd != null) {
-            root.right = left;
-            leftEnd.right = right;
-        }
-        
-        //go to right subtree and flatten it
-        TreeNode rightEnd = traverse0(right);
-        
-        //if rightEnd doesn't exist
-        if (rightEnd == null) {
-            //if left tree's end == null, return the tree otherwise return the leftEnd
-            return leftEnd == null ? root : leftEnd;
-        } 
-        //if rightEnd exists return it
-        else {
-            return rightEnd;
+        while( curr != null ) {
+            
+            // Backup the right node in stack
+            if( curr.right != null ) 
+                stack.push( curr.right );
+            
+            /* If there is a left node of current node, make it right node, else
+            take the right node from the stack's top */
+            curr.right = curr.left == null && !stack.isEmpty() ? stack.pop() : curr.left; 
+            
+            curr.left = null;
+            
+            curr = curr.right;
         }
     }
 }
