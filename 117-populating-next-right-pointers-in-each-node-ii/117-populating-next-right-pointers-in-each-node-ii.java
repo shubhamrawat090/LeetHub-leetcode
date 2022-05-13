55f37055717ef1cf3dba4c88e4b3ad7a6b05a428
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-13 07:31:19 +0530

    Time: 0 ms (100.00%), Space: 44.7 MB (56.27%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -20,29 +20,33 @@ class Node {
     }
 };
 */
-
+// DFS-> Processing right node first
 class Solution {
-    //BFS LEVEL ORDER
     public Node connect(Node root) {
-        if(root == null) return null;
-        Queue<Node> q = new ArrayDeque<>();
-        q.add(root);
-        
-        while(q.size()!=0) {
-            int size = q.size();
-            for(int i=0; i<size; i++) {
-                Node curr = q.remove();
-                if(i == size-1) {
-                    curr.next = null;
-                }else {
-                    curr.next = q.peek();
-                }
-
-                if(curr.left!=null) q.add(curr.left);
-                if(curr.right!=null) q.add(curr.right);  
-            }
-            
+        if (root == null || (root.left == null && root.right == null) ) {
+            return root;
         }
+        
+        // connect left child to right child
+        if (root.left != null && root.right != null) {
+            root.left.next = root.right;
+        }
+        
+        // connect right child (or left if right is abasent) to child of next nodes.
+        Node lNode = (root.right == null) ? root.left : root.right;
+        
+        Node next = root.next;
+        while (next != null && next.left == null && next.right == null) {
+            next = next.next;
+        }
+
+        if (next != null) {
+            lNode.next = (next.left != null) ? next.left : next.right;
+        }
+        
+        // process right child first
+        connect(root.right);
+        connect(root.left);
         
         return root;
     }
