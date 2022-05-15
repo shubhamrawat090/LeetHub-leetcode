7020ab7bb35550475a2995edb987e57e0cc222f3
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-15 21:47:02 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (8.50%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.java
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.java
@@ -14,20 +14,26 @@
  * }
  */
 class Solution {
+    //stores total sum of all the values required
     int totalSum = 0;
     public int sumNumbers(TreeNode root) {
+        //dfs function traversing tree
         helper(root, 0);
         return totalSum;
     }
     
+    //dfs helper function
     private void helper(TreeNode root, int sum) {
+        //don't do anything if node doesn't exist
         if(root == null) return;
         
+        //when leaf node reached
         if(root.left == null && root.right == null) {
+            //convert the sum into number
             totalSum += (sum*10) + root.val;
             return;
         }
-        
+        //previous value * 10 + curr val = number you want
         sum = sum*10;
         helper(root.left, sum + root.val);
         helper(root.right, sum + root.val);
