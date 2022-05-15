4f85fc19a38540859934c8e25503803a6cd81039
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-15 10:37:37 +0530

    Time: 1 ms (100.00%), Space: 44.7 MB (88.87%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -14,39 +14,31 @@
  * }
  */
 class Solution {
-    public int deepestLeavesSum(TreeNode root) {
-        //find maximum height
-        int maxHt = height(root);
-        //add node values which have height = maxHt
-        return sumHelper(root, maxHt);
+    private int deepestSum = 0;
+    private int deepestLevel = 0;
+
+    public int deepestLeavesSum(TreeNode root) { 
+        traverse(root, 0);
+        return deepestSum;
     }
-    
-    //maxHeight
-    private int height(TreeNode root) {
+
+    private void traverse(TreeNode root, int level) {
         if(root == null) {
-            return 0;
+            return;
         }
-        
-        int leftHt = height(root.left);
-        int rightHt = height(root.right);
-        
-        return 1 + Math.max(leftHt, rightHt);
-    }
-    
-    //sum finder
-    private int sumHelper(TreeNode root, int maxHt) {
-        if(root == null) {
-            return 0;
+
+        if(root.left == null && root.right == null) {
+
+            if(this.deepestLevel == level) {
+                this.deepestSum += root.val;
+            }
+            else if(this.deepestLevel < level){
+                this.deepestSum = root.val;
+                this.deepestLevel = level;
+            } 
         }
-        
-        int sum = 0;
-        if(root.left == null && root.right == null && maxHt == 1) {
-            sum += root.val;
-        }
-        
-        sum += sumHelper(root.left, maxHt-1);
-        sum += sumHelper(root.right, maxHt-1);
-        
-        return sum;
+
+        traverse(root.left, level + 1);
+        traverse(root.right, level + 1);
     }
 }
