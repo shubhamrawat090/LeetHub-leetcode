4e421856368c78876ade3cecd3883b0bc9c765ef
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-16 23:05:01 +0530

    Time: 3 ms (10.79%), Space: 43.5 MB (54.78%) - LeetHub

--- 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.java
+++ 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.java
@@ -18,38 +18,36 @@ class Solution {
         //no tree exist
         if(root == null) return new ArrayList<>();
         
-        //store all level nodes as different arraylists
         List<List<Integer>> ans = new ArrayList<>();
         
-        //helper queue 
-        Queue<TreeNode> q = new ArrayDeque<>();
-        
-        //add root to queue
+        Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
+        q.add(null);
+        
+        Stack<List<Integer>> stk = new Stack<>();
+        List<Integer> levels = new ArrayList<>();
         
         while(q.size()>0){
-            //q size can vary when we are removing inside the loop so we don't know how many times the for loop runs which is why we store it in a variable count
-            int count = q.size();
-            //list storing nodes at a particular level
-            List<Integer> levels = new ArrayList<>();
             
-            //all count(size of queue after pushing nodes) no. of nodes are nodes at a particular level
-            for(int i=0; i<count; i++){
-                //take curr node from queue
-                TreeNode curr = q.remove();
-                //add its value in levels list which contains contains values at a particular level and pop its value from queue
+            int count = q.size();
+            
+            TreeNode curr = q.remove();
+            
+            if(curr != null) {
                 levels.add(curr.val);
-                //add left and right nodes in queue if they exist
                 if(curr.left!=null) q.add(curr.left);
                 if(curr.right!=null) q.add(curr.right);
+            }else {
+                if(q.size()>0) q.add(null);
+                stk.push(levels);
+                levels = new ArrayList<>();
             }
-            
-            //add this level to ans list
-            ans.add(levels);
         }
         
-        //reverse the entire list to get the final ans in reverse order as asked
-        Collections.reverse(ans);
+        while(stk.size()>0) {
+            ans.add(stk.pop());
+        }
+        
         //return list
         return ans;
     }
