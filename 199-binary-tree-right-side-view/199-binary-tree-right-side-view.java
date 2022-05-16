ddb4a8efd624517ba457ca8d9232ba91caeb9796
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-16 23:10:22 +0530

    Time: 2 ms (35.86%), Space: 42.3 MB (65.39%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -14,23 +14,35 @@
  * }
  */
 class Solution {
-    List<Integer> res = new ArrayList<>();
-    int maxDepth = -1;
     public List<Integer> rightSideView(TreeNode root) {
-        if (root == null) return res;
-        helper(root, 0);
-        return res;
-    }
-    public void helper(TreeNode node, int depth) {
-        if (node == null) {
-            return;
+        List<Integer> ans = new ArrayList<>();
+        if(root == null) {
+            return ans;
         }
-        if (depth > maxDepth) {
-            res.add(node.val);
+        
+        Queue<TreeNode> q = new ArrayDeque<>();
+        
+        q.add(root);
+        
+        while(q.size()!=0){
+            int count = q.size();
+            
+            for(int i=0; i<count; i++){
+                TreeNode curr = q.remove();
+                
+                if(i==count-1) {
+                    ans.add(curr.val);
+                }
+                
+                if(curr.left!=null){
+                    q.add(curr.left);
+                }
+                if(curr.right!=null){
+                    q.add(curr.right);
+                }
+            }
         }
-        maxDepth = Math.max(maxDepth, depth);
-        helper(node.right, depth+1);
-        helper(node.left, depth+1);
-
+        
+        return ans;
     }
 }
