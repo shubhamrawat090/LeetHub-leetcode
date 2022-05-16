721a7af2188884cacc4019eb6de7a638375586dc
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-16 13:09:02 +0530

    Time: 22 ms (64.75%), Space: 60.8 MB (32.46%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,54 +1,34 @@
 class Solution {
-    private class Pair {
-        int row;
-        int col;
-        int path;
-        
-        Pair(int r, int c, int p) {
-            row = r;
-            col = c;
-            path = p;
-        }
-    }
-    
     public int shortestPathBinaryMatrix(int[][] grid) {
-        //you cannot enter the grid
-        if(grid[0][0] == 1) {
-            return -1;
-        }
+        if(grid[0][0] == 1) return -1;
+        int m = grid.length;
+        int n = grid[0].length;
         
-        //Run BFS on paths where you can go then you will get the shortest path
-        Queue<Pair> q = new ArrayDeque<>();
-        q.add(new Pair(0,0,1));
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0,0,1});
+        grid[0][0] =1;
         
-        //All 8 direction calls
         int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
-                 
-        //Level wise BFS
-        while(q.size()!=0) {
+        while(!q.isEmpty()) {
             int size = q.size();
-            while(size-->0) {
-                Pair rPair = q.remove();
-                //if current block is the destination block then return the path calculated
-                if(rPair.row == grid.length-1 && rPair.col == grid[0].length-1) {
-                    return rPair.path;
-                }
+            while(size-- > 0) {
+                int[] point = q.poll();
                 
-                //calling in all 8 directions
-                for(int[] arr: dir) {
-                    //next row, col to go to
-                    int r = rPair.row + arr[0];
-                    int c = rPair.col + arr[1];
+                if(point[0] ==  m-1 && point[1] == n-1)
+                    return point[2];
+                
+                for(int[] d : dir) {
+                    int r = point[0] + d[0];
+                    int c = point[1] + d[1];
                     
-                    //if this next r,c is within the range of grid and you are allowed to go to this r,c
-                    if(r>=0 && r<grid.length && c>=0 && c<grid[0].length && grid[r][c] == 0) {
-                        q.add(new Pair(r,c,rPair.path+1));
-                        grid[r][c] = 1;//mark this row as visited
+                    if(r >= 0 && c >= 0 && r < m && c < n && grid[r][c] == 0){
+                        q.add(new int[]{r, c, point[2]+1});
+                        grid[r][c] = 1;
                     }
                 }
             }
         }
-        //no minimum path found              
+        
         return -1;
     }
 }
