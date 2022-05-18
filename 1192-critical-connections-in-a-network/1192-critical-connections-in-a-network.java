30cd063f1056e36dce0ce344a4d9c26b622bc65e
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-18 13:13:59 +0530

    Time: 159 ms (69.09%), Space: 266 MB (67.79%) - LeetHub

---
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
@@ -0,0 +1,68 @@
+// Intuition -
+// An edge (u,v) will be a critical connection, if to reach node v in the network we've only node u, i.e. if we start visiting nodes from a source vertex 0 using DFS, and keep assigning discovery time to each time, while checking if it can be visited earlier from any alternate route. If it's possible to visit a node earlier from different route, than this node cannot be a bridge/critical connection.
+
+// If it's node possible to visit a node v earlier from a different route than the current parent node u, then the edge (u,v) is critical connection.
+
+class Solution {
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        // Prepare the adjaceny list from the given edges 
+        ArrayList<ArrayList<Integer>> adjList = createAdjacenyList(n, connections);
+        
+        // Stores critical edges/answer
+        List<List<Integer>> ans = new ArrayList();
+        
+        // Stores discovery time and minimum discovery time of each node in the graph
+        int[] dis = new int[n];
+        int[] minDis = new int[n];
+        
+        // Running timer, starts from 1.
+        int[] timer = new int[] { 1 };
+        
+        // DFS from 0th node, since there is no parent, set parent to -1.
+        dfs(0, -1, adjList, dis, minDis, timer, ans);
+        
+        return ans;
+    }
+    
+    private ArrayList<ArrayList<Integer>> createAdjacenyList(int n, List<List<Integer>> edges) {
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList();
+        
+        for(int i=0; i<n; i++)
+            adjList.add( new ArrayList() );
+        
+        for(List<Integer> edge : edges) {
+            int u = edge.get(0);
+            int v = edge.get(1);
+            adjList.get( u ).add( v );
+            adjList.get( v ).add( u );
+        }
+        
+        return adjList;
+    }
+    
+    private void dfs(int node, int parent, ArrayList<ArrayList<Integer>> adjList, int[] dis, int[] minDis, 
+                     int[] timer, List<List<Integer>> ans) {
+
+        // Set minimum discovery time and discovery time to current time
+        minDis[node] = dis[node] = timer[0]++;
+        
+        for(int adj : adjList.get(node)) {
+            // Skip if the adjacent node is the parent
+            if( adj == parent ) 
+                continue;
+            
+            // If adjacent node is not yet visited, DFS on the adjacent node
+            if( dis[adj] == 0 ) 
+                dfs(adj, node, adjList, dis, minDis, timer, ans);
+            
+            /* If adjacent node can be discovered early than the current node, 
+            than update the minimum discovery time of current node as well */
+            minDis[node] = Math.min(minDis[node], minDis[adj]);
+        }
+        
+        /* If current node can only be disovered from the parent node, than the
+        parent to current node's edge is critical edge (bridge) */
+        if( parent != -1 && minDis[node] > dis[parent] ) 
+            ans.add( new ArrayList() {{ add(parent); add(node); }});
+    }
+}
