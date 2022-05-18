64893751b854ce3de1bb0437be4ff28c5c85f8cb
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-18 19:22:50 +0530

    Time: 3 ms (87.88%), Space: 43.2 MB (34.45%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
@@ -14,6 +14,7 @@
  * }
  */
 class Solution {
+    //Pair class stores data like node, width(vertical line), depth(horizontal levels)
     class Pair implements Comparable<Pair> {
         TreeNode node;
         int w;
@@ -25,67 +26,84 @@ class Solution {
             this.d = d;
         }
 
-        // this - other means increasing
-        // other - this means decreasing
+        // COMPARATOR defined so that 
+        // 1. ArrayList<Pair> can be sorted using Arrays.sort() 
+        // 2. PriorityQueue<Pair> can sort and store data
+        // (this - other) means increasing
+        // (other - this) means decreasing
         public int compareTo(Pair other){
+            //if current level is same then compare according to the node's value
             if(this.d == other.d){
                 return this.node.val - other.node.val;
-            } else {
+            } 
+            //otherwise, compare current level
+            else {
                 return this.d - other.d;
             }
         }
     }
 
     public List<List<Integer>> verticalTraversal(TreeNode root) {
+        //Using Level Order BFS
         Queue<Pair> queue = new LinkedList<>();
+        //Add Root with width = 0, depth = 1
         queue.add(new Pair(root, 0, 1));
 
         int lmw = 0;
         int rmw = 0;
         
+        //Mapping width -> Pair Info of node
         HashMap<Integer, ArrayList<Pair>> map = new HashMap<>();
         while(queue.size() > 0){
-            Pair temp = queue.remove();
+            //removing current node
+            Pair currNode = queue.remove();
 
-            if(temp.w < lmw){
-                lmw = temp.w;
-            }
+            //Finding left most and right most width extreme
+            lmw = Math.min(lmw, currNode.w);
+            rmw = Math.max(rmw, currNode.w);
 
-            if(temp.w > rmw){
-                rmw = temp.w;
-            }
-
-            if(map.containsKey(temp.w) == false){
-                map.put(temp.w, new ArrayList<>());
-                map.get(temp.w).add(temp);
+            //Mapping the values and storing the info
+            if(map.containsKey(currNode.w) == false){
+                //if the key(width) is not present in the Map
+                map.put(currNode.w, new ArrayList<>());//create a new list at that index
+                map.get(currNode.w).add(currNode);//add currNode at that key
             } else {
-                map.get(temp.w).add(temp); 
+                //if the key(width) is already present in the Map
+                map.get(currNode.w).add(currNode);//add currNode at that key
             }
 
-            if(temp.node.left != null){
-                queue.add(new Pair(temp.node.left, temp.w - 1, temp.d + 1));
+            //Add left and right nodes(if they exist) to the Queue
+            if(currNode.node.left != null){
+                //going left => width-1, level+1
+                queue.add(new Pair(currNode.node.left, currNode.w - 1, currNode.d + 1));
             }
 
-            if(temp.node.right != null){
-                queue.add(new Pair(temp.node.right, temp.w + 1, temp.d + 1));
+            if(currNode.node.right != null){
+                //going right => width+1, level+1
+                queue.add(new Pair(currNode.node.right, currNode.w + 1, currNode.d + 1));
             }
         }
 
+        //Traverse the HashMap from lmw->rmw and storing the values in result
         List<List<Integer>> res = new ArrayList<>();
         for(int w = lmw; w <= rmw; w++){
+            //List storing the sorted values of nodes at that particular vertical level
             ArrayList<Integer> list = new ArrayList<>();
+            //Get the list(which is not sorted) from map
             ArrayList<Pair> unsortedList = map.get(w);
+            //Sort the list based on the conditioned mentioned in compareTo() method of Pair class
             Collections.sort(unsortedList);
 
+            //take individual Pair and store it's value inside list
             for(Pair temp: unsortedList){
                 list.add(temp.node.val);
             }
 
-
+            //Add curr vertical list to resultant list
             res.add(list);
         }
         
-
+        //Return the resultant
         return res;
     }
 }
