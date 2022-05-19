fa498fea8fbd2ef394694542d1d9b19c0970b45d
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-19 17:08:09 +0530

    Time: 9 ms (87.01%), Space: 42.4 MB (92.82%) - LeetHub

---
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
@@ -0,0 +1,64 @@
+class Solution {
+    int[][] dir = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+    public int longestIncreasingPath(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int longestPath = 0;
+        int[][] dp = new int[m][n];
+        
+        //RECURSIVE
+        //take all points in matrix as SOURCE and find all the paths and return the maximum from it
+        // for(int i=0; i<m; i++) {
+        //     for(int j=0; j<n; j++) {
+        //         int path = dfs(matrix, m, n, i, j);
+        //         longestPath = Math.max(longestPath, path);
+        //     }
+        // }
+        
+        //MEMOIZED
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                int path = memo(matrix, m, n, i, j, dp);
+                longestPath = Math.max(longestPath, path);
+            }
+        }
+        
+        return longestPath;
+    }
+    
+    //Recursive code: gives TLE
+//     public int dfs(int[][] matrix, int m, int n, int r, int c) {
+//         int maxPath = 0;
+            
+//         for(int[] d: dir) {
+//             int newRow = r + d[0];
+//             int newCol = c + d[1];
+            
+//             if(newRow>=0 && newRow<m && newCol>=0 && newCol<n && matrix[r][c]<matrix[newRow][newCol]) {
+//                 maxPath = Math.max(maxPath, dfs(matrix, m, n, newRow, newCol));
+//             }
+//         }
+        
+//         return maxPath+1;
+//     }
+    
+    //MEMOIZED
+    public int memo(int[][] matrix, int m, int n, int r, int c, int[][] dp) {
+        if(dp[r][c] != 0) {
+            return dp[r][c];
+        }
+        
+        int maxPath = 0;
+            
+        for(int[] d: dir) {
+            int newRow = r + d[0];
+            int newCol = c + d[1];
+            
+            if(newRow>=0 && newRow<m && newCol>=0 && newCol<n && matrix[r][c]<matrix[newRow][newCol]) {
+                maxPath = Math.max(maxPath, memo(matrix, m, n, newRow, newCol, dp));
+            }
+        }
+        
+        return dp[r][c] = maxPath+1;
+    }
+    
+}
