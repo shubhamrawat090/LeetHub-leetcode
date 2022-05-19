3d7bf374a1ab35dfe105aff3800da90e06307b32
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-19 23:30:15 +0530

    Time: 3 ms (64.43%), Space: 47 MB (58.37%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -14,56 +14,30 @@
  * }
  */
 class Solution {
+    TreeNode prev=null, first=null, second=null;
     public void recoverTree(TreeNode curr) {
         if(curr == null)
             return;
         
-        TreeNode prev=null, first=null, second=null;
-        
-        //work till curr exists
-        while(curr != null) {
-            //if left child doesn't exist
-            if(curr.left==null) {
-                if(prev != null && prev.val>curr.val) {
-                    if(first == null){
-                        first = prev;
-                    }
-                    second = curr;
-                }
-                //print curr and go to right
-                prev = curr;
-                curr = curr.right;
-            } 
-            //if left child exists
-            else {
-                TreeNode iop = curr.left;
-                while(iop.right!=null && iop.right!=curr) {
-                    iop = iop.right;
-                }
-                //left is unprocessed
-                if(iop.right == null) {
-                    //make thread and go to left
-                    iop.right = curr;
-                    curr = curr.left;
-                } 
-                //left is processed
-                else {
-                    if(prev != null && prev.val>curr.val) {
-                        if(first == null){
-                            first = prev;
-                        }
-                        second = curr;
-                    }
-                    //break thread, print and go to right
-                    iop.right = null;
-                    prev = curr;
-                    curr = curr.right;
-                }
-            }
-        }
+        inorder(curr);
         
         int temp = first.val;
         first.val = second.val;
         second.val = temp;
+    }
+    
+    void inorder(TreeNode curr) {
+        if(curr == null) return;
+        
+        inorder(curr.left);
+        if(prev!=null) {
+            if(prev.val>curr.val) {
+                if(first==null) first = prev;
+                second = curr;
+            }
+        }
+        
+        prev = curr;
+        inorder(curr.right);
     }
 }
