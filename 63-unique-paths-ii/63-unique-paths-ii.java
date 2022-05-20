7a1ad418881e3ff3cd5d9654adbd2f4a72267ed1
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-20 08:53:59 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (35.66%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -5,28 +5,27 @@ class Solution {
 
         int[][] dp  = new int[n][m];
 
-        if(grid[0][0] == 0)dp[0][0] = 1;
-        else dp[0][0] = 0;
+        if(grid[0][0] == 0)
+            dp[0][0] = 1;
+        else 
+            dp[0][0] = 0;
+        
         for(int i = 1 ; i < n ; i++){
-
-            if(grid[i][0] == 1)continue;
+            if(grid[i][0] == 1)
+                continue;
             dp[i][0] = dp[i-1][0];
-
         }
 
         for(int i = 1 ; i < m ; i++){
-
-            if(grid[0][i] == 1) continue;    
+            if(grid[0][i] == 1) 
+                continue;    
             dp[0][i] = dp[0][i-1];
-
         }
 
         for(int i = 1 ; i < n ; i++){
-
             for(int j = 1 ; j < m ; j++){
-
-                if(grid[i][j] == 1) continue;
-
+                if(grid[i][j] == 1) 
+                    continue;
                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
