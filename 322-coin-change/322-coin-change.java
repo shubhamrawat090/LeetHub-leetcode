de1eff22dbc7f9e73dfc9d7ca2ec93787af326f1
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-21 12:47:40 +0530

    Time: 19 ms (77.35%), Space: 41.9 MB (90.07%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -3,8 +3,41 @@ class Solution {
         int[] dp = new int[amount+1];
         Arrays.fill(dp, -1);
         
-        int minCoins = memo(coins.length, coins, amount, dp);
+        // int minCoins = memo(coins.length, coins, amount, dp);
+        // return minCoins!=Integer.MAX_VALUE? minCoins: -1;
+        
+        //tabular
+        int minCoins = tabular(coins.length, coins, amount, dp);
         return minCoins!=Integer.MAX_VALUE? minCoins: -1;
+    }
+    
+    //tabular
+    private int tabular(int n, int[] coins, int amount, int[] dp) {
+        
+        for(int a=0;  a<=amount; a++) {
+            if(a == 0) {
+                dp[a] = 0;
+                continue;
+            }
+
+            int ans = Integer.MAX_VALUE;
+
+            for(int coin: coins) {
+                if(a - coin >= 0) {
+                    int subAns = 0;
+                    
+                    subAns = dp[a - coin];
+
+                    if(subAns != Integer.MAX_VALUE && subAns+1<ans) {
+                        ans = subAns+1;
+                    }
+                }
+            }
+
+            dp[a] = ans;
+        }
+        
+        return dp[amount];
     }
     
     //memoized
