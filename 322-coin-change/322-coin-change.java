ac49f5af8c7d4cd314ee901fb2bb50dc61b4611a
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-21 12:53:04 +0530

    Time: 22 ms (66.46%), Space: 45.6 MB (30.32%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,87 +1,15 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount+1];
-        Arrays.fill(dp, -1);
-        
-        // int minCoins = memo(coins.length, coins, amount, dp);
-        // return minCoins!=Integer.MAX_VALUE? minCoins: -1;
-        
-        //tabular
-        int minCoins = tabular(coins.length, coins, amount, dp);
-        return minCoins!=Integer.MAX_VALUE? minCoins: -1;
-    }
-    
-    //tabular
-    private int tabular(int n, int[] coins, int amount, int[] dp) {
-        
-        for(int a=0;  a<=amount; a++) {
-            if(a == 0) {
-                dp[a] = 0;
-                continue;
-            }
-
-            int ans = Integer.MAX_VALUE;
-
-            for(int coin: coins) {
-                if(a - coin >= 0) {
-                    int subAns = 0;
-                    
-                    subAns = dp[a - coin];
-
-                    if(subAns != Integer.MAX_VALUE && subAns+1<ans) {
-                        ans = subAns+1;
-                    }
-                }
-            }
-
-            dp[a] = ans;
-        }
-        
-        return dp[amount];
-    }
-    
-    //memoized
-    private int memo(int n, int[] coins, int amount, int[] dp) {
-        if(amount == 0) return dp[amount] = 0;
-        
-        int ans = Integer.MAX_VALUE;
+        Arrays.fill(dp, amount+1);
+        dp[0] = 0;
         
         for(int coin: coins) {
-            if(amount - coin >= 0) {
-                int subAns = 0;
-                if(dp[amount-coin] != -1) {
-                    subAns = dp[amount-coin];
-                }
-                else {
-                    subAns = memo(n, coins, amount - coin, dp);
-                }
-                
-                if(subAns != Integer.MAX_VALUE && subAns+1<ans) {
-                    ans = subAns+1;
-                }
+            for(int i=coin; i<=amount; i++) {
+                dp[i] = Math.min(dp[i], dp[i-coin]+1);
             }
         }
         
-        return dp[amount] = ans;
-    }
-    
-    //recursive
-    private int rec(int n, int[] coins, int amount) {
-        if(amount == 0) return 0;
-        
-        int ans = Integer.MAX_VALUE;
-        
-        for(int coin: coins) {
-            if(amount - coin >= 0) {
-                int subAns = 0;
-                subAns = rec(n, coins, amount - coin);
-                
-                if(subAns != Integer.MAX_VALUE && subAns+1<ans) {
-                    ans = subAns+1;
-                }
-            }
-        }
-        
-        return ans;
+        return dp[amount] <= amount ? dp[amount] : -1;
     }
 }
