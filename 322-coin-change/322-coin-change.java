a25bc9f6083b8df62ce0fc707369ab9f22c2b310
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-21 12:55:09 +0530

    Time: 20 ms (73.65%), Space: 44.4 MB (70.46%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,15 +1,20 @@
+// Bottom-Up DP Tabulation
+// Time: O(coins * amount), Space: O(amount)
+
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount+1];
-        Arrays.fill(dp, amount+1);
-        dp[0] = 0;
+        Arrays.fill(dp, amount+1);//fill with any val > amount
+        dp[0] = 0; //as from 0 coins we can make 0 amount
         
+        //iterate for each coin
         for(int coin: coins) {
             for(int i=coin; i<=amount; i++) {
                 dp[i] = Math.min(dp[i], dp[i-coin]+1);
             }
         }
         
+        //return -1 is dp[amount] is unchanged
         return dp[amount] <= amount ? dp[amount] : -1;
     }
 }
