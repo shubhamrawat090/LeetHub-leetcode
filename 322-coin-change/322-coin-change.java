8de5c0149c545c61fb414f357252317586c9f34f
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-21 12:40:27 +0530

    Time: 45 ms (26.10%), Space: 44.7 MB (67.41%) - LeetHub

---
+++ 322-coin-change/322-coin-change.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, -1);
+        
+        int minCoins = memo(coins.length, coins, amount, dp);
+        return minCoins!=Integer.MAX_VALUE? minCoins: -1;
+    }
+    
+    private int memo(int n, int[] coins, int amount, int[] dp) {
+        if(amount == 0) return 0;
+        
+        int ans = Integer.MAX_VALUE;
+        
+        for(int coin: coins) {
+            if(amount - coin >= 0) {
+                int subAns = 0;
+                if(dp[amount-coin] != -1) {
+                    subAns = dp[amount-coin];
+                }
+                else {
+                    subAns = memo(n, coins, amount - coin, dp);
+                }
+                
+                if(subAns != Integer.MAX_VALUE && subAns+1<ans) {
+                    ans = subAns+1;
+                }
+            }
+        }
+        
+        return dp[amount] = ans;
+    }
+    
+    //recursive
+    private int rec(int n, int[] coins, int amount) {
+        if(amount == 0) return 0;
+        
+        int ans = Integer.MAX_VALUE;
+        
+        for(int coin: coins) {
+            if(amount - coin >= 0) {
+                int subAns = 0;
+                subAns = rec(n, coins, amount - coin);
+                
+                if(subAns != Integer.MAX_VALUE && subAns+1<ans) {
+                    ans = subAns+1;
+                }
+            }
+        }
+        
+        return ans;
+    }
+}
