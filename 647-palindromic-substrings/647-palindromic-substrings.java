06cfd80786606e18a07c55f63a2899c41fee536b
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-22 15:33:20 +0530

    Time: 20 ms (28.17%), Space: 44.5 MB (22.93%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,32 +1,39 @@
-// Brute Force: Time O(N^3), Space O(1)
+// 2d-DP
+// Time O(N^2), Space O(N^2)
 class Solution {
     public int countSubstrings(String s) {
-        int count=0;
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        int count = 0;
         
-        for(int i=0; i<s.length(); i++) {
-           for(int j=i+1; j<=s.length(); j++) {
-               String ss = s.substring(i,j);
-               
-               if(isPalindrome(ss)) count++;
-           }
+        for(int gap=0; gap<s.length(); gap++) {
+            for(int i=0, j=gap; j<dp[0].length; i++, j++) {
+                if(gap == 0) {
+                    //startIdx == endIdx
+                    //1 length is always palindrome
+                    dp[i][j] = true;
+                } else if(gap == 1) {
+                    //startIdx+1 == endIdx
+                    //2 length str is palindrome ONLY if both chars are same
+                    if(s.charAt(i)==s.charAt(j)) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = false;
+                    }
+                } else {
+                    //endIdx-startIdx>1
+                    //3 or more length str is palindrome ONLY if startChar = endChar AND (start+1 to end-1) substring is palindrome
+                    if(s.charAt(i)==s.charAt(j) && dp[i+1][j-1] == true) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = false;
+                    }
+                }
+                
+                //if current substring from (i to j) is palindrome then count it
+                if(dp[i][j] == true) count++;
+            }
         }
         
         return count;
-    }
-    
-    private boolean isPalindrome(String s) {
-        int i=0, j=s.length()-1;
-        
-        while(i<j) {
-            char ch1 = s.charAt(i);
-            char ch2 = s.charAt(j);
-            
-            if(ch1!=ch2) return false;
-            
-            i++;
-            j--;
-        }
-        
-        return true;
     }
 }
