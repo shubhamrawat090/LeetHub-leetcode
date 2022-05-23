ad2915219ad987e10a8c166ffa7348c04243797b
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-23 23:39:12 +0530

    Time: 0 ms (100.00%), Space: 44.8 MB (96.94%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
@@ -15,32 +15,25 @@
  */
 class Solution {
     public int countNodes(TreeNode root) {
-       return helper(root);
-    }
-
-    public int helper(TreeNode root) {
-        if(root==null) return 0;
+        if(root == null) return 0;
         
-        int l=0;
-        int r=0;
-        
-        //find lowest level on left side
-        TreeNode current=root;
-        while(current!=null) {
-            l++;
-            current=current.left;
+        int leftLv = 0;
+        TreeNode curr = root;
+        while(curr!=null) {
+            leftLv++;
+            curr = curr.left;
         }
         
-        //find lowest level on right side
-        current=root;
-        while(current!=null) {
-            r++;
-            current=current.right;
+        int rightLv = 0;
+        curr = root;
+        while(curr!=null) {
+            rightLv++;
+            curr = curr.right;
         }
         
-        //if a complete binary tree(then left and right ends are on same level) exists then nodes = 2^L-1
-        if(l==r) return (int)Math.pow(2,l)-1;
-        //otherwise traverse on left and right side
-        else return 1+helper(root.left)+helper(root.right);
+        if(leftLv == rightLv) 
+            return (int)Math.pow(2, leftLv) - 1;
+        else 
+            return 1 + countNodes(root.left) + countNodes(root.right);
     }
 }
