6fbe11a184efb4c0a02184f2a85c59b1ca39febf
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-23 12:31:41 +0530

    Time: 169 ms (26.21%), Space: 137 MB (8.28%) - LeetHub

---
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -0,0 +1,64 @@
+class Solution {
+    public int findMaxForm(String[] strs, int m, int n) {
+        int[][][] dp = new int[m+1][n+1][strs.length+1];
+        for(int[][] row: dp) {
+            for(int[] col: row) {
+                Arrays.fill(col, -1);
+            }
+        }
+        
+        int ans = memo(strs, m, n, 0, dp);
+        return ans;
+    }
+    
+    //Memoized
+    private int memo(String[] strs, int m, int n, int idx, int[][][] dp) {
+        if(idx == strs.length) return dp[m][n][idx] = 0;
+        
+        if(dp[m][n][idx] != -1) return dp[m][n][idx];
+        
+        //exclude curr value
+        int exc = memo(strs, m, n, idx+1, dp);
+        
+        //include curr value
+        int[] count = getCount(strs[idx]);
+        int inc = 0;
+        
+        //if curr values count of 0's and 1's matches the condition, then count it in ans
+        if(count[0]<=m && count[1]<=n) {
+            inc = 1 + memo(strs, m-count[0], n-count[1], idx+1, dp);
+        }
+        
+        return dp[m][n][idx] = Math.max(inc, exc); 
+    }
+    
+    //Recursive : Gives TLE
+    private int rec(String[] strs, int m, int n, int idx) {
+        if(idx == strs.length) return 0;
+        
+        //exclude curr value
+        int exc = rec(strs, m, n, idx+1);
+        
+        //include curr value
+        int[] count = getCount(strs[idx]);
+        int inc = 0;
+        
+        //if curr values count of 0's and 1's matches the condition, then count it in ans
+        if(count[0]<=m && count[1]<=n) {
+            inc = 1 + rec(strs, m-count[0], n-count[1], idx+1);
+        }
+        
+        return Math.max(inc, exc); 
+    }
+    
+    private int[] getCount(String str) {
+        int zero = 0, one = 0;
+
+        for(char c: str.toCharArray()) {
+            if(c=='0') zero++;
+            else one++;
+        }
+        
+        return new int[]{zero, one};
+    }
+}
