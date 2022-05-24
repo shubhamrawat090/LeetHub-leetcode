08fa04ee233ab607f2472deea06bf276396fb4d1
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-24 11:52:56 +0530

    Time: 9 ms (25.84%), Space: 43.4 MB (40.05%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.java
@@ -5,10 +5,13 @@ class Solution {
         int ans = 0;
         
         for(int i=0; i<s.length(); i++) {
+            //finding longest difference b/w indices of '(' ')' pair when ')' is encountered
             if(s.charAt(i)==')' && stk.size()>1 && s.charAt(stk.peek())=='(') {
                 stk.pop();
                 ans = Math.max(ans, i-stk.peek());
-            } else {
+            } 
+            //push index of '('
+            else {
                 stk.push(i);
             }
         }
