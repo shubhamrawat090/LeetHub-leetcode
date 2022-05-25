e1a4013fcdf0d6936f5db577385cd5d3a7bef88e
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-25 22:48:36 +0530

    Time: 4 ms (86.55%), Space: 45 MB (23.74%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.java
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -14,36 +14,44 @@
  * }
  */
 class Solution {
-    int count = 0;
     public int pathSum(TreeNode root, int targetSum) {
+        //stores currSum -> occurrence of curr sum
         HashMap<Integer, Integer> map = new HashMap();
+        //sum = 0 occurred 1 time
         map.put(0, 1);
-        helper(root, targetSum, 0, map);
-        return count;
+        return helper(root, targetSum, 0, map);
     }
     
-    public void helper(TreeNode root, int target, int sum, HashMap<Integer, Integer> map) {
-        if(root == null) return;
+    public int helper(TreeNode root, int target, int sum, HashMap<Integer, Integer> map) {
+        if(root == null) return 0;
         
+        //add root.val to our curr sum
         sum = sum+root.val;
+        int count = 0;
+        //if sum-target exists in map then we have caught some path = target
         if(map.containsKey(sum-target)) {
             count += map.get(sum-target);
         } 
         
+        //if the sum is occurred already => increment it
         if(map.containsKey(sum)) {
             map.put(sum, map.get(sum)+1);
-        }else {
+        }
+        //otherwise, occurred 1st time
+        else {
             map.put(sum, 1);
         }
         
-        helper(root.left, target, sum, map);
-        helper(root.right, target, sum, map);
+        count += helper(root.left, target, sum, map);
+        count += helper(root.right, target, sum, map);
         
+        //backtrack
         if(map.get(sum) == 1) {
             map.remove(sum);
         }else {
             map.put(sum, map.get(sum)-1);
-        }
+        } 
         
+        return count;
     }
 }
