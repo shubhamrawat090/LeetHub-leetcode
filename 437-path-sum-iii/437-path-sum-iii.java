b31e5b5264c0235fd31d37c0ab0e0a65c095564f
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-26 12:09:58 +0530

    Time: 53 ms (7.71%), Space: 43.4 MB (68.39%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.java
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -14,44 +14,31 @@
  * }
  */
 class Solution {
+    int count = 0;
     public int pathSum(TreeNode root, int targetSum) {
-        //stores currSum -> occurrence of curr sum
-        HashMap<Integer, Integer> map = new HashMap();
-        //sum = 0 occurred 1 time
-        map.put(0, 1);
-        return helper(root, targetSum, 0, map);
+        dfs(root, targetSum);
+        return count;
     }
     
-    public int helper(TreeNode root, int target, int sum, HashMap<Integer, Integer> map) {
-        if(root == null) return 0;
+    public void dfs(TreeNode root, int targetSum) {
+        if(root == null) return;
         
-        //add root.val to our curr sum
-        sum = sum+root.val;
-        int count = 0;
-        //if sum-target exists in map then we have caught some path = target
-        if(map.containsKey(sum-target)) {
-            count += map.get(sum-target);
-        } 
+        helper(root, targetSum, 0);
         
-        //if the sum is occurred already => increment it
-        if(map.containsKey(sum)) {
-            map.put(sum, map.get(sum)+1);
-        }
-        //otherwise, occurred 1st time
-        else {
-            map.put(sum, 1);
+        dfs(root.left, targetSum);
+        dfs(root.right, targetSum);
+    }
+    
+    private void helper(TreeNode root, int targetSum, int sum) {
+        if(root == null) return;
+        
+        sum += root.val;
+        
+        if(targetSum == sum) {
+            count++;
         }
         
-        count += helper(root.left, target, sum, map);
-        count += helper(root.right, target, sum, map);
-        
-        //backtrack
-        if(map.get(sum) == 1) {
-            map.remove(sum);
-        }else {
-            map.put(sum, map.get(sum)-1);
-        } 
-        
-        return count;
+        helper(root.left, targetSum, sum);
+        helper(root.right, targetSum, sum);
     }
 }
