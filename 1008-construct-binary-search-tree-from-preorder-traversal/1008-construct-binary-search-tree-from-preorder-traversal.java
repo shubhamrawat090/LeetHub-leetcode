2f6b04ba1c657fc7d316af03623f025bcc966395
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-26 11:56:26 +0530

    Time: 0 ms (100.00%), Space: 43 MB (6.62%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -14,23 +14,32 @@
  * }
  */
 class Solution {
+    //keeps incrementing globally
     int idx = 0;
     public TreeNode bstFromPreorder(int[] preorder) {
+        //construct a tree with upper and lower bound for current node value
         return construct(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
     
+    //helper function to construct the BST
     private TreeNode construct(int[] pre, int min, int max) {
+        //all elements are processed and tree is completed
         if(idx == pre.length) return null;
+        
+        //if the element is within range of min,max
         else if(pre[idx]>min && pre[idx]<max) {
+            //create a new node and add its value
             TreeNode node = new TreeNode();
             node.val = pre[idx];
-            idx++;
+            idx++;//increment the idx as you have used this value
             
+            //construct left and right subtree with next value and limits changes and add it using our node as root
             node.left = construct(pre, min, node.val);
             node.right = construct(pre, node.val, max);
-            
+            //return that node
             return node;
         } else {
+            //for any other case just return null
             return null;
         }
     }
