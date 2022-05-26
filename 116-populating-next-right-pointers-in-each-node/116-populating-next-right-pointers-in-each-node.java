ada35ecda4f6c8c19918e045ce0bb9a64b85380d
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-26 21:33:48 +0530

    Time: 0 ms (100.00%), Space: 47.3 MB (48.16%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
@@ -23,30 +23,23 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
-        if (root == null || (root.left == null && root.right == null) ) {
+        if(root == null || (root.left == null && root.right == null)) {
             return root;
         }
         
-        // connect left child to right child
-        if (root.left != null && root.right != null) {
+        if(root.left!=null && root.right!=null) {
             root.left.next = root.right;
         }
         
-        // connect right child (or left if right is abasent) to child of next nodes.
-        Node lNode = (root.right == null) ? root.left : root.right;
-        
-        Node next = root.next;
-        while (next != null && next.left == null && next.right == null) {
-            next = next.next;
-        }
-
-        if (next != null) {
-            lNode.next = (next.left != null) ? next.left : next.right;
+        if(root.right!=null) {
+            if(root.next!=null)
+                root.right.next = root.next.left;
+            else 
+                root.right.next = null;
         }
         
-        // process right child first
-        connect(root.right);
         connect(root.left);
+        connect(root.right);
         
         return root;
     }
