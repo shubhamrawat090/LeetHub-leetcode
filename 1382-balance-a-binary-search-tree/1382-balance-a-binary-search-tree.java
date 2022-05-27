05ea267be7606b8acca41e614b7a5c494be4777d
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-28 00:13:13 +0530

    Time: 7 ms (29.62%), Space: 61.3 MB (38.23%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.java
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.java
@@ -15,62 +15,27 @@
  */
 class Solution {
     public TreeNode balanceBST(TreeNode root) {
-        if(root == null || (root.left==null && root.right==null)) {
-            return root;
-        }
-        //store values in arraylist - inorder - increasing order
-        ArrayList<Integer> values = new ArrayList<>();
-        traverse(root, values);
-        //construct a balanced tree with those values
-        return construct(0, values.size()-1, values);
+        ArrayList<Integer> list = new ArrayList();
+        inOrder(root,list);
+        return makeTree(list,0,list.size() - 1);
+    }
+    public TreeNode makeTree(ArrayList<Integer> list, int low, int high){
+        if(low > high){
+            return null;
+        }   
+        int middle = low + (high - low)/2;
+        TreeNode node = new TreeNode(list.get(middle));
+        node.left= makeTree(list,low,middle - 1);
+        node.right= makeTree(list,middle + 1,high);
+        return node;
     }
     
-    private TreeNode construct(int start, int end, ArrayList<Integer> values) {
-        if(start > end) return null;
-        
-        //mid as root
-        int mid = start+(end-start)/2;
-        TreeNode root = new TreeNode(values.get(mid));
-        //start to mid-1 is left subtree
-        root.left = construct(start, mid-1, values);
-        //mid+1 to end is left subtree
-        root.right = construct(mid+1, end, values);
-        
-        return root;
-    }
-    
-    //morris traversal - inorder
-    private void traverse(TreeNode root, ArrayList<Integer> values) {
-        
-        while(root != null) {
-            if(root.left == null) {
-                //print 
-                values.add(root.val);
-                //go to right
-                root = root.right;
-            } else {
-                //find inorder predecessor
-                TreeNode iop = root.left;
-                while(iop.right != null && iop.right != root) {
-                    iop = iop.right;
-                }
-                //if left side is unprocessed
-                if(iop.right == null) {
-                    //make thread
-                    iop.right = root;
-                    //go to left
-                    root = root.left;
-                }
-                //if left side is processed
-                else {
-                    //break thread
-                    iop.right = null;
-                    //print
-                    values.add(root.val);
-                    //go to right
-                    root = root.right;
-                }
-            }
+    public void inOrder(TreeNode root,ArrayList<Integer> list){
+        if(root == null){
+            return;
         }
+        inOrder(root.left,list);
+        list.add(root.val);
+        inOrder(root.right,list);
     }
 }
