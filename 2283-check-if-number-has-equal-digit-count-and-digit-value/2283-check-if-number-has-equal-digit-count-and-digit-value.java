9aeee87e8c822465f601ea74b77bfe2164d25ad7
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-28 23:15:45 +0530

    Time: 1 ms (100.00%), Space: 42.5 MB (33.33%) - LeetHub

--- 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.java
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.java
@@ -1,11 +1,15 @@
+//Frequency Array
 class Solution {
     public boolean digitCount(String num) {
+        //10 is the limit in constraints
         int[] freq = new int[10];
         
+        //get freq of all digits
         for(int i=0; i<num.length(); i++) {
             freq[num.charAt(i)-'0']++;
         }
         
+        //match it according to question
         for(int i=0; i<num.length(); i++) {
            if(freq[i]!=num.charAt(i)-'0')
                return false;
