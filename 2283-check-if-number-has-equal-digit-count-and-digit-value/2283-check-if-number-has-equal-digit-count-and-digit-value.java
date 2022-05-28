06893624d4d289f4e389629fd04df3be4c42a88a
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-28 23:14:45 +0530

    Time: 1 ms (100.00%), Space: 40.7 MB (100.00%) - LeetHub

--- 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.java
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.java
@@ -1,27 +1,14 @@
 class Solution {
     public boolean digitCount(String num) {
-        int n = num.length();
+        int[] freq = new int[10];
         
-        HashMap<Integer, Integer> map = new HashMap();
-        
-        for(int i=0; i<n; i++) {
-            int digit = Character.getNumericValue(num.charAt(i));
-            if(!map.containsKey(digit)) {
-                map.put(digit, 1);
-            } else {
-                map.put(digit, map.get(digit)+1);
-            }  
+        for(int i=0; i<num.length(); i++) {
+            freq[num.charAt(i)-'0']++;
         }
         
-        for(int i=0; i<n; i++) {
-            if(!map.containsKey(i))
-                map.put(i,0);
-        }
-        
-        for(int key: map.keySet()) {
-            int digit = Character.getNumericValue(num.charAt(key));
-            int mapFreq = map.get(key);
-            if(digit!=mapFreq) return false;
+        for(int i=0; i<num.length(); i++) {
+           if(freq[i]!=num.charAt(i)-'0')
+               return false;
         }
         
         return true;
