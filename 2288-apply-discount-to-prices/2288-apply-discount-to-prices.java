0b257bcfd50875bc938c4cdff3acf89544d74795
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-29 10:27:40 +0530

    Time: 64 ms (33.33%), Space: 72.7 MB (33.33%) - LeetHub

--- 2288-apply-discount-to-prices/2288-apply-discount-to-prices.java
+++ 2288-apply-discount-to-prices/2288-apply-discount-to-prices.java
@@ -1,31 +1,47 @@
 class Solution {
     public String discountPrices(String sentence, int discount) {
-        String[] words = sentence.split(" ");
+        String [] vals = sentence.split(" ");
         
-        StringBuilder ans = new StringBuilder();
-        for(String word: words) {
-            if(word.charAt(0)=='$' && word.length()!=1) {
-                boolean check = false;
-                for(int i=1; i<word.length(); i++) {
-                    if(word.charAt(i)=='$' || !Character.isDigit(word.charAt(i))) {
-                        check = true;
-                        break;
-                    }   
-                }
-                
-                if(check == false) {
-                    double val = Double.parseDouble(word.substring(1));
-                    double disc = (double)discount/100;
-                    double newVal = val - (val*disc);
-                    word = "$"+String.format("%.2f", newVal);
-                }
+        StringBuilder sb = new StringBuilder();
+        String s;
+        double d;
+        int v;
+        
+        for (int i = 0; i < vals.length; ++i){
+            s = vals[i];
+            
+            if (isPrice(s)){
+                d = Double.parseDouble(s.substring(1));
+                d -= d * discount / 100;
+                sb.append('$');
+                sb.append((long) d);
+                d %= 1;
+                d *= 100;
+                v = (int)d + (d % 1 >= 0.5 ? 1 : 0);
+                sb.append('.');
+                if (v < 10 )
+                    sb.append(0);
+                sb.append(v);
+            }else{
+                sb.append(s);
             }
             
-            ans.append(word+" ");   
+            if (i < vals.length - 1)
+                sb.append(' ');
         }
         
-        String finalAns = ans.toString().trim();
+        return sb.toString();
+    }
+    
+    private boolean isPrice(String s){
+        if (s.length() < 2 || s.charAt(0) != '$' || s.charAt(1) == '0')
+            return false;
         
-        return finalAns;
+        
+        for (int i = 1; i < s.length(); ++i){
+            if (s.charAt(i) < '0' || s.charAt(i) > '9') return false;
+        }
+        
+        return true;
     }
 }
