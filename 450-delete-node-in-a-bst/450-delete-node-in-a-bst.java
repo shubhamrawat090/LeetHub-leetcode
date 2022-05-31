268b948ebda299ed7c55a08fd1190aca8bc5dba5
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-06-01 00:12:10 +0530

    Time: 0 ms (100.00%), Space: 50.4 MB (5.48%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
@@ -14,44 +14,48 @@
  * }
  */
 class Solution {
-    private int min(TreeNode root){
-        if(root.left!=null){
-            return min(root.left);
-        }else{
-            return root.val;
-        }
-    }
     public TreeNode deleteNode(TreeNode root, int key) {
-        if(root == null){
+        if(root == null) {
             return null;
         }
         
-        if(key<root.val){
-            root.left = deleteNode(root.left, key);
-        }else if(key>root.val){
+        if(key>root.val) {
             root.right = deleteNode(root.right, key);
-        }else{
-            //case 1: no children i.e. leaf node
-            if(root.right == null && root.left == null){
+        } else if(key<root.val) {
+            root.left = deleteNode(root.left, key);
+        }
+        
+        if(root.val == key) {
+            //no child-> leaf node
+            if(root.left == null && root.right == null) {
                 return null;
             }
             
-            //case 2: 1 child either left or right
-            if(root.left == null){
+            //1 child
+            if(root.left == null) {
                 return root.right;
             }
-            if(root.right == null){
+            else if(root.right == null) {
                 return root.left;
             }
-            
-            //case 3: two children
-            else{
-                int minRight = min(root.right);
-                root.val = minRight;
-                root.right = deleteNode(root.right, minRight);
+            else {
+                //both children
+                //get right min
+                int min = getMin(root.right);
+                root.val = min;//set data
+                root.right = deleteNode(root.right, min);//delete that min node in right subtree
             }
+            
         }
         
         return root;
+    }
+    
+    private int getMin(TreeNode root) {
+        if(root.left!=null) {
+            return getMin(root.left);
+        }
+        
+        return root.val;
     }
 }
