f1b5a26fc3af7f82477d8064b38ed5450a0e8c40
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-05-31 22:06:46 +0530

    Time: 1 ms (73.68%), Space: 43.3 MB (59.62%) - LeetHub

---
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count = 0;
+    public int minCameraCover(TreeNode root) {
+        //0-> camera, 1->monitored, -1->not monitored
+        if(helper(root)==-1) {
+            //if root is not monitored after putting all cameras, add 1 camera to root
+            return count+1;
+        }
+        
+        return count;
+    }
+    
+    private int helper(TreeNode root) {
+        if(root == null) {
+            return 1;
+        }
+        
+        int left = helper(root.left);
+        int right = helper(root.right);
+        
+        if(left==-1 || right==-1) {
+            //any side is not monitored
+            count++;
+            return 0;
+        }
+        
+        if(left==0 || right==0) {
+            //any side has camera, then we are monitored
+            return 1;
+        }
+        
+        //we are unmonitored
+        return -1;
+    }
+}
