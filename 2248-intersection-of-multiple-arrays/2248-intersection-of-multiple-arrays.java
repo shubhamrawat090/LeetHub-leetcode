ec13ea211a11e89ff0cf124f44c13a98d8f6b9bf
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-06-04 12:07:23 +0530

    Time: 3 ms (90.86%), Space: 46 MB (68.15%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.java
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.java
@@ -1,26 +1,25 @@
 class Solution {
     public List<Integer> intersection(int[][] nums) {
-        //make a frequency map
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<nums.length; i++) {
-            for(int j=0; j<nums[i].length; j++) {
-                if(!map.containsKey(nums[i][j])) {
-                    map.put(nums[i][j], 1);
-                } else {
-                    map.put(nums[i][j], map.get(nums[i][j])+1);
-                }
+        if (nums == null || nums.length == 0) {
+            return new ArrayList<>();
+        } 
+        
+        int len = nums.length;
+        
+        int[] freq = new int[1001];
+        for (int[] arr : nums) {
+            for (int n : arr) {
+                freq[n]++;
             }
         }
         
-        //add only those values whose frequency is equal to length of nums array
-        ArrayList<Integer> ans = new ArrayList<>();
-        for(int key: map.keySet()) {
-            if(map.get(key) == nums.length) {
-                ans.add(key);
+        List<Integer> res = new ArrayList<>();
+        for (int i = 1; i <= 1000; i++) {
+            if (freq[i] == len) {
+                res.add(i);
             }
         }
         
-        Collections.sort(ans);
-        return ans;
+        return res;
     }
 }
