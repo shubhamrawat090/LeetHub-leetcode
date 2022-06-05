a1599834333d9a64f4ffa60c3938b27a24997ec9
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-06-05 21:08:33 +0530

    Time: 1 ms (100.00%), Space: 42.1 MB (50.00%) - LeetHub

--- 2293-min-max-game/2293-min-max-game.java
+++ 2293-min-max-game/2293-min-max-game.java
@@ -1,32 +1,14 @@
 class Solution {
     public int minMaxGame(int[] nums) {
-        int flag = 0;
-        int n = log2(nums.length), size = nums.length;
-        while(n-->0) {
-            for(int i=0; i<nums.length-1; i+=2) {
-                if(flag == 0) {
-                    nums[i/2] = Math.min(nums[i], nums[i+1]);
-                    flag = 1;
-                } else {
-                    nums[i/2] = Math.max(nums[i], nums[i+1]);
-                    flag = 0;
-                }
-
-                size /= 2;
+        while (nums.length > 1) {
+            int n = nums.length;
+            int[] newNums = new int[n/2];
+            for (int i = 0; i < n/2; i++) {
+                if (i % 2 == 0) newNums[i] = Math.min(nums[2 * i], nums[2 * i + 1]);
+                else newNums[i] = Math.max(nums[2 * i], nums[2 * i + 1]);
             }
-            flag = 0;
+            nums = newNums;
         }
-        
         return nums[0];
-    }
-    
-    public int log2(int N)
-    {
- 
-        // calculate log2 N indirectly
-        // using log() method
-        int result = (int)(Math.log(N) / Math.log(2));
- 
-        return result;
     }
 }
