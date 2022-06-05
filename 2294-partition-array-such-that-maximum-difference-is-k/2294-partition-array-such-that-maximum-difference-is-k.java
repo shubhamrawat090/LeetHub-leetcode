e8e3aef36e6953ec7a4d9ae662c850ce33a7d9cb
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-06-05 21:24:23 +0530

    Time: 16 ms (85.71%), Space: 58.7 MB (85.71%) - LeetHub

--- 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.java
+++ 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.java
@@ -1,19 +1,23 @@
-// GREEDY SOLUTION: Time O(NlogN) due to sorting, Space O(1)
 class Solution {
     public int partitionArray(int[] nums, int k) {
-        Arrays.sort(nums);
-        int ans = 0;
-        int i=0;
-        while(i<nums.length) {
-            int j=i;
-            //find the longest possible subsequence with max-min<=k
-            while(j<nums.length && nums[j]-nums[i]<=k) {
-                j++;
-            }
-            ans++;//count 1 subsequence
-            //starting of the next subsequence
-            i=j;
+        //Arrays.sort(nums);
+        int result = 0;
+        boolean [] visited = new boolean [100_001];
+        int prev = - k - 1;
+        
+        for (int num : nums){
+            visited[num] = true;
         }
-        return ans;
+        
+        for (int i = 0; i < visited.length; ++i){
+            if (!visited[i]) continue;
+            
+            if (i - prev > k){
+                ++result;
+                prev = i;
+            }
+        }
+        
+        return result;
     }
 }
