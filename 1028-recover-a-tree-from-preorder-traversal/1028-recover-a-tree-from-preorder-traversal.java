6b88aea4cb3a715ac4fed46b8f39d8c7758b5e95
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-06-07 23:09:27 +0530

    Time: 5 ms (79.22%), Space: 45.8 MB (60.24%) - LeetHub

--- 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.java
+++ 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.java
@@ -9,6 +9,7 @@ class Solution {
     public pair dfs(String traversal, int level){
         int initial = i;
 
+        //count value i.e. non '-'
         for( ; i < traversal.length(); i++)
            if(traversal.charAt(i) == '-')
                break;
@@ -16,35 +17,32 @@ class Solution {
         String s = traversal.substring(initial , i);
         int val =  Integer.parseInt(s);
 
-
+        //count '-'
         int count = 0;
-
         for( ; i < traversal.length(); i++){
 
             if(traversal.charAt(i) != '-')
-             break;
+                break;
 
             count++;
-           } 
+        } 
 
         pair p = new pair();
         p.node = new TreeNode(val);
 
         p.count = count;
 
-
         if(p.count != level)
           return p;
 
-        pair left = dfs(traversal , level + 1 );
+        pair left = dfs(traversal, level + 1);
         p.node.left = left.node;
         p.count = left.count;
 
         if(left.count != level)
             return p;
 
-
-        pair right = dfs(traversal , level+1 );
+        pair right = dfs(traversal, level+1);
         p.node.right = right.node;
         p.count = right.count;
 
