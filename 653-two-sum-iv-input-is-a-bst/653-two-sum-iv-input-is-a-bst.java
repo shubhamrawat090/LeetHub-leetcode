0ad5e7e7f5be081e711f4c333620633ce902f966
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-06-09 11:08:32 +0530

    Time: 1 ms (100.00%), Space: 43.3 MB (76.23%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -15,50 +15,41 @@
  */
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
-        //root is sent 2 times because 1 of them is the root of original tree as we have to start searching in the original tree and not the root of the subtree in different recursive function calls
-        return targetSumPair(root, root, k);
+        return helper(root, root, k);
     }
     
-    //for every node search its complement(targetSum - node.data) in the entire tree
-    //time O(NlogN) => traversing each node * searching in entire tree
-    public boolean targetSumPair(TreeNode node, TreeNode originalRoot, int targetSum){
-        if(node == null){
-          return false;
+    private boolean helper(TreeNode orig, TreeNode node, int target) {
+        if(node == null) {
+            return false;
         }
-
-        //get ans from left subtree if there is a pair available
-        boolean left = targetSumPair(node.left, originalRoot, targetSum);
-
-        //complement of our root's value
-        int complement = targetSum - node.val;
-        //only pairs allowed are (a,b) where a<b, a = root.val, b = complement of root.val
-        if(complement>node.val){
-          if(search(originalRoot, complement)==true)
+        
+        boolean left = helper(orig, node.left, target);
+        
+        //curr node
+        if(target-node.val>node.val) {
+            if(search(orig, target-node.val)) {
+                return true;
+            }
+        }
+        
+        boolean right = helper(orig, node.right, target);
+        
+        return left || right;
+    }
+    
+    private boolean search(TreeNode node, int val) {
+        if(node == null) {
+            return false;
+        }
+        
+        if(node.val == val) {
             return true;
         }
-
-        //get ans from left subtree if there is a pair available
-        boolean right = targetSumPair(node.right, originalRoot, targetSum);
         
-        //return true if either of the ans is true which means we have found atleast 1 pair
-        return right || left;
-    } 
-    
-      
-      // function to search in the entire tree
-      public boolean search(TreeNode node, int target){
-        if(node == null){
-          return false;
+        if(val < node.val) {
+            return search(node.left, val);
+        } else {
+            return search(node.right, val);
         }
-
-        if(node.val == target){
-          return true;
-        }
-
-        if(target<node.val){
-          return search(node.left, target);
-        }else{
-          return search(node.right, target);
-        }
-      }
+    }
 }
