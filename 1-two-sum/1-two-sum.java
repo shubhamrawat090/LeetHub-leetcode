51f97587e3348629813cf593548b876dc0a62ca5
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-06-09 10:56:44 +0530

    Time: 3 ms (85.90%), Space: 45.4 MB (39.48%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -5,16 +5,10 @@ class Solution {
         int size = nums.length;
         
         for(int i=0; i<size; i++) {
-            map.put(nums[i], i);
-        }
-        
-        for(int i=0; i<size; i++) {
             if(map.containsKey(target-nums[i])) {
-                if(map.get(target-nums[i])==i) {
-                    continue;
-                }
                 return new int[]{i, map.get(target-nums[i])};
             }
+            map.put(nums[i], i);
         }
         
         return null;
