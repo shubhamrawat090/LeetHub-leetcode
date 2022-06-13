1cf56f22746e6e971f4b7e45d9088f0b5518a1d4
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-06-13 21:45:00 +0530

    Time: 4 ms (69.63%), Space: 57 MB (56.71%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -5,7 +5,8 @@ class Solution {
         for(int i=0; i<grid.length; i++) {
             for(int j=0; j<grid[0].length; j++) {
                 if(grid[i][j] == '1') {
-                   dfs(grid, i, j);
+                    dfs(grid, i, j);//dfs marks all current as well as its '1' neighbours visited
+                    //counting all new connected components
                     ans++;
                 }
             }
@@ -15,15 +16,18 @@ class Solution {
     }
     
     private void dfs(char[][] grid, int r, int c) {
+        //out of bounds or already visited
         if(r<0 || r>=grid.length || c<0 || c>=grid[0].length || grid[r][c]=='0') {
             return;
         }
 
+        //mark visited
         grid[r][c] = '0';
         
-        dfs(grid, r+1, c);
-        dfs(grid, r-1, c);
-        dfs(grid, r, c+1);
-        dfs(grid, r, c-1);
+        //call in all 4 directions
+        dfs(grid, r+1, c);//down
+        dfs(grid, r-1, c);//up
+        dfs(grid, r, c+1);//right
+        dfs(grid, r, c-1);//left
     }
 }
