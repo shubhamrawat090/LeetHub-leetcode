d5929f736c777d2c3ce53a06cc95ca7ec538d3ab
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-06-13 06:34:40 +0530

    Time: 4 ms (61.51%), Space: 42.2 MB (77.71%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -7,11 +7,36 @@ class Solution {
         }
         
         return memo(triangle, 0, 0, dp);
+        
+        // return tab(triangle, dp);
     }
+    
+    //TABULATION
+//     private int tab(List<List<Integer>> triangle, int[][] dp) {
+//         for(int r=triangle.size()-1; r>=0; r--) {
+//             for(int c=triangle.get(r).size()-1; c>=0; c--) {
+//                 if(r==triangle.size()-1) {
+//                     int val = c == triangle.get(r).size()-1? triangle.get(r).get(c): Math.min(triangle.get(r).get(c), triangle.get(r).get(c+1));
+//                     dp[r][c] = val;
+//                     continue;
+//                 }
+
+//                 int same = triangle.get(r).get(c);
+//                 same += dp[r+1][c];
+
+//                 int next = triangle.get(r).get(c);
+//                 next += dp[r+1][c+1];
+
+//                 dp[r][c] = Math.min(same, next);
+//             }
+//         }
+        
+//         return dp[0][0];
+//     }
     
     //MEMOIZED
     private int memo(List<List<Integer>> triangle, int r, int c, int[][] dp) {
-        if(r==triangle.size() || c==triangle.get(r).size()) {
+        if(r==triangle.size()) {
             return dp[r][c] = 0;
         }
         
@@ -30,7 +55,7 @@ class Solution {
     
     //RECURSIVE
     private int rec(List<List<Integer>> triangle, int r, int c) {
-        if(r==triangle.size() || c==triangle.get(r).size()) {
+        if(r==triangle.size()) {
             return 0;
         }
         
