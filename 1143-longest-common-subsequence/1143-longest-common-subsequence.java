007b6943a82558331f4b782dce803679621162d6
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-06-14 07:23:39 +0530

    Time: 27 ms (24.77%), Space: 46.4 MB (50.31%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,12 +1,46 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int[][] dp = new int[text1.length()+1][text2.length()+1];
-        for(int[] d: dp) {
-            Arrays.fill(d, -1);
+        // for(int[] d: dp) {
+        //     Arrays.fill(d, -1);
+        // }
+        
+        // return memo(text1, text2, 0, 0, dp);
+        // return helper(text1, text2, 0, 0);
+        return tab(text1, text2, dp);
+    }
+    
+    //TABULATE
+    private int tab(String s1, String s2, int[][] dp) {
+        for(int idx1=s1.length()-1; idx1>=0; idx1--) {
+            for(int idx2=s2.length()-1; idx2>=0; idx2--) {
+                //0 length cannot have any subsequence
+                if(idx1==s1.length() || idx2==s2.length()) {
+                    dp[idx1][idx2] = 0;
+                    continue;
+                }
+
+                //found 1 common character, so increment the common subsequence length by 1
+                if(s1.charAt(idx1) == s2.charAt(idx2)) {
+                    //also go to next character of both strings
+                    dp[idx1][idx2] = 1 + dp[idx1+1][idx2+1];
+                    continue;
+                }
+
+                //no common character
+                //2 scenarios possible
+
+                //skip s1's current char
+                int skip1 = dp[idx1+1][idx2];
+                //skip s2's current char
+                int skip2 = dp[idx1][idx2+1];
+
+                //maximum of both the scenarios is returned
+                dp[idx1][idx2] = Math.max(skip1, skip2);
+            }
         }
         
-        return memo(text1, text2, 0, 0, dp);
-        // return helper(text1, text2, 0, 0);
+        return dp[0][0];
     }
     
     // MEMOIZED
