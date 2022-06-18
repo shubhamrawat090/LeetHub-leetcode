d53814cd0fde8a945d3ba465cebf2e553dda6985
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-06-18 11:11:27 +0530

    Time: 606 ms (32.31%), Space: 63.2 MB (93.59%) - LeetHub

--- 745-prefix-and-suffix-search/745-prefix-and-suffix-search.java
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.java
@@ -1,32 +1,27 @@
 class WordFilter {
-    String[] words;
-    HashMap<String, Integer> dp;
-
-    public WordFilter(String[] w) {
-        this.words = w;
-        this.dp = new HashMap<>();
-    }
-    
-    public int f(String prefix, String suffix) {
-        int ans = -1;
+    HashMap<String,Integer> map;
+    public WordFilter(String[] words) {
         
-        String combPreSuf = prefix + "-" + suffix;
+        String prefix="", suffix="";
         
-        if(dp.containsKey(combPreSuf)) {
-            return dp.get(combPreSuf);
-        }
+        map = new HashMap<String,Integer>();
         
-        for(int i=0; i<words.length; i++) {
-            String word = words[i];
-            
-            if(word.startsWith(prefix) && word.endsWith(suffix)) {
-                ans = i;
+        for(int i=0;i<words.length;i++) {
+            for(int j=0;j<words[i].length();j++) {
+                
+                prefix= words[i].substring(0,j+1);
+                
+                for(int k=words[i].length()-1;k>=0;k--) {
+                    
+                    suffix = words[i].substring(k);
+                    map.put(prefix+"#"+suffix,i);
+                }
             }
         }
-        
-        dp.put(combPreSuf, ans);
-        
-        return ans;
+    }
+
+    public int f(String prefix, String suffix) {
+       return map.getOrDefault(prefix+"#"+suffix,-1);
     }
 }
 
