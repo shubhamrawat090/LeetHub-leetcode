f809a028dee3a9ec82f6aae49f5c673a0adf152c
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-06-20 12:40:41 +0530

    Time: 16 ms (92.64%), Space: 42.2 MB (97.55%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.java
+++ 820-short-encoding-of-words/820-short-encoding-of-words.java
@@ -1,35 +1,38 @@
 class Solution {
     public int minimumLengthEncoding(String[] words) {
         //Take a word and check if there is any suffix of that word is present in the array and ignore that .
-        HashMap<String, Integer> valid = new HashMap<>();
+        HashMap<String, Integer> wordMap = new HashMap<>();
         
         int n = words.length;
         
         //frequency map for words
         for(int i=0;i<n;i++) { 
-            valid.put(words[i], valid.getOrDefault(words[i], 0) + 1);
+            wordMap.put(words[i], wordMap.getOrDefault(words[i], 0) + 1);
         }
         
-        //
+        //checking if any suffix of curr word already exists in array => SKIP it
         for(int i=0;i<n;i++){
             for(int len=1;len<words[i].length();len++){
-                    if(valid.containsKey(words[i].substring(len))){
-                        valid.put(words[i].substring(len), valid.get(words[i].substring(len)) - 1);
-                    } 
+                String suffix = words[i].substring(len);
+                
+                //skipping the suffix by decreasing its frequency
+                if(wordMap.containsKey(suffix)){
+                    wordMap.put(suffix, wordMap.get(suffix) - 1);
+                } 
             }  
         }
         
-        int count=0;
-        int res=0;
+        // Encoded string : word1#word2#, length of encoded string = len(word1) + len(word2) + 2 #'s i.e. no. of words left in the frequency map after removing the suffixes present in array
+        int countOfWords=0;
+        int totalWordsLength=0;
         
-         for(String i: valid.keySet()){
-             if(valid.get(i) > 0){
-                 count++;
-                 res+= i.length();
+         for(String i: wordMap.keySet()){
+             if(wordMap.get(i) > 0){
+                 countOfWords++;
+                 totalWordsLength += i.length();
              }
          }
         
-        res += count;
-        return res;
+        return totalWordsLength + countOfWords;
     }
 }
