abab5ae5de4952b8e93ac2b8a62e737389b028f0
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-06-20 16:57:23 +0530

    Time: 11 ms (79.68%), Space: 46.4 MB (33.71%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.java
+++ 778-swim-in-rising-water/778-swim-in-rising-water.java
@@ -1,5 +1,5 @@
 class Solution {
-    class Pair{
+    class Pair implements Comparable<Pair>{
         int i;
         int j;
         int time;
@@ -9,6 +9,10 @@ class Solution {
             this.j = j;
             this.time = time;
         }
+        
+        public int compareTo(Pair o) {
+            return this.time - o.time;
+        }
     }
     
     public int swimInWater(int[][] grid) {
@@ -16,7 +20,7 @@ class Solution {
         
         boolean[][] vis = new boolean[n][n];
         
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.time - b.time);
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
         pq.add(new Pair(0, 0, grid[0][0]));
         
         while(pq.size()>0) {
