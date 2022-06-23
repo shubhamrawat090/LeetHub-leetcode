f0a7566a20e0879f6c2b632c7ec8af391453cbc2
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-06-23 11:15:38 +0530

    Time: 70 ms (30.19%), Space: 66.2 MB (78.15%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.java
+++ 630-course-schedule-iii/630-course-schedule-iii.java
@@ -1,7 +1,9 @@
 class Solution {
     public int scheduleCourse(int[][] courses) {
+        //sort courses on the basis of their last day for ease in processing
         Arrays.sort(courses, (a, b) -> a[1] == b[1] ? a[0]-b[0]: a[1]-b[1]);
         
+        //get more time taking course from the ones we have taken so far
         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
         
         int timeElapsed = 0;
@@ -11,10 +13,12 @@ class Solution {
             
             //only work if duration is within the last day to finish the course
             if(duration<=lastDay) {
-                if(duration + timeElapsed <= lastDay) {
+                //if time taken till now + duration of current course is withing the last day then add it in our time taken
+                if(timeElapsed + duration <= lastDay) {
                     timeElapsed += duration;
                     pq.offer(duration);
                 } else {
+                    //try to swap the longest course taken till now with the current course and check if can be withing last day of current course
                     if(!pq.isEmpty() && pq.peek()>duration) {
                         timeElapsed = timeElapsed - pq.poll();
                         timeElapsed += duration;
