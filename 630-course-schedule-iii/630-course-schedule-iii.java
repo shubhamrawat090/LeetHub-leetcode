c22895eab93f0a04b4ea1d5c87af143cac20f7be
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-06-23 11:03:46 +0530

    Time: 57 ms (55.00%), Space: 67.1 MB (75.37%) - LeetHub

---
+++ 630-course-schedule-iii/630-course-schedule-iii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int scheduleCourse(int[][] courses) {
+        Arrays.sort(courses, (a, b) -> a[1] == b[1] ? a[0]-b[0]: a[1]-b[1]);
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
+        
+        int time = 0;
+        for(int[] course: courses) {
+            //only work if duration is within the last day to finish the course
+            if(course[0]<=course[1]) {
+                if(course[0] + time <= course[1]) {
+                    time += course[0];
+                    pq.add(course[0]);
+                } else {
+                    if(pq.peek()>course[0]) {
+                        time -= pq.remove();
+                        time += course[0];
+                        pq.add(course[0]);
+                    }
+                }
+            }
+        }
+        
+        return pq.size();
+    }
+}
