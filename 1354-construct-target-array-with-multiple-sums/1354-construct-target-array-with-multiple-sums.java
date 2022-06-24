9ed3786985160df9a2fa734a63288fd0ab966887
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-06-24 12:45:40 +0530

    Time: 12 ms (79.80%), Space: 58.1 MB (32.32%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
@@ -7,52 +7,54 @@
 // 5. Repeat till all values become 1 in array.
 
 
-// TIME: O(Nlog(maxVal)), Space: log(maxVal) for recursive stack
+// TIME: O(NlogN), Space: O(N)
 class Solution {
     public boolean isPossible(int[] target) {
         long sum = 0;
-        int maxIdx = 0;
         
-        //get sum of all elements and maxIdx
+        //maxHeap to get maximum element
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        
+        //get sum of all elements and add in priority queue
         for(int i=0; i<target.length; i++) {
             sum += target[i];
-            if(target[i]>target[maxIdx]) {
-                maxIdx = i;
+            pq.add(target[i]);
+        }
+        
+        while(pq.peek()!=1) {
+            //MAXIMUM VALUE
+            int maxVal = pq.remove();
+
+            //get diff sum
+            long diffSum = sum - maxVal;
+
+            //if diff sum is 1 then we can keep on subtracting 1 from maxValue to make it 1
+            if(diffSum == 1) {
+                return true;
             }
+
+            //on updating maxValue, it shouldn't become -ve as we won't be able to make it 1 again(because all values are +ve)
+            if(diffSum > maxVal) {
+                return false;
+            }
+
+            //if diffSum = 0 then maxVal will never be changed and we might get TLE
+            if(diffSum == 0) {
+                return false;
+            }
+
+            //if we keep on subtracting diffSum and then we get to 0 => we won't ever reach to 1
+            if(maxVal % diffSum == 0) {
+                return false;
+            }
+
+            //update maxVal
+            //we use "%" instead of "-" because we can save steps where maxValue is at the same idx and we keep on subtracting the same diffSum from it
+            maxVal %= diffSum;
+            sum = diffSum + maxVal;//new total sum for updated array
+            pq.add(maxVal);//add new maxValue after updating
         }
         
-        //MAXIMUM VALUE
-        long maxVal = target[maxIdx];
-        
-        //get diff sum
-        long diffSum = sum - maxVal;
-        
-        //if diff sum is 1 then we can keep on subtracting 1 from maxValue to make it 1
-        //also, if maxValue is 1 then we have successfully converted our maximum value to 1
-        if(diffSum == 1 || maxVal == 1) {
-            return true;
-        }
-        
-        //on updating maxValue, it shouldn't become -ve as we won't be able to make it 1 again(because all values are +ve)
-        if(diffSum > maxVal) {
-            return false;
-        }
-        
-        //if diffSum = 0 then maxVal will never be changed and we might get TLE
-        if(diffSum == 0) {
-            return false;
-        }
-        
-        //if we keep on subtracting diffSum and then we get to 0 => we won't ever reach to 1
-        if(maxVal % diffSum == 0) {
-            return false;
-        }
-        
-        //update maxVal
-        //we use "%" instead of "-" because we can save steps where maxValue is at the same idx and we keep on subtracting the same diffSum from it
-        target[maxIdx] %= diffSum;
-        
-        //repeat for updated array
-        return isPossible(target);
+        return true;
     }
 }
