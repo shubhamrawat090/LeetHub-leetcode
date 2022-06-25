a966d1dc61de4977a0e1a327e725ff3699c5941a
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-06-25 10:35:47 +0530

    Time: 1 ms (97.53%), Space: 53.8 MB (59.83%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.java
+++ 665-non-decreasing-array/665-non-decreasing-array.java
@@ -1,21 +1,29 @@
-// TIME: O(N), SPACE: O(1)
 class Solution {
-    public boolean checkPossibility(int[] nums) {
-        int pos = -1;
-        for(int i=0; i<nums.length-1; i++) {
-            //finding anomaly i.e. curr>next
-            if(nums[i]>nums[i+1]) {
-                //if pos!=-1, this means pos changed because there was an anomaly before and now we have 2 anomalies which will violate the condition of changing just 1 value to make non-decreasing array
-                if(pos != -1) {
-                    return false;
-                }
-                pos = i;
-            }
-        }
-        
-        //if pos is unchanged => array is already increasing order and no anomaly found
-        //if pos = 0 or pos = len-2 => we need to change only 1 value
-        //we can change the pos value to make entire array non-decreasing only if nums[pos-1]<=nums[pos+1] || nums[pos]<=nums[pos+2]
-        return pos == -1 || pos == 0 || pos == nums.length-2 || nums[pos-1]<=nums[pos+1] || nums[pos]<=nums[pos+2];
-    }
+	public boolean checkPossibility(int[] arr) {
+		int index = helper(arr);
+		if (index == -1) {
+			return true;
+		}
+		int temp = arr[index];
+		arr[index] = arr[index + 1];
+		if (helper(arr) == -1) {
+			return true;
+		}
+
+		arr[index] = temp;
+		arr[index + 1] = temp;
+		if (helper(arr) == -1) {
+			return true;
+		}
+		return false;
+	}
+
+	public int helper(int[] arr) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[i] > arr[i + 1]) {
+				return i;
+			}
+		}
+		return -1;
+	}
 }
