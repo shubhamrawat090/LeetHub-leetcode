78577fe42e93bc264ac146d50a2ea7ec47dd0e51
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-06-28 16:39:52 +0530

    Time: 20 ms (48.70%), Space: 54 MB (74.58%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,39 +1,23 @@
 class Solution {
     public int minDeletions(String s) {
-        HashMap<Character, Integer> hm = new HashMap<>();
-
-        for (int i=0; i<s.length(); i++) {
-            hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0) + 1);
+        Set<Integer> set = new HashSet<>();
+        int[] alpha = new int[26];
+        char[] arr =s.toCharArray();
+        for(char c:arr)
+        {
+            alpha[c-'a']++;
         }
-
-        PriorityQueue<Character> pq = new PriorityQueue<>((a, b) -> hm.get(b) - hm.get(a));
-
-        for (Map.Entry<Character, Integer> entry : hm.entrySet()) {
-            pq.add(entry.getKey());
-        }
-
-        Set<Integer> hs = new HashSet<>();
-
-        int ans = 0;
-        while (!pq.isEmpty()) {
-            char c = pq.poll();
-            if (hs.isEmpty()) {
-               hs.add(hm.get(c));
-                continue;
-            }
-            int freq = hm.get(c);
-            int del = 0;
-            for (int i = freq; i>=1; i--) {
-                if (!hs.contains(i)) {
-                    hs.add(i);
-                    break;
+        int count = 0;
+        for(int i=0;i<26;i++) {
+            if(alpha[i] != 0) {
+                while(set.contains(alpha[i]) && alpha[i]>0) {
+                    alpha[i]--;
+                    count++;
                 }
-                del++;
-            }
-            ans += del;
+                set.add(alpha[i]);
+            } 
         }
-
-        return ans;
+        
+        return count;
     }
 }
-
