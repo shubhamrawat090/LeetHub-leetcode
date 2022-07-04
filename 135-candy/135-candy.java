e094f52790f01038c1762ee2b46aa388032684cc
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-07-04 12:31:40 +0530

    Time: 5 ms (45.91%), Space: 51.7 MB (50.80%) - LeetHub

--- 135-candy/135-candy.java
+++ 135-candy/135-candy.java
@@ -2,30 +2,29 @@ class Solution {
     public int candy(int[] ratings) {
         int size = ratings.length;
         
-        //process from left
-        int[] left = new int[size];
-        Arrays.fill(left, 1);
+        //process from left side
+        int[] processingArray = new int[size];
+        Arrays.fill(processingArray, 1);
         
         for(int i=1; i<size; i++) {
             if(ratings[i]>ratings[i-1]) {
-                left[i] = left[i-1] + 1;
+                processingArray[i] = processingArray[i-1] + 1;
             }
         }
         
-        //process from right
-        int[] right = new int[size];
-        Arrays.fill(right, 1);
-        
+        //process from right side
+        int rightVal = 1;
         for(int i=size-2; i>=0; i--) {
             if(ratings[i]>ratings[i+1]) {
-                right[i] = right[i+1] + 1;
+                processingArray[i] = Math.max(processingArray[i], rightVal + 1);
             }
+            rightVal = processingArray[i];
         }
         
         //get maximum of both left and right combined
         int totalCandies = 0;
         for(int i=0; i<size; i++) {
-            totalCandies += Math.max(left[i], right[i]);
+            totalCandies += processingArray[i];
         }
         
         return totalCandies;
