1bda8b93c3a75b8533ed98dbbf1cff0296fa6e97
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-07-09 11:51:24 +0530

    Time: 48 ms (61.89%), Space: 84.6 MB (75.89%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.java
+++ 1696-jump-game-vi/1696-jump-game-vi.java
@@ -8,19 +8,24 @@ class Solution {
         dq.offerLast(0);
         
         for(int i=1;i<n;i++){
+            //add maximum value at the starting of deque to nums[i]
             nums[i] += nums[dq.peekFirst()];
             
+            //if current value is greater than minimum in queue then remove last values from queue
             while(dq.size()>0 && nums[i]>=nums[dq.peekLast()]) {
                 dq.pollLast();
             }
             
+            //add current value to queue
             dq.addLast(i);
             
+            //if front value is beyond k members from i the remove front value
             if(i-dq.peekFirst()>=k) {
                 dq.pollFirst();
             }
         }
         
+        //answer is stored at last value
         return nums[n-1];
     }
 }
