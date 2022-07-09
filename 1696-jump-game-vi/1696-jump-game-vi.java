712d07c315ff5d0bb342ba0cdab0d1a01bf4092f
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-07-09 11:42:28 +0530

    Time: 146 ms (11.64%), Space: 112.8 MB (41.49%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.java
+++ 1696-jump-game-vi/1696-jump-game-vi.java
@@ -1,20 +1,33 @@
 class Solution {
     public int maxResult(int[] nums, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+         /**
+        For every index starting from 1: --- n 
+            Find out the max sum from all { i -1 } to  { i –k } index -- klogk
+            sum[index] = value[index] + maxfound
+        Result is sum[length -1]
+
+        */
         
-        pq.add(new int[]{0, nums[0]});
-        
+        int n = nums.length;
         int max = nums[0];
-        for(int i=1; i<nums.length; i++) {
-            while(i-pq.peek()[0]>k) {
-                pq.remove();
+        
+        // index --- maxSum
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[1]-a[1]);
+        
+        pq.offer(new int[]{0,nums[0]});
+        
+        for(int i=1;i<n;i++){
+            
+            while(i-pq.peek()[0]>k){
+                pq.poll();
             }
             
             int[] top = pq.peek();
             
             max = nums[i] + top[1];
             
-            pq.add(new int[]{i, max});
+            pq.offer(new int[]{i,max});
+            
         }
         
         return max;
