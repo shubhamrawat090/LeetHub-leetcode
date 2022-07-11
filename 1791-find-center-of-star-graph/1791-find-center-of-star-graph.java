4b35f43beb117af993c81a13a25a29e994495e9e
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-07-11 17:20:52 +0530

    Time: 0 ms (100.00%), Space: 78.8 MB (46.95%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -3,6 +3,9 @@ class Solution {
         int[] firstEdge = edges[0];
         int[] secondEdge = edges[1];
         
+        // edges = [[1,2],[5,1],[1,3],[1,4]] => take any 2 edges and find the common vtx between the two 
+        // like in [1,2],[5,1], 1 is common => 1 is the center 
+        
         int center = -1;
         if(firstEdge[0] == secondEdge[0] || firstEdge[0] == secondEdge[1]) {
             center = firstEdge[0];
