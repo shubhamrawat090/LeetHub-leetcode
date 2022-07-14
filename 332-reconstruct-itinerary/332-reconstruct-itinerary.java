1e422b5e41569b1920711baffb290282f14ee328
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-07-14 22:53:06 +0530

    Time: 8 ms (81.15%), Space: 50.6 MB (31.50%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.java
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.java
@@ -1,5 +1,7 @@
 class Solution {
+    // Using HEIRHOLZER'S ALGORITHM to find Euler Path
     public List<String> findItinerary(List<List<String>> tickets) {
+        //converting tickets into a adjacency list where neighbors are sorted using priority queue
         HashMap<String, PriorityQueue<String>> adj = new HashMap<>();
         
         for(List<String> ticket: tickets) {
@@ -13,6 +15,7 @@ class Solution {
             adj.get(src).add(dest);
         }
         
+        //can use arraylist, but then need to add at 0th idx everytime which is time costing
         LinkedList<String> res = new LinkedList<>();
         dfs("JFK", res, adj);
         
@@ -20,13 +23,16 @@ class Solution {
     }
     
     private void dfs(String v, LinkedList<String> res, HashMap<String, PriorityQueue<String>> adj) {
-        if(adj.containsKey(v) == false || adj.get(v).size() == 0) {
+        //destination detected(as it was not encountered prev src vtx)
+        if(!adj.containsKey(v) || adj.get(v).size() == 0) {
             res.addFirst(v);
             return;
         } 
         
-        while(adj.get(v).size() > 0) {
-            String nbr = adj.get(v).remove();
+        //get the neighbors of current src vtx "v"
+        PriorityQueue<String> neighbors = adj.get(v);
+        while(neighbors.size() > 0) {
+            String nbr = neighbors.remove();
             
             dfs(nbr, res, adj);
         }
