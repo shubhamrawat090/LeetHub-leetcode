3483277a6b75b94f904b37267dd2ef1a80be58d3
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-07-16 19:17:27 +0530

    Time: 42 ms (8.96%), Space: 72.8 MB (5.63%) - LeetHub

---
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.java
@@ -0,0 +1,66 @@
+class Solution {
+    public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        int[][][] dp = new int[51][51][51];
+        
+        //fill entire dp with -1
+        for(int[][] d: dp) {
+            for(int[] arr: d) {
+                Arrays.fill(arr, -1);
+            }
+        }
+        
+        return memo(m, n, maxMove, startRow, startColumn, dp);
+        // return rec(m, n, maxMove, startRow, startColumn);
+        
+    }
+    
+    // RECURSIVE
+    public int rec(int m, int n, int maxMove, int startRow, int startColumn) {
+        if(maxMove < 0) {
+            return 0; //all allowed moves finished
+        }
+        
+        if(startRow < 0 || startRow >= m  || startColumn < 0 || startColumn >= n) {
+            return 1;//got outside 1 path
+        }
+        
+        int moves = 0;
+        int MOD = 1000000007;
+        
+        int up = rec(m, n, maxMove-1, startRow-1, startColumn);
+        int down = rec(m, n, maxMove-1, startRow+1, startColumn);
+        int left = rec(m, n, maxMove-1, startRow, startColumn-1);
+        int right = rec(m, n, maxMove-1, startRow, startColumn+1);
+        
+        moves += (up % MOD) + (down % MOD) + (left % MOD) + (right % MOD);
+        
+        return moves % MOD;
+    }
+    
+    // MEMOIZED
+    public int memo(int m, int n, int maxMoves, int startRow, int startColumn, int[][][] dp) {
+        if(maxMoves < 0) {
+            return 0; //all allowed moves finished
+        }
+        
+        if(startRow < 0 || startRow >= m  || startColumn < 0 || startColumn >= n) {
+            return 1;
+        }
+        
+        if(dp[startRow][startColumn][maxMoves] != -1) {
+            return dp[startRow][startColumn][maxMoves];
+        }
+        
+        long moves = 0;
+        int MOD = 1000000007;
+        
+        long up = memo(m, n, maxMoves-1, startRow-1, startColumn, dp);
+        long down = memo(m, n, maxMoves-1, startRow+1, startColumn, dp);
+        long left = memo(m, n, maxMoves-1, startRow, startColumn-1, dp);
+        long right = memo(m, n, maxMoves-1, startRow, startColumn+1, dp);
+        
+        moves += (up % MOD) + (down % MOD) + (left % MOD) + (right % MOD);
+        
+        return dp[startRow][startColumn][maxMoves] = (int)(moves % MOD);
+    }
+}
