232d33582a234a657435c82b5bfceb1d5a4072b6
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-07-20 02:12:59 +0530

    Time: 12 ms (35.53%), Space: 46.4 MB (78.14%) - LeetHub

---
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+        
+        int[] distPlus1 = null;
+        
+        for(int i=1; i<=k+1; i++) {
+            distPlus1 = dist.clone();
+            for(int[] flight: flights) {
+                // going from u --> v
+                int u = flight[0];
+                int v = flight[1];
+                
+                //cost of going from u --> v
+                int uv = flight[2]; 
+                
+                //if we have currently reached point "u" only then we can go to point "v"
+                if(dist[u] != Integer.MAX_VALUE) {
+                    distPlus1[v] = Math.min(distPlus1[v], dist[u] + uv);
+                }
+            }
+            dist = distPlus1; // update dist array to current points that we have covered
+        } 
+        
+        //NO PATH FOUND with minimum k stops in between src and dest
+        if(dist[dst] == Integer.MAX_VALUE) {
+            return -1;
+        } else {
+            return dist[dst];
+        }
+    }
+}
