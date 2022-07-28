60367efa8157159e784176513ede64b64706a0b2
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-07-28 15:54:24 +0530

    Time: 6 ms (63.45%), Space: 43.6 MB (65.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -4,27 +4,14 @@ class Solution {
             return false;
         }
         
-        HashMap<Character, Integer> sMap = new HashMap<>();
-        for(char c: s.toCharArray()) {
-            if(!sMap.containsKey(c)) {
-                sMap.put(c, 1);
-            } else {
-                sMap.put(c, sMap.get(c) + 1);
-            }
+        int[] freq = new int[26];
+        for(int i=0; i<s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
         }
         
-        for(char c: t.toCharArray()) {
-            if(!sMap.containsKey(c)) {
-                return false;
-            }
-            
-            if(sMap.containsKey(c)) {
-                sMap.put(c, sMap.get(c)-1);
-            }
-        }
-        
-        for(char c: sMap.keySet()) {
-            if(sMap.get(c) != 0) {
+        for(int i=0; i<26; i++) {
+            if(freq[i] != 0) {
                 return false;
             }
         }
