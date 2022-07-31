e6da9a8af037ad3043d0dbffa35d0d20a1eaeddd
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-07-31 09:55:07 +0530

    Time: 143 ms (56.61%), Space: 128.4 MB (57.11%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.java
@@ -3,55 +3,55 @@
 // TC : O(len) for build and O(log(len)) for query and update
 // SC : O(len)
 
-// class NumArray {
-//     int fenwickArr[];
-//     int len;
-//     int nums[];
+class NumArray {
+    int fenwickArr[];
+    int len;
+    int nums[];
     
-//     public NumArray(int[] nums) {
-//         this.nums = nums;
-//         len = nums.length+1;
-//         fenwickArr = new int[len];
-//         int prefixSum[] = new int[len];
+    public NumArray(int[] nums) {
+        this.nums = nums;
+        len = nums.length+1;
+        fenwickArr = new int[len];
+        int prefixSum[] = new int[len];
         
-//         for(int idx=1;idx<len;idx++){
-//             prefixSum[idx] = nums[idx-1]+prefixSum[idx-1];
-//         }
+        for(int idx=1;idx<len;idx++){
+            prefixSum[idx] = nums[idx-1]+prefixSum[idx-1];
+        }
         
-//         for(int idx=1;idx<len;idx++){
-//             fenwickArr[idx] = prefixSum[idx] - prefixSum[idx-(idx&(-idx))];
-//         }
-//     }
+        for(int idx=1;idx<len;idx++){
+            fenwickArr[idx] = prefixSum[idx] - prefixSum[idx-(idx&(-idx))];
+        }
+    }
     
-//     public void update(int index, int val) {
-//         int toAdd = val-nums[index];
-//         nums[index] = val;
-//         index++;
+    public void update(int index, int val) {
+        int toAdd = val-nums[index];
+        nums[index] = val;
+        index++;
         
-//         while(index<len){
-//             fenwickArr[index]+=toAdd;
-//             index+=(index&(-index));
-//         }
-//     }
+        while(index<len){
+            fenwickArr[index]+=toAdd;
+            index+=(index&(-index));
+        }
+    }
     
-//     public int sumRange(int left, int right) {
-//         left++;
-//         right++;
+    public int sumRange(int left, int right) {
+        left++;
+        right++;
         
-//         return query(right)-query(left-1);
-//     }
+        return query(right)-query(left-1);
+    }
     
-//     private int query(int index){
-//         int sum = 0;
+    private int query(int index){
+        int sum = 0;
         
-//         while(index>0){
-//             sum+=fenwickArr[index];
-//             index-=(index&(-index));
-//         }
+        while(index>0){
+            sum+=fenwickArr[index];
+            index-=(index&(-index));
+        }
         
-//         return sum;
-//     }
-// }
+        return sum;
+    }
+}
 
 /**
  * Your NumArray object will be instantiated and called as such:
@@ -66,62 +66,62 @@
 // TC : O(len) for build and O(log(len)) for query and update
 // SC : O(len)
 
-class NumArray {
-   int tree[];
-   int nums[];
-   int len;
+// class NumArray {
+//    int tree[];
+//    int nums[];
+//    int len;
    
-   public NumArray(int[] nums) {
-      this.nums = nums;
-      len = nums.length;
-      tree = new int[4*len];
+//    public NumArray(int[] nums) {
+//       this.nums = nums;
+//       len = nums.length;
+//       tree = new int[4*len];
       
-      build(1,0,len-1); 
-   }
+//       build(1,0,len-1); 
+//    }
    
-   private void build(int node,int start,int end){
-       if(start==end){
-           tree[node] = nums[start];
-           return;
-       }
+//    private void build(int node,int start,int end){
+//        if(start==end){
+//            tree[node] = nums[start];
+//            return;
+//        }
        
-       int mid = start + (end-start)/2;
-       build(node*2,start,mid);
-       build(node*2+1,mid+1,end);
+//        int mid = start + (end-start)/2;
+//        build(node*2,start,mid);
+//        build(node*2+1,mid+1,end);
        
-       tree[node] = tree[node*2]+tree[node*2+1];
-   }
+//        tree[node] = tree[node*2]+tree[node*2+1];
+//    }
    
-   public void update(int index, int val) {
-       update(1,0,len-1,index,val-nums[index]);
-       nums[index] = val;
-   }
+//    public void update(int index, int val) {
+//        update(1,0,len-1,index,val-nums[index]);
+//        nums[index] = val;
+//    }
    
-   private void update(int node,int start,int end,int index,int toAdd){
-       if(start==end){
-           tree[node]+=toAdd;
-           return;
-       }
+//    private void update(int node,int start,int end,int index,int toAdd){
+//        if(start==end){
+//            tree[node]+=toAdd;
+//            return;
+//        }
        
-       int mid = start + (end-start)/2;
-       if(index<=mid) update(node*2,start,mid,index,toAdd);
-       else update(node*2+1,mid+1,end,index,toAdd);
+//        int mid = start + (end-start)/2;
+//        if(index<=mid) update(node*2,start,mid,index,toAdd);
+//        else update(node*2+1,mid+1,end,index,toAdd);
        
-       tree[node] = tree[node*2]+tree[node*2+1];
-   }
+//        tree[node] = tree[node*2]+tree[node*2+1];
+//    }
    
-   public int sumRange(int left, int right) {
-       return query(1,0,len-1,left,right);
-   }
+//    public int sumRange(int left, int right) {
+//        return query(1,0,len-1,left,right);
+//    }
    
-   private int query(int node,int start,int end,int left,int right){
-       if(start>right||end<left) return 0;
-       if(start>=left&&end<=right) return tree[node];
+//    private int query(int node,int start,int end,int left,int right){
+//        if(start>right||end<left) return 0;
+//        if(start>=left&&end<=right) return tree[node];
        
-       int mid = start + (end-start)/2;
-       return query(node*2,start,mid,left,right) + query(node*2+1,mid+1,end,left,right);
-   }
-}
+//        int mid = start + (end-start)/2;
+//        return query(node*2,start,mid,left,right) + query(node*2+1,mid+1,end,left,right);
+//    }
+// }
 
 /**
 * Your NumArray object will be instantiated and called as such:
