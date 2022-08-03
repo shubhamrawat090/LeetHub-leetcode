851449079f2659cf6fc44f78ba6f964d14a76ab9
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-08-03 07:00:52 +0530

    Time: 1 ms (90.58%), Space: 46 MB (9.22%) - LeetHub

--- 768-max-chunks-to-make-sorted-ii/768-max-chunks-to-make-sorted-ii.java
+++ 768-max-chunks-to-make-sorted-ii/768-max-chunks-to-make-sorted-ii.java
@@ -7,10 +7,8 @@ class Solution {
         rightMin[rightMin.length-1] = Integer.MAX_VALUE; // last elem is +infinity
         
         //creating rightmin arr
-        int min = Integer.MAX_VALUE;
         for(int i=rightMin.length-2; i>=0; i--) {
-            min = Math.min(min, arr[i]);
-            rightMin[i] = min;
+            rightMin[i] = Math.min(rightMin[i+1], arr[i]);
         }
         
         
