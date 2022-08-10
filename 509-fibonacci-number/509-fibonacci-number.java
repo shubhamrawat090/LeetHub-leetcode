ab5f786423c9b5f9c5d928abf2f367d80987ea2e
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-08-10 15:25:41 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (57.05%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,9 +1,23 @@
 class Solution {
     public int fib(int n) {
+        int[] dp = new int[n+1];
+        
+        Arrays.fill(dp, -1);
+        
+        return memo(n, dp);
+    }
+    
+    private int memo(int n, int[] dp) {
         if(n == 0 || n == 1) {
-            return n;
+            return dp[n] = n;
         }
         
-        return fib(n-1) + fib(n-2);
+        if(dp[n] != -1) {
+            return dp[n];
+        }
+        
+        int ans = memo(n-1, dp) + memo(n-2, dp);
+        
+        return dp[n] = ans;
     }
 }
