92a8db4b3082c65a91962a6983138861dc819aae
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-08-10 15:31:55 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (25.81%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,22 +1,23 @@
 class Solution {
     public int fib(int n) {
-        int[] dp = new int[n+1];
-        
-        return tabulate(n, dp);
+        return spaceOptimized(n);
     }
     
-    private int tabulate(int n, int[] dp) {
-        for(int i=0; i<=n; i++) {
-            if(i == 0 || i == 1) {
-                dp[i] = i;
-                continue;
-            }
-
-            int ans = dp[i-1] + dp[i-2];
-
-            dp[i] = ans;
+    private int spaceOptimized(int n) {
+        int first = 0, second = 1;
+        
+        if(n == 0) {
+            return first;
+        }
+        if(n == 1) {
+            return second;
+        }
+        for(int i=2; i<=n; i++) {
+            int ans = first + second;
+            first = second;
+            second = ans;
         }
         
-        return dp[n];
+        return second;
     }
 }
