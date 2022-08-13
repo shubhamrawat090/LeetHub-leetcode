9afff298b5e795aabb8e1d8eb09fac36df353bc9
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-08-13 13:27:31 +0530

    Time: 1 ms (89.32%), Space: 44.3 MB (11.46%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,5 +1,24 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x, n);
+        if(n < 0) {
+            x = 1/x;
+        }
+        
+        return helper(x, n);
+    }
+    
+    private double helper(double x, int n) {
+        if(n == 0) {
+            return 1;
+        }
+        
+        double xNb2 = helper(x, n/2);
+        double ans = xNb2 * xNb2;
+        
+        if((n&1) != 0) {
+            ans *= x;
+        }
+        
+        return ans;
     }
 }
