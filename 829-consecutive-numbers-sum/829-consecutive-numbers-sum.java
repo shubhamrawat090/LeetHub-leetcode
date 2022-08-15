93d8b6d07c6c5d090d5694604dfe644ed025320f
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-08-15 18:19:21 +0530

    Time: 12 ms (12.33%), Space: 41 MB (42.42%) - LeetHub

--- 829-consecutive-numbers-sum/829-consecutive-numbers-sum.java
+++ 829-consecutive-numbers-sum/829-consecutive-numbers-sum.java
@@ -3,7 +3,7 @@ class Solution {
         int count = 0;
         
         //checking for all no. of values which add up to n
-        for(int k=1; 2*n > k*(k-1); k++) {
+        for(int k = 1; 2*n > k * (k-1); k++) {
             //which k value satisfies the condition
             int numerator = n - (k * (k-1) / 2);
             
