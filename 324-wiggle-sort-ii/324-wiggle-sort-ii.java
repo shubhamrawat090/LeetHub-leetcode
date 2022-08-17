8859410f3c73b015c907f91f6605a8832d6daa3d
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-08-17 21:03:47 +0530

    Time: 4 ms (96.35%), Space: 54.8 MB (81.54%) - LeetHub

--- 324-wiggle-sort-ii/324-wiggle-sort-ii.java
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.java
@@ -1,16 +1,21 @@
 class Solution {
     public void wiggleSort(int[] nums) {
-        int[] freq = new int[5001];
         int size = nums.length;
         int[] ans = new int[size];
         
+        //create freq array
         int max = -1, min = 5001;
+        int[] freq = new int[5001];
         for(int i=0; i<size; i++) {
             max = Math.max(max, nums[i]);
             min = Math.min(min, nums[i]);
             freq[nums[i]]++;
         }
         
+        //go left to right, skipping 2 steps each iteration
+        //keep adding highest value till its freq goes to 0
+        //when freq goes to 0, add second highest
+        //keep this process going
         int idx = max;
         for(int odIdx=1; odIdx<size; odIdx+=2) {
             while(freq[idx]==0) {
@@ -21,8 +26,12 @@ class Solution {
             freq[idx]--;
         } 
         
+        //go right to left, skipping 2 steps each iteration
+        //keep adding lowest value till its freq goes to 0
+        //when freq goes to 0, add second lowest
+        //keep this process going
         idx = min;
-        int evIdx = (size%2 == 0) ? size-2: size-1;
+        int evIdx = (size%2 == 0) ? size-2: size-1; //start pt diff for odd and even idx
         while(evIdx>=0) {
             while(freq[idx]==0) {
                 idx++;
