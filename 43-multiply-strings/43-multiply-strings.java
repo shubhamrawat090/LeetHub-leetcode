b1e1fb67fd9b2469495c8fa79ac020002b5f12b1
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2022-09-23 21:26:10 +0530

    Time: 8 ms (65.17%), Space: 42.5 MB (82.45%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.java
+++ 43-multiply-strings/43-multiply-strings.java
@@ -5,15 +5,27 @@ class Solution {
         }
         
         int l1 = num1.length(), l2 = num2.length();
-        int[] res = new int[l1+l2];
+        int[] res = new int[l1+l2]; // max length of the ans can be sum of digits of both numbers
         
         int carry = 0;
         int skipPlaces = 0;
         
+        // traverse num2 from backwards JUST like we do in multiplication
         for(int i=l2-1; i>=0; i--) {
-            carry = 0;
-            int n2 = num2.charAt(i) - '0';
+            carry = 0; //reset carry
+            int n2 = num2.charAt(i) - '0'; //current digit of num2
             
+            /* we fill res[] backwards but first we skip some spaces like depicted with "X"
+                                       1 2 3
+                                     * 4 5 6
+                                   ----------
+                                     1 3 6 8   10^0 => skip 0 places
+                                     9 0 2 X   10^1 => skip 1 places
+                                   4 5 6 X X   10^2 => skip 2 places
+                                   ----------
+                                   5 6 0 8 8
+                                   ---------- 
+           */
             int k = res.length-1 - skipPlaces;
             for(int j=l1-1; j>=0 || carry!=0; j--) {
                 int n1 = j>=0 ? num1.charAt(j) - '0': 0;
